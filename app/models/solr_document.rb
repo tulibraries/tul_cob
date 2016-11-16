6ab7cea9efd667a5178165aff6257dd6a7ebc0ff
Steven Ng <steven.ng@temple.edu>
2016-11-16 14:38:44 -0500

    COB-6 Initial commit

---
+++ app/models/solr_document.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+class SolrDocument
+  include Blacklight::Solr::Document    
+      # The following shows how to setup this blacklight document to display marc documents
+  extension_parameters[:marc_source_field] = :marc_display
+  extension_parameters[:marc_format_type] = :marcxml
+  use_extension( Blacklight::Solr::Document::Marc) do |document|
+    document.key?( :marc_display  )
+  end
+  
+  field_semantics.merge!(    
+                         :title => "title_display",
+                         :author => "author_display",
+                         :language => "language_facet",
+                         :format => "format"
+                         )
+
+
+
+  # self.unique_key = 'id'
+
+  # Email uses the semantic field mappings below to generate the body of an email.
+  SolrDocument.use_extension(Blacklight::Document::Email)
+
+  # SMS uses the semantic field mappings below to generate the body of an SMS email.
+  SolrDocument.use_extension(Blacklight::Document::Sms)
+
+  # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
+  # Semantic mappings of solr stored fields. Fields may be multi or
+  # single valued. See Blacklight::Document::SemanticFields#field_semantics
+  # and Blacklight::Document::SemanticFields#to_semantic_values
+  # Recommendation: Use field names from Dublin Core
+  use_extension(Blacklight::Document::DublinCore)
+end
---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  
+  Blacklight::Marc.add_routes(self)
+  mount Blacklight::Engine => '/'
+  root to: "catalog#index"
+    concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
