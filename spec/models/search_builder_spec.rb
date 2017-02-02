df4586473b65cc23abaaeb80b3148304f3c9d04c
Steven Ng <steven.ng@temple.edu>
2017-02-02 15:54:15 -0500

    More specific description

---
+++ spec/models/search_builder_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe SearchBuilder, type: :model do
+  describe "Simple instantiation test in lieu of implementation" do
+    subject { SearchBuilder.new(any_args) }
+    it { is_expected.to be }
+  end
+end
