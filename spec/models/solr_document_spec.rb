dca0cb84893cd39e6a15acf7d4f27f388bfcf9be
Steven Ng <steven.ng@temple.edu>
2017-02-02 15:15:50 -0500

    Add SolrDocument model spec

---
+++ spec/models/solr_document_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe SolrDocument, type: :model do
+  let(:document) { SolrDocument.new }
+  
+  it "handles Email" do
+    expect(document).to respond_to(:to_email_text)
+  end
+  it "handles SMS" do
+    expect(document).to respond_to(:to_sms_text)
+  end
+  it "handles Dubin Core" do
+    expect(document).to respond_to(:dublin_core_field_names)
+  end
+end
