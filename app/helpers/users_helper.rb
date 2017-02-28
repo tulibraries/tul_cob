4861206a3a70bcc53349d520a249502e07b4ad09
Chris Doyle <cdoyle@temple.edu>
2017-02-28 12:00:01 -0500

    Created helpers to format dates and display Overdue only when status is overdue.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+	require 'date'
+
+	def make_date date
+		DateTime.parse(date).strftime("%m/%d/%Y") 
+	end
+	def total_holds holds
+		holds.list.size
+	end
+	def total_loans loans
+		loans.list.size
+	end
+	def is_overdue status
+		status == 'Overdue' ? status : ''
+	end
 end
