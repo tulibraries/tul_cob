d84fa735582bc0d4ce1a3978e1a825a6a3c98f02
Chad Nelson <chad.nelson@temple.edu>
2017-05-22 10:04:26 -0400

    Use dynamic number of processes

--- scripts/massage.sh
+++ scripts/massage.sh
@@ -5,6 +5,6 @@
 PATH_TO_MARCXMLS=$1
 NUM_PROCESSES=`grep -c ^processor /proc/cpuinfo`
 
-find $PATH_TO_MARCXMLS -name "*.xml"| xargs -t -n 1 -P 4 sed -i "s~<collection><record>~<collection xmlns=\"http://www.loc.gov/MARC21/slim\"><record>~"
+find $PATH_TO_MARCXMLS -name "*.xml"| xargs -t -n 1 -P $NUM_PROCESSES sed -i "s~<collection><record>~<collection xmlns=\"http://www.loc.gov/MARC21/slim\"><record>~"
 
 exit $?
