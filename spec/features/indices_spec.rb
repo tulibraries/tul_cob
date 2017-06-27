3792ef5ff640e4db3b186bcbbad274f331091f70
Steven Ng <steven.ng@temple.edu>
2017-06-27 16:31:54 -0400

    Use marc_fixture

--- spec/features/indices_spec.rb
+++ spec/features/indices_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 require 'solr_wrapper'
+require 'traject'
+require 'traject/command_line'
 
 RSpec.feature "Indices", type: :feature do
   SOLR_INSTANCE = SolrWrapper.default_instance({})
-  before(:all) { SOLR_INSTANCE.start }
+  before(:all) {
+    SOLR_INSTANCE.start
+    Traject::CommandLine.new(["-c",
+                              "app/models/traject_indexer.rb",
+                              File.join("spec", "fixtures", "marc_fixture")]).execute
+  }
   after(:all) { SOLR_INSTANCE.stop }
 
-  scenario "User visits home page" do
-    visit '/'
-    expect(page).to have_text("Welcome!")
+  context "publicly available pages" do
+    scenario "User visits home page" do
+      visit '/'
+      expect(page).to have_text("Welcome!")
+    end
+
+    scenario "User visits a document" do
+      visit 'catalog/991012177509703811'
+      expect(page).to have_text("Academic freedom in an age of conformity")
+    end
   end
 end
