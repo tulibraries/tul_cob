c80e83d963c4222dce8b88b5ed3686d6c11d09a0
Steven Ng <steven.ng@temple.edu>
2017-06-29 17:10:35 -0400

    Specs for simple item title search

--- spec/features/indices_spec.rb
+++ spec/features/indices_spec.rb
@@ -1,27 +1,74 @@
 require 'rails_helper'
-require 'solr_wrapper'
 require 'traject'
 require 'traject/command_line'
+require 'yaml'
 
+require 'pry' # FIXME
 RSpec.feature "Indices", type: :feature do
-  SOLR_INSTANCE = SolrWrapper.default_instance({})
-  before(:all) {
-    SOLR_INSTANCE.start
-    Traject::CommandLine.new(["-c",
-                              "app/models/traject_indexer.rb",
-                              File.join("spec", "fixtures", "marc_fixture")]).execute
-  }
-  after(:all) { SOLR_INSTANCE.stop }
+  feature "Home Page" do
+    context "publicly available pages" do
+      scenario "User visits home page" do
+        visit '/'
+        expect(page).to have_text "Welcome!"
+        within("#facets") do
+            expect(page).to have_text "Date"
+        end
+      end
 
-  context "publicly available pages" do
-    scenario "User visits home page" do
+    end
+  end
+
+  feature "Catalog" do
+    let (:title) { "Academic freedom in an age of conformity" }
+    let (:results_url) { "http://www.example.com/?utf8=%E2%9C%93&search_field=all_fields&q=Academic+freedom+in+an+age+of+conformity" }
+    scenario "Search" do
       visit '/'
-      expect(page).to have_text("Welcome!")
+      fill_in 'q', with: title
+      click_button 'Search'
+      expect(current_url).to eq results_url
+      within(".document-position-0 h3") do
+        expect(page).to have_text title 
+      end
+      within(".document-metadata") do
+        expect(page).to have_text "Resource Type:"
+        expect(page).to have_text "Book and Print"
+        expect(page).to have_text "Status/Location:"
+      end
+    end
+  end
+
+  feature "Document" do
+    let (:fixtures) {
+      YAML.load_file("#{fixture_path}/features.yml")
+    }
+
+    let (:item) {
+      fixtures.fetch("simple_search")
+    }
+
+    let (:item_url) {
+      "#{Capybara.default_host}/catalog/#{item['doc_id']}"
+    }
+
+    scenario "Search" do
+      visit '/'
+      fill_in 'q', with: item['title']
+      click_button 'Search'
+      expect(current_url).to eq item['url']
+      within(".document-position-0") do
+        click_link item['title']
+        expect(current_url).to eq item_url
+        within("h3") do
+          expect(page).to have_text item['title']
+        end
+        click_link item['title']
+      end
     end
 
-    scenario "User visits a document" do
-      visit 'catalog/991012177509703811'
-      expect(page).to have_text("Academic freedom in an age of conformity")
+    scenario "User visits a document directoy" do
+      visit "catalog/#{item['doc_id']}"
+      expect(current_url).to eq item_url
+      expect(page).to have_text(item['title'])
     end
   end
 end
