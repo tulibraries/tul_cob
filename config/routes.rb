2d003c4933d3e17fd45ee8cf8494f2ea3d1aba9f
Jennifer Anton <jenanton01@gmail.com>
2017-07-07 10:14:59 -0400

    Remove get routes for errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'errors/not_found'
-
-  get 'errors/internal_erver_error'
-
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
@@ -77,7 +73,7 @@ Rails.application.routes.draw do
   end
 
   match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all 
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
