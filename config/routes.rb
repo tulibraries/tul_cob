1b89e07a2ac73cb818df9345c7f136889c617e7e
Jennifer Anton <jenanton01@gmail.com>
2017-07-07 08:38:19 -0400

    Fix type in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -77,7 +77,7 @@ Rails.application.routes.draw do
   end
 
   match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_erver_error", :via => :all 
+  match "/500", :to => "errors#internal_server_error", :via => :all 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
