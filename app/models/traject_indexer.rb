f77e9f79fa5c227815bddb6c30e85ee40f109617
Jennifer Anton <jenanton01@gmail.com>
2017-07-20 14:17:26 -0400

    Updated traject_indexer
    - moved date_copyright below pub_date
    - put regex into a method

--- app/models/traject_indexer.rb
+++ app/models/traject_indexer.rb
@@ -30,6 +30,10 @@ def get_xml options={}
   end
 end
 
+def four_digit_year
+  gsub(/[^0-9,.]/, '').gsub(/[[:punct:]]/, '')[0..3].strip
+end
+
 settings do
   # type may be 'binary', 'xml', or 'json'
   provide "marc_source.type", "xml"
@@ -240,20 +244,20 @@ end
     to_field 'pub_location_t', extract_marc('260a:264a', :trim_punctuation => true)
     to_field 'publisher_t', extract_marc('260b:264b', :trim_punctuation => true)
 
-    to_field 'date_copyright_display' do |rec, acc|
-      rec.fields(['264']).each do |field|
-        acc << field['c'].gsub(/[^0-9,.]/, '').gsub(/[[:punct:]]/, '')[0..3].strip  if field.indicator2 == '4'
-      end
-    end
-
     to_field 'pub_date' do |rec, acc|   #, extract_marc('260c:264c')
       # fairly aggressive prune to get pub dates down to a 4 digit year
       rec.fields(['260']).each do |field|
-        acc << field['c'].gsub(/[^0-9,.]/, '').gsub(/[[:punct:]]/, '')[0..3].strip  unless field['c'].nil?
+        acc << field['c'].four_digit_year  unless field['c'].nil?
       end
 
       rec.fields(['264']).each do |field|
-        acc << field['c'].gsub(/[^0-9,.]/, '').gsub(/[[:punct:]]/, '')[0..3].strip  unless field['c'].nil? || field.indicator2 == '4' 
+        acc << field['c'].four_digit_year unless field['c'].nil? || field.indicator2 == '4'
+      end
+    end
+
+    to_field 'date_copyright_display' do |rec, acc|
+      rec.fields(['264']).each do |field|
+        acc << field['c'].four_digit_year  if field.indicator2 == '4'
       end
     end
 
