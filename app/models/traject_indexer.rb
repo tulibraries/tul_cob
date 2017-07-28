aa2126e762a14301a9fbb70c775e424ec7bcaba2
Jennifer Anton <jenanton01@gmail.com>
2017-07-28 15:21:53 -0400

    BL-26 Add link from creator_display_field

--- app/models/traject_indexer.rb
+++ app/models/traject_indexer.rb
@@ -230,7 +230,7 @@ end
     to_field 'title_addl_vern_display', extract_marc('210ab:246abfgnp:247abcdefgnp:740anp', :alternate_script=>:only)
 
     #Creator/contributor fields
-
+    to_field "creator", extract_marc("100abcdejlmnopqrtu:110abcdelmnopt:111acdejlnopt:700abcdejlmnopqrtu:710abcdelmnopt:711acdejlnopt", :trim_punctuation => true)
     to_field 'creator_display', extract_marc('100abcdejlmnopqrtu:110abcdelmnopt:111acdejlnopt:700abcdejlmnopqrtu:710abcdelmnopt:711acdejlnopt', :trim_punctuation => true, :alternate_script=>false)
     to_field 'creator_vern_display', extract_marc('100abcdejlmnopqrtu:110abcdelmnopt:111acdejlnopt:700abcdejlmnopqrtu:710abcdelmnopt:711acdejlnopt', :trim_punctuation => true, :alternate_script=>:only)
     #creator_facet?
