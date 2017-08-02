d398d0c457c8391507e1751c9bb24bd29e46d383
Jennifer Anton <jenanton01@gmail.com>
2017-08-02 08:54:42 -0400

    BL-23 Helper methods for displaying links and lists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,30 @@ module ApplicationHelper
     render_location(value[:value].first)
   end
 
+  def list(args)
+    content_tag :ul do
+      args[:document][args[:field]].map { |field| content_tag(:li, field, class: "list_items") }.join("<br /> ").html_safe
+    end
+  end
+
+  def get_search_params(field, query)
+    { :controller => "catalog", :action => 'index', :search_field => field, :q=> query }
+    if 'creator_display'
+      { :controller => "catalog", :action => 'index', :search_field => 'creator', :q=> query }
+    elsif 'subject_display'
+      { :controller => "catalog", :action => 'index', :search_field => 'subject', :q=> query }
+    end
+  end
+
+  def fielded_search(query, field)
+    params = get_search_params(field, query)
+    link_url = search_action_path(params)
+    link_to(query, link_url)
+  end
+
+  def list_with_links(args)
+    content_tag :ul do
+      args[:document][args[:field]].map { |field| content_tag(:li,  fielded_search(field, args[:field]), class: "list_items") }.join("<br /> ").html_safe
+    end
+  end
 end
