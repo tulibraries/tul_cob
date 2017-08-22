173cefd6dfad7d4613d6bca0e9329137867d4212
Jennifer Anton <jenanton01@gmail.com>
2017-08-22 16:00:34 -0400

    BL-26 Add root_url for links to creator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,7 +37,7 @@ module ApplicationHelper
   def browse_creator(args)
     args[:document][args[:field]].each_with_index do |name, i|
       content_tag :ul do
-      newname = link_to(name, "/?f[creator_facet][]=#{name}", class: "list_items")
+      newname = link_to(name, root_url + "/?f[creator_facet][]=#{name}", class: "list_items")
       args[:document][args[:field]][i] = newname.html_safe
       end
     end
