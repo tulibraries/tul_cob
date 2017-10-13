12e221af220dc417afe3a8f3766f9b918575bf1f
David Kinzer <dtkinzer@gmail.com>
2017-10-12 21:22:46 -0400

    Make build step more verbose.

--- lib/tasks/docker.rb
+++ lib/tasks/docker.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+# rubocop:disable BlockLength
 namespace :docker do
-  task :build do
+  task build: :config do
     server_pid = 'tmp/pids/server.pid'
     File.delete server_pid if File.exist? server_pid
     IO.popen('docker-compose build') do |io|
@@ -17,6 +18,16 @@ namespace :docker do
   task up: :build do
     print `docker-compose up -d`
     Rake::Task['docker:ps'].invoke
+  end
+
+  task :config do
+    Dir.glob('config/*')
+       .select { |p| p.match(/example$/) }
+       .each do |p|
+      src = p
+      dest = p.gsub('.example', '')
+      copy_file(src, dest) unless File.exist? dest
+    end
   end
 
   task :stop do
