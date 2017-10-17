4799d19fca39c6137bfdc9bb11ed0794ece68249
David Kinzer <dtkinzer@gmail.com>
2017-10-17 10:12:49 -0400

    apply linter fixes

--- db/migrate/20161116190713_create_searches.blacklight.rb
+++ db/migrate/20161116190713_create_searches.blacklight.rb
@@ -1,5 +1,6 @@
 # This migration comes from blacklight (originally 20140202020201)
 # frozen_string_literal: true
+
 class CreateSearches < ActiveRecord::Migration
   def self.up
     create_table :searches do |t|
