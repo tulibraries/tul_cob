ff093ef10d6196adacc8b6f9ddca030b1159b74f
sensei100 <tuh42082@temple.edu>
2017-10-18 11:50:20 -0400

    BL-146 - Edit alma_service_type method to return viewit iframe - Add availability_facet to solr schema so that it is stored

--- app/helpers/blacklight_alma_helper.rb
+++ app/helpers/blacklight_alma_helper.rb
@@ -2,12 +2,11 @@ module BlacklightAlmaHelper
 
   include BlacklightAlma::HelperBehavior
 
-def alma_service_type_for_fulfillment_url(document)
-  if (document['format'] || '').first.downcase == 'electronic'
-    'viewit'
-  else
-    'getit'
+  def alma_service_type_for_fulfillment_url(document)
+    if document['availability_facet'].include?("Online")
+      output = 'viewit'
+    else
+      output = 'getit'
+    end
   end
-end
-
 end
