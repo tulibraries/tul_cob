03f80590343adfef1228eb4b3b842ec0cd2773ce
David Kinzer <dtkinzer@gmail.com>
2017-10-19 17:41:25 -0400

    Make VCR config more lenient for now.
    We only need some very specific search cases for a tests so it doesn't
    make sense to have a tape selector that is too smart.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -130,6 +130,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.default_cassette_options = {
-    match_requests_on: [:method, :path]
+    match_requests_on: [:method]
   }
 end
