d98e75247063e4da8e0d5fb980584171546480d7
David Kinzer <dtkinzer@gmail.com>
2017-10-20 10:51:01 -0400

    Separate search engine tests.

--- spec/search_engines/primo_search_spec.rb
+++ spec/search_engines/primo_search_spec.rb
@@ -3,28 +3,26 @@
 require "rails_helper"
 
 RSpec.describe BentoSearch, type: :search_engine do
-  blacklight_se = BentoSearch.get_engine("blacklight")
+  primo_se = BentoSearch.get_engine("primo")
 
-  blacklight_search_results = VCR.use_cassette("bento_search_blacklight") do
-    blacklight_se.search("james")
+  primo_search_results = VCR.use_cassette("bento_search_primo") do
+    primo_se.search("james")
   end
-
 
   let(:expected_fields) { Rails.configuration.bento["expected_fields"]
     .map { |f| f.to_sym } }
 
-  describe "Bento Blacklight Search Engine" do
-    let (:item) { blacklight_search_results[0] }
-
+  describe "Bento  Primo Search Engine" do
+    let (:item) { primo_search_results[0] }
     it "has all the expected fields" do
       expected_fields.each do |field|
-        expect(item.send field).not_to be_nil, "expect #{field} to be set."
+        expect(item.send field).not_to be_nil, "expect #{field.inspect} to be set."
       end
     end
 
     it "uses TulStandardDecorator" do
       expect(item.decorator).to eq("TulDecorator")
     end
-
   end
+
 end
