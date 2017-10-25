510b6a944c4a30b55c81454e7ee41a62080f10dc
Chad Nelson <chad.nelson@temple.edu>
2017-10-25 09:26:53 -0400

    Add omniauth-shibboleth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,8 +248,15 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :alma
-
+  config.omniauth :shibboleth, {
+                                uid_field: 'employeeNumber',
+                                info_fields: {
+                                  email: 'mail', 
+                                  name: 'cn', 
+                                  last_name: 'sn', 
+                                  first_name: 'givenName'
+                                } 
+                               }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
