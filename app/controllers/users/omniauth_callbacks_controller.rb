20c259ef07df0ca25481e738e8ffbc6c6164b245
David Kinzer <dtkinzer@gmail.com>
2017-10-25 13:46:04 -0400

    Debugging setup.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
   end
 
+  def shibboleth
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    set_flash_message(:notice, :success, :kind => "Alma") if is_navigational_format?
+    sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+  end
+
   def failure
     redirect_to root_path
   end
