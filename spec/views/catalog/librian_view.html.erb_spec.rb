71aaed77e6ba8f2040a3e11c6b5dab5b143488ae
David Kinzer <dtkinzer@gmail.com>
2017-11-14 14:00:44 -0500

    Fix librarian_view.html.erb_spec.

--- spec/views/catalog/librian_view.html.erb_spec.rb
+++ spec/views/catalog/librian_view.html.erb_spec.rb
@@ -4,9 +4,21 @@ require "rails_helper"
 
 RSpec.describe "catalog/librarian_view.html.erb", type: :view do
 
-  it "displays the correct title name"  do
-    assign(:marc_view, [])
-    render
-    expect(rendered).to match(/Staff View/)
+  context "Empty mar_view " do
+    it "displays the correct title name"  do
+      assign(:marc_view, [])
+      render
+      expect(rendered).to match(/Staff View/)
+    end
   end
+
+  context "Non empty @marc_view" do
+    it "displays the correct title name"  do
+      assign(:marc_view, ["Hello World"])
+      render
+      expect(rendered).to match(/Staff View/)
+      expect(rendered).to match(/Hello World/)
+    end
+  end
+
 end
