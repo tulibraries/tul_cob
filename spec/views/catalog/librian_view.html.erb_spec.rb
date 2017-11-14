d94e0f3463e0866cbe3ed9d8d679078e58bde69a
David Kinzer <dtkinzer@gmail.com>
2017-11-14 13:57:15 -0500

    Fix template test.

--- spec/views/catalog/librian_view.html.erb_spec.rb
+++ spec/views/catalog/librian_view.html.erb_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe "catalog/librarian_view.html.erb", type: :view do
 
   it "displays the correct title name"  do
+    assign(:marc_view, [])
     render
     expect(rendered).to match(/Staff View/)
   end
