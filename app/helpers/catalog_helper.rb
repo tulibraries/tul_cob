0a5ca9bbd1314bc5e9dda9399a1108e9896c9cbd
Chad Nelson <chad.nelson@temple.edu>
2017-11-17 16:50:19 -0500

    [WIP] add book covers from google books via javascript

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -3,4 +3,26 @@
 module CatalogHelper
   include Blacklight::CatalogHelperBehavior
   include BlacklightAlma::CatalogOverride
+
+  def thumbnail_classes(document)
+    classes = %w[thumbnail col-sm-3 col-lg-2]
+    document[:format].each do |format|
+      classes << "#{format.downcase}-format"
+    end
+    classes.join " "
+  end
+
+  def isbn_data_attribute(document)
+    value = document.fetch(:isbn_display, "")
+    return value if value.empty?
+    # Get the first ISBN and strip non-numerics
+    "data-isbn=#{value.first.gsub(/\D/,'')}"
+  end
+
+  def lccn_data_attribute(document)
+    value = document.fetch(:lccn_display, "")
+    return value if value.empty?
+    # Get the first ISSN and strip non-numerics
+    "data-lccn=#{value.first.gsub(/\D/,'')}"
+  end
 end
---
+++ spec/helpers/catalog_helper_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the CatalogHelper. For example:
+#
+# describe CatalogHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe CatalogHelper, type: :helper do
+  describe "#isbn_data_attribute" do
+    context "document contains an isbn" do
+      let(:document) { {isbn_display: ["123456789"]} }
+      it 'returns the data-isbn string' do
+        expect(isbn_data_attribute(document)).to eql "data-isbn=123456789"
+      end
+    end
+
+    context "document contains multiple isbn" do
+      let(:document) { {isbn_display: ["23445667890","123456789"]} }
+      it 'returns the data-isbn string' do
+        expect(isbn_data_attribute(document)).to eql "data-isbn=23445667890"
+      end
+    end
+
+
+    context "document contains an isbn" do
+      let(:document) { {} }
+      it 'returns the data-isbn string' do
+        expect(isbn_data_attribute(document)).to eql ""
+      end
+    end
+  end
+end
