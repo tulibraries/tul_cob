37b888bf7c8aa67721b7d2c883752c3f8574e0ae
Chad Nelson <chad.nelson@temple.edu>
2017-11-29 16:35:19 -0500

    Tests, and use routh helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,4 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action do
-    binding.pry
-    blacklight_config.add_nav_action(:library_account, partial: "#{root_path}/users/account_link")
-  end
 end
---
+++ spec/features/basic_page_elements_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+
+RSpec.feature "Basic Page Elements", type: :feature do
+
+  feature "Check the home page for nav elements when anonymous" do
+    scenario "Go to the home page" do
+      visit "/"
+      within(".navbar-right") do
+        expect(page).to have_text "Bookmarks"
+        expect(page).to have_text "History"
+        expect(page).to have_text "Login"
+        expect(page).to_not have_text "Library Account"
+      end
+    end
+
+  end
+
+
+
+  feature "Check for home page features when logged in" do
+
+    before do
+      DatabaseCleaner.clean
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
+
+    end
+
+    scenario "Go to the home page" do
+      visit "/"
+      within(".navbar-right") do
+        expect(page).to_not have_text "Login"
+        expect(page).to have_text "Library Account"
+        expect(page).to have_text "Log Out"
+      end
+    end
+
+    after(:all) do
+      DatabaseCleaner.clean
+      Warden.test_reset!
+    end
+
+  end
+end
