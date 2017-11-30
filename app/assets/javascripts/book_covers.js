b80b5538d3e264ee1efcb300726fd40e86633a91
Steven Ng <steven.ng@temple.edu>
2017-11-30 17:08:11 -0500

    Renders book covers

--- app/assets/javascripts/book_covers.js
+++ app/assets/javascripts/book_covers.js
@@ -1,24 +1,38 @@
 $(document).on('turbolinks:load', function() {
-  $.ajax(
-    {
-      url: "https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:9781935410812,LCCN:2015048918",
-      dataType: "jsonp",
-      jsonp: "callback"
-    }
-  ).done(
-    function(response) {
-      for (var bib in response) {
-        b = response[bib];
-        if(b.hasOwnProperty("thumbnail_url")) {
-          type, identifier = b.bib_key.split(":")[1];
-          $('[data-' + type + '=' + identifier + ']').html(thumbnail_img )
-        }
+  thumbnail_list = $(".thumbnail");
+  thumbnail_list.each(function(index, thumbnail) {
+    console.log (index, thumbnail);
+    lccn = $(thumbnail).attr('data-lccn');
+    isbn = $(thumbnail).attr('data-isbn');
+    console.log (index, lccn);
+    console.log (index, isbn);
+    //thumbnail_url = "https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:9781935410812,LCCN:2015048918";
+    thumbnail_url = "https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:" + isbn + ",LCCN:" + String(lccn);
+    console.log(index, "Thumbnail_url", thumbnail_url);
+    
+    $.ajax(
+      {
+        url: thumbnail_url,
+        dataType: "jsonp",
+        jsonp: "callback"
       }
-    }
+      ).done(
+        function(response) {
+          for (var bib in response) {
+            b = response[bib];
+            if(b.hasOwnProperty("thumbnail_url")) {
+              type = b.bib_key.split(":")[0];
+              identifier = b.bib_key.split(":")[1];
+              $('[data-' + type.toLowerCase() + '=' + identifier + ']').html(thumbnail_img(b) );
+            }
+          }
+        }
+      )
+   }
   )
  }
 )
 
 function thumbnail_img(gb_result) {
-  "<img src='" + gb_result.thumbnail_url +  "' />"
+  return "<img src='" + gb_result.thumbnail_url +  "' />"
 }
