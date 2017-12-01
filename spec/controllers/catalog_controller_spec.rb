2ecacb8c91cf059203f9653a1fcaba3565ab2812
Steven Ng <steven.ng@temple.edu>
2017-12-01 13:33:10 -0500

    Merge catalog controller specs

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe CatalogController, type: :controller do
+
+
+  let(:doc_id) { "991012041239703811" }
+  let(:mock_response) { instance_double(Blacklight::Solr::Response) }
+  let(:mock_document) { instance_double(SolrDocument) }
+
+  describe "show action" do
+    it "gets the staff_view_path" do
+      get :show, params: { id: doc_id }
+      expect(staff_view_path).to eq("/catalog/#{doc_id}/staff_view")
+    end
+
+    it "is properly routed for staff_view" do
+      expect(get: "/catalog/:id/staff_view").to route_to(controller: "catalog", action: "librarian_view", id: ":id")
+    end
+  end
+
   describe "GET index as json" do
     render_views
     before do
@@ -22,20 +39,7 @@ RSpec.describe CatalogController, type: :controller do
           expect(docs_keys).to include key
         end
       end
-
-
-  let(:doc_id) { "991012041239703811" }
-  let(:mock_response) { instance_double(Blacklight::Solr::Response) }
-  let(:mock_document) { instance_double(SolrDocument) }
-
-  describe "show action" do
-    it "gets the staff_view_path" do
-      get :show, params: { id: doc_id }
-      expect(staff_view_path).to eq("/catalog/#{doc_id}/staff_view")
-    end
-
-    it "is properly routed for staff_view" do
-      expect(get: "/catalog/:id/staff_view").to route_to(controller: "catalog", action: "librarian_view", id: ":id")
     end
   end
+
 end
