f3c9b6d51b0588a2ed2d1d5f9f3f0012386f66c8
David Kinzer <dtkinzer@gmail.com>
2017-12-05 15:11:46 -0500

    Remove modal button and add back link. (#163)
    REF BL-176

--- spec/views/catalog/librian_view.html.erb_spec.rb
+++ spec/views/catalog/librian_view.html.erb_spec.rb
@@ -3,8 +3,14 @@
 require "rails_helper"
 
 RSpec.describe "catalog/librarian_view.html.erb", type: :view do
-  it "displays the correct title name"  do
+  before(:each) do
+    @document = SolrDocument.new(id: 1)
+  end
+
+  it "renders as expected"  do
     render
     expect(rendered).to match(/Staff View<\/h3>/)
+    expect(rendered).to match(/Back to Main View/)
+    expect(rendered).to_not match(/×<\/button>/)
   end
 end
