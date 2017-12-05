8ef687b6b8622bf9bdf85a8fa3217de185cb9221
sensei100 <tuh42082@temple.edu>
2017-12-05 12:30:27 -0500

    Remove specs for now.  Code saved to revisit during an off week.

--- spec/features/availability_response_spec.rb
+++ spec/features/availability_response_spec.rb
@@ -1,53 +1,11 @@
 # frozen_string_literal: true
 require "rails_helper"
 require "yaml"
-require "selenium-webdriver"
 
-describe Alma::AvailabilityResponse, js: true  do
+describe Alma::AvailabilityResponse do
 
   before(:all) do
-    Capybara.javascript_driver = :headless_chrome
-
-    Capybara.register_driver :headless_chrome do |app|
-      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-        chromeOptions: { args: %w[headless disable-gpu] }
-  )
-
-    Capybara::Selenium::Driver.new(app,
-                                 browser: :chrome,
-                                 desired_capabilities: capabilities)
-    end
-
     Alma.configure
-  end
-
-  feature "Availability Buttons by status" do
-    scenario "Available items should have a green button" do
-      visit "/"
-      fill_in "q", with: "Academic freedom"
-      click_button "Search"
-      within(".document-position-0") do
-        expect(page).to have_css(".btn-success")
-      end
-    end
-
-    scenario "Items with only a check_holdings status should not have a button" do
-      visit "/"
-      fill_in "q", with: "Declassified documents quarterly catalog"
-      click_button "Search"
-      within(".document-position-0") do
-        expect(page).to have_no_css(".btn-success" || ".btn-warning")
-      end
-    end
-
-    scenario "Items with an unavailable status should have a yellow button" do
-      visit "/"
-      fill_in "q", with: "Vital dust : the origin and evolution of life on earth"
-      click_button "Search"
-      within(".document-position-0") do
-        expect(page).to have_css(".btn-warning")
-      end
-    end
   end
 
   describe "availability attribute" do
