2ec7fb33f1e51a6f16002d62b9ecbc747aa74d3f
sensei100 <tuh42082@temple.edu>
2017-12-20 12:04:36 -0500

    BL-230 Remove trailing punctuation from creator facet and displays

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,11 +30,12 @@ module ApplicationHelper
   def browse_creator(args)
     creator = args[:document][args[:field]]
     creator.map do |name|
-      linked_subfield = name.split("|").first.sub(/ *[ ,.\/;:] *\Z/, "")
-      newname = link_to(linked_subfield, root_url + "/?f[creator_facet][]=#{linked_subfield}").html_safe
-      plain_text_subfields = name.split("|").second.sub(/ *[ ,.\/;:] *\Z/, "")
+      linked_subfields = name.split("|").first.sub(/ *[ ,\/;:] *\Z/, '').sub(/( *[[:word:]]{3,})\. *\Z/, '\1')
+      newname = link_to(linked_subfields, root_url + "/?f[creator_facet][]=#{linked_subfields}").html_safe
+      plain_text_subfields = name.split("|").second
       creator = newname
       if plain_text_subfields.present?
+        plain_text_subfields = plain_text_subfields.sub(/ *[ ,.\/;:] *\Z/, '')
         creator = newname + " " + plain_text_subfields
       end
       creator
@@ -45,7 +46,7 @@ module ApplicationHelper
     creator = args[:document][args[:field]]
     creator.map do |name|
       plain_text_subfields = name.gsub("|", " ")
-      creator = plain_text_subfields
+      creator = plain_text_subfields.sub(/ *[ ,\/;:] *\Z/, '').sub(/( *[[:word:]]{3,})\. *\Z/, '\1')
     end
     creator
   end
