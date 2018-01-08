3463253007834c732d8163d8e183e0806d07384e
sensei100 <tuh42082@temple.edu>
2018-01-08 11:03:14 -0500

    Set up default configurations in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -102,17 +102,17 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  if config.files_to_run.one?
+  #if config.files_to_run.one?
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+    #config.default_formatter = 'doc'
+  #end
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  #config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
