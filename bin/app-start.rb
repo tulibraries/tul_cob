173ea1e7d2f6d33418ee9bb922d48e9e7fc6d787
David Kinzer <dtkinzer@gmail.com>
2018-01-11 15:28:12 -0500

    Do not block on rails server up.

--- bin/app-start.rb
+++ bin/app-start.rb
@@ -13,10 +13,14 @@ end
 server_pid = "tmp/pids/server.pid"
 File.delete server_pid if File.exist? server_pid
 
-# Start rails app
-`rails s -p 3000 -b '0.0.0.0'`
-
-# Provision with test data.
+# Start rails app but do not block the rest of the script.
 `rails db:migrate`
+exec("rails s -p 3000 -b '0.0.0.0'") if fork == nil
+
+# Next, provision with test data.
+# (If we do this first it works, but site will be blank until rails app loads).
 `rake fortytu:solr:load_fixtures`
 `rake ingest`
+
+# Wait for rails server to shutdown before stopping the process.
+Process.wait
