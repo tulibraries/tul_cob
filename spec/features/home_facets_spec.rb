67b08771e04467bbce20fe51871f55a7f9106d1a
sensei100 <tuh42082@temple.edu>
2018-01-15 15:53:36 -0500

    Remove extra whitespace

--- spec/features/home_facets_spec.rb
+++ spec/features/home_facets_spec.rb
@@ -6,9 +6,9 @@ RSpec.feature "Advanced Search" do
 
   describe "Facets rendered" do
     it "Only a subset of all the facets render on the homepage" do
-      visit  "/catalog"
+      visit "/catalog"
       home_facets = page.all(".facet_limit").length
-      visit  "/catalog?search_field=all_fields"
+      visit "/catalog?search_field=all_fields"
       search_facets = page.all(".facet_limit").length
       expect(home_facets).to be < search_facets
     end
