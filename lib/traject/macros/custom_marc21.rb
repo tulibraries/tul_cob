bd41d9af3d060c524a8fe6018f885a053b0ccd0d
sensei100 <tuh42082@temple.edu>
2018-01-18 12:09:26 -0500

    Add specs for creator_name_trim_punctuation and fix rubocop offenses

--- lib/traject/macros/custom_marc21.rb
+++ lib/traject/macros/custom_marc21.rb
@@ -1,22 +1,34 @@
-require 'traject/marc_extractor'
-require 'traject/translation_map'
-require 'traject/util'
-require 'base64'
-require 'json'
-require 'marc/fastxmlwriter'
+# frozen_string_literal: true
+
+require "traject/marc_extractor"
+require "traject/translation_map"
+require "traject/util"
+require "base64"
+require "json"
+require "marc/fastxmlwriter"
 
 module Traject::Macros
   # Some of these may be generic for any MARC, but we haven't done
   # the analytical work to think it through, some of this is
   # def specific to Marc21.
   module Marc21
+    # Trims punctuation mostly from end, and occasionally from beginning
+    # of string. Not nearly as complex logic as SolrMarc's version, just
+    # pretty simple.
+    #
+    # Removes
+    # * trailing: comma, slash, semicolon, colon (possibly preceded and followed by whitespace)
+    # * trailing period if it is preceded by at least three letters (possibly preceded and followed by whitespace)
+    # * single square bracket characters if they are the start and/or end
+    #   chars and there are no internal square brackets.
+    #
+    # Returns altered string, doesn't change original arg.
     def self.trim_punctuation(str)
-
       # If something went wrong and we got a nil, just return it
       return str unless str
 
       # trailing: comma, slash, semicolon, colon (possibly preceded and followed by whitespace)
-      str = str.sub(/ *[ ,\/;:] *\Z/, '')
+      str = str.sub(/ *[ ,\/;:] *\Z/, "")
 
       # trailing period if it is preceded by at least three letters (possibly preceded and followed by whitespace)
       str = str.sub(/( *[[:word:]]{3,})\. *\Z/, '\1')
