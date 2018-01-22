ebd83ea19c0dedae86fdbe73fa5eb4f8c1efe70d
sensei100 <tuh42082@temple.edu>
2018-01-22 15:37:55 -0500

    Update spec for views/catalog/_show_default.html.erb to include format

--- spec/views/catalog/_show_default.html.erb._spec.rb
+++ spec/views/catalog/_show_default.html.erb._spec.rb
@@ -12,7 +12,9 @@ RSpec.describe "catalog/_show_default.html.erb", type: :view do
     @config = Blacklight::Configuration.new {}
     @context = Blacklight::Configuration::Context.new(controller)
     @document = SolrDocument.new(id: 1)
+    @document[:format] = []
     without_partial_double_verification do
+      allow(view).to receive(:blacklight_config).and_return(@config)
       allow(view).to receive(:blacklight_config).and_return(@config)
       allow(view).to receive(:blacklight_configuration_context).and_return(@context)
       allow(view).to receive(:staff_view_path).and_return("/catalog/1/staff_view")
