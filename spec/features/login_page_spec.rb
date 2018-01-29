0c4a702392503bafb3311de876e248dc4c4f0d6d
David Kinzer <dtkinzer@gmail.com>
2018-01-29 14:21:24 -0500

    Update sign-in form.
    REF BL-271

---
+++ spec/features/login_page_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Login Page" do
+  
+  it "has a My Library Account link" do
+    visit "/"
+    expect(page).to have_link("My Library Account")
+  end
+
+
+  it "does not have the typical login form" do
+    visit "/users/sign_in"
+    expect(page).not_to have_button("Log inh")
+  end
+
+
+  it "has a link to shibboleth" do
+    visit "/users/sign_in"
+    expect(page).to have_link("Sign in with your AccessNet Username")
+  end
+end
