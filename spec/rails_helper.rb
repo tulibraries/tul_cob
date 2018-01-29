7cc5f097b56730e8a5720dc0c8e54aa427741aa6
David Kinzer <dtkinzer@gmail.com>
2018-01-29 13:31:51 -0500

    Update max wait to 5 seconds.
    Doubles the amount of time a page is given to load to avoid timing
    issues.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ require "capybara/rspec"
 require "capybara/rails"
 
 # Add additional requires below this line. Rails is not loaded until this point!
+Capybara.default_max_wait_time = 5
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
