21250faf959e5fb54c3777943d2173d18b56fc75
David Kinzer <dtkinzer@gmail.com>
2018-01-29 12:30:19 -0500

    Make spec less brittle to timing issues.
    By adding adding a `have_text` expectation we force capybara to retry
    for up to 2 seconds (by default).  This makes sure that we get the
      number of facets on the rendered search page instead of potentially on
      the front page where it will be 3 thus creating a false negative when
      3 is not < 3.

--- spec/features/home_facets_spec.rb
+++ spec/features/home_facets_spec.rb
@@ -8,7 +8,10 @@ RSpec.feature "Advanced Search" do
     it "Only a subset of all the facets render on the homepage" do
       visit "/catalog"
       home_facets = page.all(".facet_limit").length
-      visit "/catalog?search_field=all_fields"
+
+      visit "/catalog?search_field=all_fields&q=test"
+      expect(page).to have_text("You searched for:")
+
       search_facets = page.all(".facet_limit").length
       expect(home_facets).to be < search_facets
     end
