82fa3558561dc1d7631ac7c6f38d3e357ae28e3f
Steven Ng <steven.ng@temple.edu>
2018-01-30 12:18:30 -0500

    List in descending chronological order (#203)
    * List in descending chronological order
    
    * Intermediate commit - Extend sorting
    
    - Breaks out sort function into separate method
    - Extends sort to end date and title
    
    * Fine tune sort and place make it a separate method
    
    * Fix tertiary sort
    
    - Teritary sort on title instead of ID and title
    
    * Remove unused fixture file
    
    * Refactor sort electronic url (#221)
    
    * Refactor sort electronic url
    
    * Fixes bug where an empty accumulater gets clobbered.
    * Removes unused fixtures.
    * Adds  smoke test to verify code sorts as expected.
    * Adds test against bug
    
    * Update solr-wrapper download url.
    
    * Update solr zip url via params.
    
    * Update md5sum url.
    
    * Add md5url not md5sum.
    
    * Use md5sum instead of URL to md5sum file.
    
    Even though the solr_wrapper docs say the mad5sum takes a URL to an
    md5sum or a file path, it actual fails in those cases.  Looking at the
    code it will work fine if we pass the actual md5sum here (I think).
    
    I made a PR against solr_wrapper to fix the issue I found:
    https://github.com/cbeer/solr_wrapper/pull/114
    
    * Use valid checksum
    
    * Rescue from bad record on sort.

--- lib/traject/macros/custom.rb
+++ lib/traject/macros/custom.rb
@@ -116,19 +116,11 @@ module Traject
       end
 
       def extract_electronic_resource
-        lambda do |rec, acc|
-          resources = []
+        lambda do |rec, acc, context|
           rec.fields("PRT").each do |f|
-            resources << [f["a"], f["c"], f["g"]]
+            selected_subfields = [f["a"], f["c"], f["g"]].compact.join("|")
+            acc << selected_subfields
           end
-          # Sort on availability
-          unless resources.empty?
-            resources.sort_by! { |r|  (r[2] || "9999").scan(/\d+/).first.to_i }
-            resources.each do |res|
-              acc << res.compact.join("|")
-            end
-          end
-
           # Short circuit if PRT field present.
           if !rec.fields("PRT").empty?
             return acc
@@ -141,6 +133,26 @@ module Traject
                 acc << [label, f["u"]].compact.join("|")
               end
             end
+          end
+        end
+      end
+
+      def sort_electronic_resource!
+        lambda do |rec, acc, context|
+          begin
+            acc.sort_by! { |r|
+              subfields = r.split("|")
+              available = /Available from (\d+).* until (\d+)/.match(subfields.last)
+              title = subfields[1]
+              subtitle = subfields[2]
+              unless available
+                available = []
+              end
+              [available[1] || "9999", available[2] || "9999", title, subtitle || ""]
+            }.reverse!
+          rescue
+            logger.error("Failed `sort_electronic_resource!` on sorting #{rec}")
+            acc
           end
         end
       end
