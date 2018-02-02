bc633952c7f09f52247af0d4f40b428c2fda80ed
sensei100 <tuh42082@temple.edu>
2018-02-02 13:22:09 -0500

    Remove icons from book cover images

--- app/assets/javascripts/book_covers.js
+++ app/assets/javascripts/book_covers.js
@@ -24,18 +24,9 @@ $(document).on('turbolinks:load', function() {
         if(b.hasOwnProperty("thumbnail_url")) {
           type = b.bib_key.split(":")[0];
           identifier = b.bib_key.split(":")[1];
-          titles = $(".thumbnail")
-          for(ind in titles)
-          title = titles[ind]
-          $(title).parents().find(".index_title a").text();
-          console.log(title);
-          $('[data-' + type.toLowerCase() + '=' + identifier + ']').html(thumbnail_img(b));
+          $('[data-' + type.toLowerCase() + '=' + identifier + '] .book_cover').attr("src" , b.thumbnail_url).removeClass("invisible").addClass("google-image").siblings().remove();
         }
       }
     }
   )
 })
-
-function thumbnail_img(gb_result, title) {
-  return "<img src='" + gb_result.thumbnail_url +  "' alt='' />"
-}
