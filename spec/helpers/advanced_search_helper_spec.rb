ac599e4ee758aeb5f5444c2f40929bc7f6b79ba1
sensei100 <tuh42082@temple.edu>
2018-03-29 16:37:13 -0400

    Remove count tests since they are being depricated

--- spec/helpers/advanced_search_helper_spec.rb
+++ spec/helpers/advanced_search_helper_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-module BlacklightAdvancedSearch
-  module RenderConstraintsOverride
-  end
-end
 
 RSpec.describe BlacklightAdvancedSearch::RenderConstraintsOverride, type: :helper do
   describe "#guided_search" do
@@ -12,46 +8,7 @@ RSpec.describe BlacklightAdvancedSearch::RenderConstraintsOverride, type: :helpe
     example "empty search fields" do
       expect(helper.guided_search).to be_empty
     end
-
-    example "one search field" do
-      params = ActionController::Parameters.new(
-        f1: "all_fields",
-        search_field: "advanced",
-        q1: "james"
-      )
-      expect(helper.guided_search(params).count).to eq(1)
-    end
-
-    example "two search fields" do
-      params = ActionController::Parameters.new(
-        f1: "all_fields",
-        search_field: "advanced",
-        q1: "james",
-        f2: "all_fields",
-        q2: "james",
-        op2: "OR"
-      )
-      expect(helper.guided_search(params).count).to eq(2)
-    end
-
-    it "can handle more than the number of fields defined (current is 3)" do
-      params = ActionController::Parameters.new(
-        f1: "all_fields",
-        search_field: "advanced",
-        q1: "james",
-        f2: "all_fields",
-        q2: "john",
-        op2: "OR",
-        f3: "all_fields",
-        q3: "david",
-        op3: "OR",
-        f4: "all_fields",
-        q4: "summer",
-        op4: "OR"
-      )
-      expect(helper.guided_search(params).count).to eq(4)
-    end
-  end
+   end
 
   describe ".op_row_default" do
     example "default" do
