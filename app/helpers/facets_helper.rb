c45a086e9739917baf40dcf8c1610de6e2d75837
David Kinzer <dtkinzer@gmail.com>
2018-04-17 09:20:56 -0400

    Add resource icons and book images.
    REF BL-379
    
    Associates icons with all primo central resource types and enables
    isbn/lccn lookup of primo central book covers if available.

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -13,6 +13,7 @@ module FacetsHelper
 
   def render_facet_value(facet_field, item, options = {})
     path = path_for_facet(facet_field, item)
+
     content_tag(:span, class: "facet-label") do
       link_to_unless(options[:suppress_link], facet_display_value(facet_field, item), path, class: "facet_select facet_" + item.value.downcase.parameterize.underscore)
     end + render_facet_count(item.hits)
--- app/models/blacklight/primo_central/document.rb
+++ app/models/blacklight/primo_central/document.rb
@@ -9,14 +9,15 @@ module Blacklight::PrimoCentral::Document
   def initialize(doc, req = nil)
     format = doc["@TYPE"] || doc["type"]
     doc["type"] = [format]
-    url = doc["delivery"]["GetIt1"]
-      .first["links"]
-      .first["link"]
+    doc["format"] = [format]
+    @url = url(doc)
+    @url_query = url_query
 
     # dots do not get URL encoded and break links to articles.
     doc[:pnxId] = doc[:pnxId].gsub(".", "-dot-") if doc[:pnxId]
-
-    doc["link"] = url
+    doc["link"] = @url
+    doc["isbn"] = isbn unless doc["isbn"]
+    doc["lccn"] = lccn unless doc["lccn"]
 
     solr_to_primo_keys.each do |solr_key, primo_key|
       doc[solr_key] = doc[primo_key] || FIELD_DEFAULT_VALUES[primo_key]
@@ -28,8 +29,29 @@ module Blacklight::PrimoCentral::Document
   private
 
     FIELD_DEFAULT_VALUES = {
-      "isbn" => "",
-      "issn" => "",
-      "lccn" => "",
+      "isbn" => [],
+      "issn" => [],
+      "lccn" => [],
     }
+
+    def url(doc)
+      doc["delivery"]["GetIt1"].first["links"].first["link"]
+    end
+
+    def url_query
+      query = URI.parse(@url).query
+      if (query)
+        CGI.parse(query)
+      else
+        {}
+      end
+    end
+
+    def isbn
+      @url_query["rft.isbn"]
+    end
+
+    def lccn
+      @url_query["rtf.lccn"]
+    end
 end
