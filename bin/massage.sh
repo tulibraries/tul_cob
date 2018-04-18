55f1d1582074ecfbaa26a8b4ed59554aa3e2e32a
Chad Nelson <chadbnelson@gmail.com>
2018-04-18 10:47:27 -0400

    Stupid bash tricks (#336)

--- bin/massage.sh
+++ bin/massage.sh
@@ -2,6 +2,14 @@
 
 # Takes single argument - path to the xml files you want to transform
 
+case "$OSTYPE" in
+  darwin*)  SEDTYPE="BSD" ;; 
+  bsd*)     SEDTYPE="BSD" ;;
+  linux*)   SEDTYPE="LINUX" ;;
+  *)        SEDTYPE="LINUX" ;;
+esac
+
+
 PATH_TO_MARCXMLS=$1
 
 if  [ -f /proc/cpufinfo ]; then
@@ -10,6 +18,12 @@ else
   NUM_PROCESSES=1
 fi
 
-find $PATH_TO_MARCXMLS -name "*.xml"| xargs -t -n 1 -P $NUM_PROCESSES sed -i '' "s~<collection><record>~<collection xmlns=\"http://www.loc.gov/MARC21/slim\"><record>~"
+if [[ $SEDTYPE == "BSD" ]]; then
+  find $PATH_TO_MARCXMLS -name "*.xml"| xargs -t -n 1 -P $NUM_PROCESSES sed -i '' "s~<collection><record>~<collection xmlns=\"http://www.loc.gov/MARC21/slim\"><record>~"
+fi
+
+if [[ $SEDTYPE == "LINUX" ]]; then
+  find $PATH_TO_MARCXMLS -name "*.xml"| xargs -t -n 1 -P $NUM_PROCESSES sed -i "s~<collection><record>~<collection xmlns=\"http://www.loc.gov/MARC21/slim\"><record>~"
+fi
 
 exit $?
