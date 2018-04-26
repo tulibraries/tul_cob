84e2fd74378bf1f9c080acd5ec84fa390591d8ec
David Kinzer <dtkinzer@gmail.com>
2018-04-26 12:13:00 -0400

    Correct test wording.

--- spec/views/primo_central/_online.html.erb._spec.rb
+++ spec/views/primo_central/_online.html.erb._spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe "primo_central/_online.html.erb", type: :view do
-  it "Creates the expected tStaff View link" do
+  it "adds the expected parameter to the iframe link" do
     view.instance_variable_set("@document", "link" => "")
     expect(view.render("online")).to match(/is_new_ui=true/)
   end
