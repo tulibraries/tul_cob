bef6a3ef3891e0a98891f99cac46dcbcb1c52702
David Kinzer <dtkinzer@gmail.com>
2018-04-27 10:50:35 -0400

    Filter out query values equal to [""]. (#353)
    * Filter out query values equal to [""].
    
    REF BL-407
    
    Sometimes we grab values from the query strings of the document link.
    Some query values end up being [""].  These need to be filtered out or
    they end up making field values appear with blank values.

---
+++ spec/models/primo_central_document_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PrimoCentralDocument, type: :model do
+  let (:subject) { PrimoCentralDocument.new(doc, nil) }
+
+  context "document direct_link contains rft.isbn = ''" do
+    let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+      delivery: {
+        GetIt1: [{
+          "links" => [{ "link" => "http://foobar.com?rft.isbn=" }],
+        }] }) }
+    it "sets @doc['isbn'] to nil" do
+      expect(subject["isbn"]).to be_nil
+    end
+  end
+
+  context "document direc_link contains a regular value" do
+    let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+      delivery: {
+        GetIt1: [{
+          "links" => [{ "link" => "http://foobar.com?rft.isbn=a" }],
+        }] }) }
+    it "sets @doc['isbn'] to [a]" do
+      expect(subject["isbn"]).to eq(["a"])
+    end
+  end
+end
