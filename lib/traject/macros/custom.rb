0dc90adf8b60bfd77bf8db4e831194dde174791c
Jen <tuh42082@temple.edu>
2018-05-01 11:09:23 -0400

    Try force_encoding method for genre_facet (#361)

--- lib/traject/macros/custom.rb
+++ lib/traject/macros/custom.rb
@@ -254,7 +254,7 @@ module Traject
           MarcExtractor.cached("600v:610v:611v:630v:648v:650v:651v:655av:647v").collect_matching_lines(rec) do |field, spec, extractor|
             genre = extractor.collect_subfields(field, spec).first
             unless genre.nil?
-              unless GENRE_STOP_WORDS.match(genre.unicode_normalize)
+              unless GENRE_STOP_WORDS.match(genre.force_encoding(Encoding::UTF_8).unicode_normalize)
                 acc << genre.gsub(/[^[:alnum:])]*$/, "")
               end
             end
