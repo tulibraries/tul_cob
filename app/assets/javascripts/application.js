7aee44a478f88ee64dc6f27969a50295420a1edd
David Kinzer <dtkinzer@gmail.com>
2018-06-12 17:26:42 -0400

    Do not load all article iframes at once.
    REF #BL-482
    
    Bypass a bug on the Ex-Libres server that returns the wrong content when
    multiple iframes are reloaded simultaneously by not loading iframes
    simultaneously.
    
    The iframe gets loaded on a button click event.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -117,3 +117,17 @@ function deselectallchecks() {
 	    }
 	}
 }
+
+function loadArticleIframe(id) {
+  var element = $(id)
+  var url = element.attr("data-iframe-url")
+
+  if (element.attr("processed") == undefined) {
+    element.attr("processed", true);
+    $("<iframe>", {
+      src: url,
+      "class": "bl_alma_iframe",
+      id: 'iframe-' + id,
+    }).appendTo(id);
+  }
+}
