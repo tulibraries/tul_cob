35c8867b3b4f21a76eed7f8eeaf5ba957f7e331c
Chad Nelson <chadbnelson@gmail.com>
2018-06-13 10:53:47 -0400

    No lowercase booleans (#426)
    * Don't treat lowercase and or not as boolean commands
    
    * Adds a test for handling lowercase booleans
    
    * Actually commit the :rubocop: changes why don't you
    
    * Remove focus

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -43,4 +43,13 @@ RSpec.describe CatalogController, type: :controller do
     end
   end
 
+  describe "using lower case boolen operators in normal search" do
+    render_views
+    let(:uppercase_and) { JSON.parse(get(:index, params: { q: "race AND education" }, format: :json).body)["response"]["pages"]["total_count"] }
+    let(:lowercase_and) { JSON.parse(get(:index, params: { q: "race and education" }, format: :json).body)["response"]["pages"]["total_count"] }
+
+    it "returns more results that using uppercase boolean" do
+      expect(lowercase_and).to be > uppercase_and
+    end
+  end
 end
