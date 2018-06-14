4468e31e426a49f34febf1ba641d2f7578c7c391
Chad Nelson <chad.nelson@temple.edu>
2018-06-14 17:30:16 -0400

    Remove helpers methods hanfdled by alma gem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,28 +7,9 @@ module UsersHelper
     DateTime.parse(date).strftime("%m/%d/%Y")
   end
 
-  def total_holds(holds)
-    holds.list.size
-  end
-
-  def total_loans(loans)
-    loans.list.size
-  end
-
-  def is_overdue(status)
-    status == "Overdue" ? status : ""
-  end
-
-  def is_not_renewable?(loan)
-    # Alma API returns string true or false.
-    # If renewable attribute exists and has value of 'false', return true,
-    # If renewable attribute doesn't exist or has value 'true' return false
-    loan.renewable.eql? "false"
-  end
-
   def loan_options(loan)
     options = { class: "form-check-input" }
-    options[:disabled] = true if is_not_renewable?(loan)
+    options[:disabled] = true unless loan.renewable?
     options
   end
 end
