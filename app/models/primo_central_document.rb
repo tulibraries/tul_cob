957db5acbf2ca769ba7dfac9bc1ac25e0a8113ff
David Kinzer <dtkinzer@gmail.com>
2018-07-10 13:39:27 -0300

    Allow emails of articles.
    REF BL-501.
    
    Depends on #499

--- app/models/primo_central_document.rb
+++ app/models/primo_central_document.rb
@@ -6,4 +6,17 @@ class PrimoCentralDocument
   include Blacklight::PrimoCentral::Document
 
   self.unique_key = :pnxId
+  field_semantics.merge!(
+    title: "title" ,
+    part_of: "isPartOf",
+    author: "creator",
+    contributor: "contributor",
+    date: "date",
+    isbn: "isbn",
+    issn: "issn",
+    doi: "doi",
+  )
+
+  # Email uses the semantic field mappings below to generate the body of an email.
+  PrimoCentralDocument.use_extension(Blacklight::Document::ArticleEmail)
 end
