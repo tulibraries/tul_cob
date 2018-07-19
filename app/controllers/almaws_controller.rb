fd8dd72d370de722977e3037b40694fdbc0ec66d
sensei100 <tuh42082@temple.edu>
2018-07-19 13:37:16 -0400

    Fix rubocop offenses

--- app/controllers/almaws_controller.rb
+++ app/controllers/almaws_controller.rb
@@ -25,14 +25,15 @@ class AlmawsController < ApplicationController
     @item_pid = CobAlma::Requests.item_pid(@items)
     @author = @items.map { |item| item["bib_data"]["author"].to_s }.first
     @description = CobAlma::Requests.descriptions(@items)
-    @pickup_locations = params[:pickup_location].split(",").collect { |lib| {lib => helpers.library_name_from_short_code(lib)} }
+    @pickup_locations = params[:pickup_location].split(",").collect { |lib| { lib => helpers.library_name_from_short_code(lib) } }
     @user_id = current_user.uid
     @request_level = params[:request_level]
     if @request_level == "item"
-          @request_options = Alma::ItemRequestOptions.get(@mms_id, @holding_id, @item_pid, user_id: @user_id)
-        else
-          @request_options = Alma::RequestOptions.get(@mms_id, user_id: @user_id)
-        end  end
+      @request_options = Alma::ItemRequestOptions.get(@mms_id, @holding_id, @item_pid, user_id: @user_id)
+    else
+      @request_options = Alma::RequestOptions.get(@mms_id, user_id: @user_id)
+    end
+    end
 
   def send_hold_request
     not_needed_date = DateTime.new(params[:last_interest_date]["year"].to_i, params[:last_interest_date]["month"].to_i, params[:last_interest_date]["day"].to_i)
