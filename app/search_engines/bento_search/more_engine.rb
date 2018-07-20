7decaf26f122456942b225b40dd3bcaae262b26c
David Kinzer <dtkinzer@gmail.com>
2018-07-20 16:03:35 -0300

    Revert "Add back books and journals."
    This reverts commit 0621bc68c82524e5410374740f08ab6b30815340.

--- app/search_engines/bento_search/more_engine.rb
+++ app/search_engines/bento_search/more_engine.rb
@@ -6,11 +6,18 @@ module BentoSearch
       query = args.fetch(:query, "")
       query = { q: query, per_page: 2 }
 
-      response = search_results(query).first
+      response = search_results(query, &proc_minus_books_journals).first
 
       item = BentoSearch::ResultItem.new(custom_data: response)
 
       results(response).append(item)
+    end
+
+    def proc_minus_books_journals
+      Proc.new { |builder|
+        processor_chain = [ :no_books_or_journals ]
+        builder.append(*processor_chain)
+      }
     end
   end
 end
