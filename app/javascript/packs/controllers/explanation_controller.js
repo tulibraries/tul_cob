9ad235e2fea409011431e46174330f06decc877e
sensei100 <tuh42082@temple.edu>
2018-07-23 14:02:14 -0400

    Remove explanation text for entire session

--- app/javascript/packs/controllers/explanation_controller.js
+++ app/javascript/packs/controllers/explanation_controller.js
@@ -1,9 +1,38 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "text" ]
+  static targets = [ "catalog", "article", "bento" ]
 
-  hide() {
-    this.textTarget.remove();
+  catalog() {
+    this.catalogOnce()
+  }
+
+  article() {
+    this.articleOnce()
+  }
+
+  bento() {
+    this.bentoOnce()
+  }
+
+  catalogOnce() {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)catalogOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+      this.catalogTarget.remove();
+      document.cookie = "catalogOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
+    }
+  }
+
+  articleOnce() {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)articleOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+      this.articleTarget.remove();
+      document.cookie = "articleOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
+    }
+  }
+
+  bentoOnce() {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)bentoOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+      this.bentoTarget.remove();
+      document.cookie = "bentoOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
+    }
   }
 }
