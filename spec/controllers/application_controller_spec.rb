29590c1910f9d9078106ea8ed95d524697069fc0
Chad Nelson <chad.nelson@temple.edu>
2018-08-02 17:09:52 -0400

    Update test to test what it is testing

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe ApplicationController, type: :controller do
     let (:sign_out_url) { Rails.configuration.devise[:sign_out_redirect_url] }
 
     it "should return the configured shib signout URL" do
+      session[:alma_auth_type] = "sso"
       sign_out_url = controller.after_sign_out_path_for(:foo)
       expect(sign_out_url).to_not be_nil
       expect(controller.after_sign_out_path_for(:foo)).to eq(sign_out_url)
