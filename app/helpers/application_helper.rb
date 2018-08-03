e2fe522b01cf12df7f4c8f998aaecce6b2d3df59
David Kinzer <dtkinzer@gmail.com>
2018-08-03 14:21:58 -0400

    Only text location and link.
    REF BL-548
    Also refactors render_location helper (was not being used).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # value ex: "MAIN stacks"
   def render_location(value)
-    Rails.configuration.locations[value]
+    params = value.to_s.split
+    [ Rails.configuration.libraries[params.first],
+      Rails.configuration.locations.dig(*params) ]
+      .compact
+      .join
   end
 
-  def render_location_show(value)  # why do we get the entire solr document in show fields?
+  def render_location_show(value)
     render_location(value[:value].first)
   end
 
--- app/models/concerns/blacklight/document/sms.rb
+++ app/models/concerns/blacklight/document/sms.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 # This module provides the body of an email export based on the document's semantic values
+
 module Blacklight::Document::Sms
   # Return a text string that will be the body of the email
+  include ApplicationHelper
+
   def to_sms_text
     semantics = self.to_semantic_values
     body = []
-    [ "title", "location", "alma_mms" ].each do |field|
-      if !semantics[field.to_sym].blank?
-        value = semantics[field.to_sym]
-        label = "blacklight.sms.text.#{field}"
-        body << I18n.t(label, value: value.join("; ").gsub("|", "; "))
-      end
-    end
+
+    locations = semantics[:location].map { |l| render_location(l) }.join(", ")
+    body << I18n.t("blacklight.sms.text.location", value: locations) unless locations.empty?
 
     return body.join unless body.empty?
   end
