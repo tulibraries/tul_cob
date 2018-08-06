af83bb8c2085bb67489cd00c268884593bc10b63
Chad Nelson <chadbnelson@gmail.com>
2018-08-06 14:20:07 -0400

    Fix shib logout and redirect to current page issues (#548)
    * Use a URL param to force logout on Shibboleth
    
    * Use the target param which Shibboleth passes along
    
    * Fix test by being more explicit

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,15 +4,12 @@ require "rails_helper"
 
 RSpec.describe ApplicationController, type: :controller do
 
-  # Overrides Devise::Controllers::Helpers#after_sign_out_path_for
-  describe "#after_sign_out_path_for" do
-    let (:sign_out_url) { Rails.configuration.devise[:sign_out_redirect_url] }
+  let (:sign_out_url) { Rails.configuration.devise[:sign_out_redirect_url] }
 
-    it "should return the configured shib signout URL" do
-      session[:alma_auth_type] = "sso"
-      sign_out_url = controller.after_sign_out_path_for(:foo)
-      expect(sign_out_url).to_not be_nil
-      expect(controller.after_sign_out_path_for(:foo)).to eq(sign_out_url)
-    end
+  it "should return the configured shib signout URL" do
+    request.params[:type] = "sso"
+    sign_out_url = controller.after_sign_out_path_for(:foo)
+    expect(sign_out_url).to_not be_nil
+    expect(controller.after_sign_out_path_for(:foo)).to eq(sign_out_url)
   end
 end
