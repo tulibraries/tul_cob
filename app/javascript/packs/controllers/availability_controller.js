9f7ebffbb42044cb1e3458799accf2fb2be47166
sensei100 <tuh42082@temple.edu>
2018-08-08 11:36:18 -0400

    Add unique ids to prevent bug in search results

--- app/javascript/packs/controllers/availability_controller.js
+++ app/javascript/packs/controllers/availability_controller.js
@@ -25,7 +25,9 @@ export default class extends Controller {
           this.panelTarget.innerHTML = html
           this.buttonTarget.classList.add("clicked")
           $(this.panelTarget).parent().removeClass("hidden");
-          var requests_url = $("#request-url-data").data("requests-url");
+          var mms_id = $(this.buttonTarget).data("availability-ids");
+          console.log(mms_id);
+          var requests_url = $("#request-url-data-" + mms_id).data("requests-url");
           $(this.requestTarget).attr("data-requests-url", requests_url);
           $(this.requestTarget).removeClass("hidden");
           this.requestTarget.classList.add("search-results-request-btn")
