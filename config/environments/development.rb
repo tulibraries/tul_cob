6c549cedfbda387f69d849af4b4cbd98a15ceee6
David Kinzer <dtkinzer@gmail.com>
2018-08-08 16:19:51 -0400

    Fix cache_classes not set for dev environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  ENV["CACHE_CLASSES"] ? true : false
+  config.cache_classes = ENV["CACHE_CLASSES"] ? true : false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = ENV["CACHE_CLASSES"] ? true : false
 
   # Show full error reports.
   config.consider_all_requests_local = true
