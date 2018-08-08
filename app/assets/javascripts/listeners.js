8e854edae62adcdf1acdfc4a5216af1c52f4d080
Chris Doyle <cdoyle@temple.edu>
2018-08-08 15:56:00 -0400

    Analytics (#567)
    * Add ga click tracking to select elements: #BL-551. Bookmark checkboxes excluded for now.
    
    * Fixed rubocop fussiness.
    
    * Updated js listeners to only listen if element exists.
    
    * Refactor listeners.js
    
    * Removed testing alert...

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -1,102 +1,47 @@
 $(document).ready(function(){
-	document.getElementById("header-logo").addEventListener("click", function(){
-    handleEventClicks("header-logo", "navigation");
-	});	
-	document.getElementById("login").addEventListener("click", function(){
-    handleEventClicks("login", "navigation");
-	});	
-	document.getElementById("logout").addEventListener("click", function(){
-    handleEventClicks("logout", "navigation");
-	});	
-	document.getElementById("bookmarks").addEventListener("click", function(){
-    handleEventClicks("bookmarks", "navigation");
-	});	
-	document.getElementById("articles-header").addEventListener("click", function(){
-    handleEventClicks("articles-header", "navigation");
-	});	
-	document.getElementById("articles-button").addEventListener("click", function(){
-    handleEventClicks("articles-button", "navigation");
-	});	
-	document.getElementById("catalog-header").addEventListener("click", function(){
-    handleEventClicks("catalog-header", "navigation");
-	});	
-	document.getElementById("catalog-button").addEventListener("click", function(){
-    handleEventClicks("catalog-button", "navigation");
-	});	
-	document.getElementById("bento_books").addEventListener("click", function(){
-    handleEventClicks("bento_books", "bento-results");
-	});	
-	document.getElementById("bento_articles").addEventListener("click", function(){
-    handleEventClicks("bento_articles", "bento-results");
-	});	
-	document.getElementById("bento_more").addEventListener("click", function(){
-    handleEventClicks("bento_more", "bento-results");
-	});	
-	document.getElementById("bento_journals").addEventListener("click", function(){
-    handleEventClicks("bento_journals", "bento-results");
-	});	
-	document.getElementById("bento_resource_types").addEventListener("click", function(){
-    handleEventClicks("bento_resource_types", "bento-results");
-	});	
-	document.getElementById("one_online_button").addEventListener("click", function(){
-    handleEventClicks("one_online_button", "bento-results");
-	});	
-	document.getElementById("many_online_button").addEventListener("click", function(){
-    handleEventClicks("many_online_button", "bento-results");
-	});	
-	document.getElementById("advanced_search").addEventListener("click", function(){
-    handleEventClicks("advanced_search", "search-results");
-	});	
-	document.getElementById("basic_search").addEventListener("click", function(){
-    handleEventClicks("basic_search", "search-results");
-	});	
-	document.getElementById("online_button").addEventListener("click", function(){
-    handleEventClicks("online_button", "search-results");
-	});	
-	document.getElementById("direct_link_online").addEventListener("click", function(){
-    handleEventClicks("direct_link_online", "search-results");
-	});	
-	document.getElementById("single_link_online").addEventListener("click", function(){
-    handleEventClicks("single_link_online", "search-results");
-	});	
-	document.getElementById("many_links_online").addEventListener("click", function(){
-    handleEventClicks("many_links_online", "search-results");
-	});	
-	document.getElementById("online-only").addEventListener("click", function(){
-    handleEventClicks("online-only", "search-results");
-	});	
-	document.getElementById("articles_basic_search").addEventListener("click", function(){
-    handleEventClicks("articles_basic_search", "search-results");
-	});	
-	document.getElementById("articles_advanced_search").addEventListener("click", function(){
-    handleEventClicks("articles_advanced_search", "search-results");
-	});	
-	document.getElementById("back_to_search").addEventListener("click", function(){
-    handleEventClicks("back_to_search", "search-results");
-	});	
-	document.getElementById("start_over").addEventListener("click", function(){
-    handleEventClicks("start_over", "search-results");
-	});	
-	document.getElementById("emailLink").addEventListener("click", function(){
-    handleEventClicks("emailLink", "search-results");
-	});	
-	document.getElementById("smsLink").addEventListener("click", function(){
-    handleEventClicks("smsLink", "search-results");
-	});	
-	document.getElementById("refworksLink").addEventListener("click", function(){
-    handleEventClicks("refworksLink", "search-results");
-	});	
-	document.getElementById("citationLink").addEventListener("click", function(){
-    handleEventClicks("citationLink", "search-results");
-	});
-	document.getElementById("signin_for_request_options").addEventListener("click", function(){
-    handleEventClicks("signin_for_request_options", "search-results");
-	});	
-	document.getElementById("request").addEventListener("click", function(){
-    handleEventClicks("request", "search-results");
-	});	
-	document.getElementById("available_button").addEventListener("click", function(){
-    handleEventClicks("available_button", "search-results");
-	});	
+
+	var tracks = [
+		{id: "header-logo", category: "navigation"},
+		{id: "login", category: "navigation"},
+		{id: "logout", category: "navigation"},
+		{id: "bookmarks", category: "navigation"},
+		{id: "articles-header", category: "navigation"},
+		{id: "articles-button", category: "navigation"},
+		{id: "catalog-header", category: "navigation"},
+		{id: "catalog-button", category: "navigation"},
+		{id: "bento_books", category: "bento-results"},
+		{id: "bento_articles", category: "bento-results"},
+		{id: "bento_more", category: "bento-results"},
+		{id: "bento_journals", category: "bento-results"},
+		{id: "bento_resource_types", category: "bento-results"},
+		{id: "one_online_button", category: "bento-results"},
+		{id: "many_online_button", category: "bento-results"},
+		{id: "advanced_search", category: "search-results"},
+		{id: "basic_search", category: "search-results"},
+		{id: "online_button", category: "search-results"},
+		{id: "bookmarks", category: "search-results"},
+		{id: "direct_link_online", category: "search-results"},
+		{id: "single_link_online", category: "search-results"},
+		{id: "many_links_online", category: "search-results"},
+		{id: "online-only", category: "search-results"},
+		{id: "articles_basic_search", category: "search-results"},
+		{id: "articles_advanced_search", category: "search-results"},
+		{id: "back_to_search", category: "search-results"},
+		{id: "start_over", category: "search-results"},
+		{id: "emailLink", category: "search-results"},
+		{id: "smsLink", category: "search-results"},
+		{id: "refworksLink", category: "search-results"},
+		{id: "citationLink", category: "search-results"},
+		{id: "signin_for_request_options", category: "search-results"},
+		{id: "request", category: "search-results"},
+		{id: "available_button", category: "search-results"}
+	];
+
+	tracks.forEach(function(track) {
+		if (el = document.getElementById(track.id)) {
+			el.addEventListener("click", function(){
+		    handleEventClicks(track.id, track.category) })
+			}
+		});	
 
 });
