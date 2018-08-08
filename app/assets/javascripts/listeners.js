64cd970b8514830253c53369ecb562a578c6e930
Chris Doyle <cdoyle@temple.edu>
2018-08-08 14:37:08 -0400

    Updated js listeners to only listen if element exists.

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -1,102 +1,202 @@
 $(document).ready(function(){
-	document.getElementById("header-logo").addEventListener("click", function(){
-    handleEventClicks("header-logo", "navigation");
-	});	
-	document.getElementById("login").addEventListener("click", function(){
-    handleEventClicks("login", "navigation");
-	});	
-	document.getElementById("logout").addEventListener("click", function(){
-    handleEventClicks("logout", "navigation");
-	});	
-	document.getElementById("bookmarks").addEventListener("click", function(){
-    handleEventClicks("bookmarks", "navigation");
-	});	
-	document.getElementById("articles-header").addEventListener("click", function(){
-    handleEventClicks("articles-header", "navigation");
-	});	
-	document.getElementById("articles-button").addEventListener("click", function(){
-    handleEventClicks("articles-button", "navigation");
-	});	
-	document.getElementById("catalog-header").addEventListener("click", function(){
-    handleEventClicks("catalog-header", "navigation");
-	});	
-	document.getElementById("catalog-button").addEventListener("click", function(){
-    handleEventClicks("catalog-button", "navigation");
-	});	
-	document.getElementById("bento_books").addEventListener("click", function(){
-    handleEventClicks("bento_books", "bento-results");
-	});	
-	document.getElementById("bento_articles").addEventListener("click", function(){
-    handleEventClicks("bento_articles", "bento-results");
-	});	
-	document.getElementById("bento_more").addEventListener("click", function(){
-    handleEventClicks("bento_more", "bento-results");
-	});	
-	document.getElementById("bento_journals").addEventListener("click", function(){
-    handleEventClicks("bento_journals", "bento-results");
-	});	
-	document.getElementById("bento_resource_types").addEventListener("click", function(){
-    handleEventClicks("bento_resource_types", "bento-results");
-	});	
-	document.getElementById("one_online_button").addEventListener("click", function(){
-    handleEventClicks("one_online_button", "bento-results");
-	});	
-	document.getElementById("many_online_button").addEventListener("click", function(){
-    handleEventClicks("many_online_button", "bento-results");
-	});	
-	document.getElementById("advanced_search").addEventListener("click", function(){
-    handleEventClicks("advanced_search", "search-results");
-	});	
-	document.getElementById("basic_search").addEventListener("click", function(){
-    handleEventClicks("basic_search", "search-results");
-	});	
-	document.getElementById("online_button").addEventListener("click", function(){
-    handleEventClicks("online_button", "search-results");
-	});	
-	document.getElementById("direct_link_online").addEventListener("click", function(){
-    handleEventClicks("direct_link_online", "search-results");
-	});	
-	document.getElementById("single_link_online").addEventListener("click", function(){
-    handleEventClicks("single_link_online", "search-results");
-	});	
-	document.getElementById("many_links_online").addEventListener("click", function(){
-    handleEventClicks("many_links_online", "search-results");
-	});	
-	document.getElementById("online-only").addEventListener("click", function(){
-    handleEventClicks("online-only", "search-results");
-	});	
-	document.getElementById("articles_basic_search").addEventListener("click", function(){
-    handleEventClicks("articles_basic_search", "search-results");
-	});	
-	document.getElementById("articles_advanced_search").addEventListener("click", function(){
-    handleEventClicks("articles_advanced_search", "search-results");
-	});	
-	document.getElementById("back_to_search").addEventListener("click", function(){
-    handleEventClicks("back_to_search", "search-results");
-	});	
-	document.getElementById("start_over").addEventListener("click", function(){
-    handleEventClicks("start_over", "search-results");
-	});	
-	document.getElementById("emailLink").addEventListener("click", function(){
-    handleEventClicks("emailLink", "search-results");
-	});	
-	document.getElementById("smsLink").addEventListener("click", function(){
-    handleEventClicks("smsLink", "search-results");
-	});	
-	document.getElementById("refworksLink").addEventListener("click", function(){
-    handleEventClicks("refworksLink", "search-results");
-	});	
-	document.getElementById("citationLink").addEventListener("click", function(){
-    handleEventClicks("citationLink", "search-results");
-	});
-	document.getElementById("signin_for_request_options").addEventListener("click", function(){
-    handleEventClicks("signin_for_request_options", "search-results");
-	});	
-	document.getElementById("request").addEventListener("click", function(){
-    handleEventClicks("request", "search-results");
-	});	
-	document.getElementById("available_button").addEventListener("click", function(){
-    handleEventClicks("available_button", "search-results");
-	});	
+	var el;
+	el = document.getElementById("header-logo");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("header-logo", "navigation");
+		});	
+	}
+	el = document.getElementById("login");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("login", "navigation");
+		});	
+	}
+	el = document.getElementById("logout");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("logout", "navigation");
+		});	
+	}
+	el = document.getElementById("bookmarks");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("bookmarks", "navigation");
+		});	
+	}
+	el = document.getElementById("articles-header");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("articles-header", "navigation");
+		});	
+	}
+	el = document.getElementById("articles-button");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("articles-button", "navigation");
+		});	
+	}
+	el = document.getElementById("catalog-header");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("catalog-header", "navigation");
+		});	
+	}
+	el = document.getElementById("catalog-button");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("catalog-button", "navigation");
+		});	
+	}
+	el = document.getElementById("bento_books");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("bento_books", "bento-results");
+		});	
+	}
+	el = document.getElementById("bento_articles");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("bento_articles", "bento-results");
+		});	
+	}
+	el = document.getElementById("bento_more");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("bento_more", "bento-results");
+		});	
+	}
+	el = document.getElementById("bento_journals");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("bento_journals", "bento-results");
+		});	
+	}
+	el = document.getElementById("bento_resource_types");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("bento_resource_types", "bento-results");
+		});	
+	}
+	el = document.getElementById("one_online_button");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("one_online_button", "bento-results");
+		});	
+	}
+	el = document.getElementById("many_online_button");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("many_online_button", "bento-results");
+		});	
+	}
+	el = document.getElementById("advanced_search");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("advanced_search", "search-results");
+		});	
+	}
+	el = document.getElementById("basic_search");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("basic_search", "search-results");
+		});	
+	}
+	el = document.getElementById("online_button");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("online_button", "search-results");
+		});	
+	}
+	el = document.getElementById("direct_link_online");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("direct_link_online", "search-results");
+		});	
+	}
+	el = document.getElementById("single_link_online");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("single_link_online", "search-results");
+		});	
+	}
+	el = document.getElementById("many_links_online");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("many_links_online", "search-results");
+		});	
+	}
+	el = document.getElementById("online-only");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("online-only", "search-results");
+		});	
+	}
+	el = document.getElementById("articles_basic_search");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("articles_basic_search", "search-results");
+		});	
+	}
+	el = document.getElementById("articles_advanced_search");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("articles_advanced_search", "search-results");
+		});	
+	}
+	el = document.getElementById("back_to_search");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("back_to_search", "search-results");
+		});	
+	}
+	el = document.getElementById("start_over");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("start_over", "search-results");
+		});	
+	}
+	el = document.getElementById("emailLink");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("emailLink", "search-results");
+		});	
+	}
+	el = document.getElementById("smsLink");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("smsLink", "search-results");
+		});	
+	}
+	el = document.getElementById("refworksLink");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("refworksLink", "search-results");
+		});	
+	}
+	el = document.getElementById("citationLink");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("citationLink", "search-results");
+		});
+	}
+	el = document.getElementById("signin_for_request_options");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("signin_for_request_options", "search-results");
+		});	
+	}
+	el = document.getElementById("request");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("request", "search-results");
+		});	
+	}
+	el = document.getElementById("available_button");
+	if(el) {
+		el.addEventListener("click", function(){
+	    handleEventClicks("available_button", "search-results");
+		});	
+	}
 
 });
