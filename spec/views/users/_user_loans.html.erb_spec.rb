01151db0725f36a531052e8d03aa440cb042946c
Chad Nelson <chad.nelson@temple.edu>
2018-08-20 09:11:00 -0400

    Fix loans test

--- spec/views/users/_user_loans.html.erb_spec.rb
+++ spec/views/users/_user_loans.html.erb_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 require "time"
 
-RSpec.describe "users/_user_loans.html.erb", type: :view do
+RSpec.describe "users/_loans_details.html.erb", type: :view do
   before :each do
     @user = FactoryBot.build(:user)
   end
@@ -37,16 +37,14 @@ RSpec.describe "users/_user_loans.html.erb", type: :view do
     }
 
     it "doesn't show exclamation point in renewal column" do
-      allow(@user).to receive(:loans).and_return([renewable_loan])
-
-      render partial: "users/user_loans"
+      loans = double("Loan Set", all: [renewable_loan])
+      render partial: "users/loans_details", locals: {loans: loans}
       expect(rendered).to_not have_css('td.renewal-check span.glyphicon-exclamation-sign[title="unable to renew"]')
     end
 
     it "doesn't show exclamation point in renewal column" do
-      allow(@user).to receive(:loans).and_return([renewable_loan_without_flag])
-
-      render partial: "users/user_loans"
+      loans = double("Loan Set", all: [renewable_loan_without_flag])
+      render partial: "users/loans_details", locals: {loans: loans}
       expect(rendered).to_not have_css('td.renewal-check span.glyphicon-exclamation-sign[title="unable to renew"]')
     end
   end
@@ -66,9 +64,8 @@ RSpec.describe "users/_user_loans.html.erb", type: :view do
     }
 
     it "shows exclamation point in renewal column" do
-      allow(@user).to receive(:loans).and_return([nonrenewable_loan])
-
-      render partial: "users/user_loans"
+      loans = double("Loan Set", all: [nonrenewable_loan])
+      render partial: "users/loans_details", locals: {loans: loans}
       expect(rendered).to have_css('td.renewal-check span.glyphicon-exclamation-sign[title="unable to renew"]')
     end
   end
