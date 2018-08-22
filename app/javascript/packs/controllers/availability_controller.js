7821657dff6921c9a072019787a9984798e97e7f
sensei100 <tuh42082@temple.edu>
2018-08-22 15:55:05 -0400

    Adjust login message view for search results request

--- app/javascript/packs/controllers/availability_controller.js
+++ app/javascript/packs/controllers/availability_controller.js
@@ -27,8 +27,8 @@ export default class extends Controller {
           $(this.panelTarget).parent().removeClass("hidden");
           var mms_id = $(this.buttonTarget).data("availability-ids");
           var requests_url = $("#request-url-data-" + mms_id).data("requests-url");
-          $(this.hrefTarget).attr("href", requests_url);
           $(this.requestTarget).removeClass("hidden");
+          $(this.hrefTarget).attr("href", requests_url);
           this.requestTarget.classList.add("search-results-request-btn")
       })
     }
