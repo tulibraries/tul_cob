19a4ee2e86f70901a1724aad88f4a8e2305b39f1
David Kinzer <dtkinzer@gmail.com>
2018-08-31 14:44:53 -0400

    Move polyfills to separate file. (#640)
    I was taking a look at blacklight_alma_libs.js and it's getting to be
    pretty big file.
    
    This change moves polyfills to another file where we can keep easier
    track of it and also replaces with some `includes` function polyfill
    with some more robust versions (sources linked).

--- app/assets/javascripts/blacklight_alma/blacklight_alma_lib.js
+++ app/assets/javascripts/blacklight_alma/blacklight_alma_lib.js
@@ -383,18 +383,3 @@ var BlacklightAlma = function (options) {
         $(element).html("<span style='color: red'>No status available for this item</span>");
      });
  };
-
- /** This is a polyfill to get rid of a console error in Internet Explorer 11
- that is preventing the availability information from being shown
- */
-if (!String.prototype.includes) {
- String.prototype.includes = function(value){
-   return this.indexOf(value)>-1
- }
-}
-
-if (!Array.prototype.includes) {
- Array.prototype.includes = function(value){
-   return this.indexOf(value)>-1||!1
- }
-}
