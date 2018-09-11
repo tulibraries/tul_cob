f530769ab8a90dd49ecf523a47e4566757d6f8e9
sensei100 <tuh42082@temple.edu>
2018-09-11 09:22:02 -0400

    Use jQuery remove method

--- app/javascript/packs/controllers/explanation_controller.js
+++ app/javascript/packs/controllers/explanation_controller.js
@@ -19,7 +19,7 @@ export default class extends Controller {
   showOnce(namespace) {
     let reg = new RegExp("(?:(?:^|.*;\s*)" + namespace + "Once\s*\=\s*([^;]*).*$)|^.*$")
     if (document.cookie.replace(reg, "$1") !== "true") {
-        this[namespace + "Target"].remove();
+        $(this[namespace + "Target"]).remove();
         document.cookie = namespace + "Once=true; expires=Fri, 31 Dec 9999 23:59:59 GMT;";
       }
     }
