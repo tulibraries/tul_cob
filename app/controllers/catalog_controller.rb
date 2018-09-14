f15586ae5fc796f969995759f0c83787c8e511f0
sensei100 <tuh42082@temple.edu>
2018-09-14 11:39:18 -0400

    Use double quotes

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -500,7 +500,7 @@ class CatalogController < ApplicationController
     # marc config
     # Do not show library_view link
     config.show.document_actions.delete(:librarian_view)
-    add_show_tools_partial(:ris, label: 'RIS File', if: :render_ris_action?, modal: false, path: :ris_path)
+    add_show_tools_partial(:ris, label: "RIS File", if: :render_ris_action?, modal: false, path: :ris_path)
     # Do not show endnotes for beta release
     config.show.document_actions.delete(:endnote)
     config.show.document_actions.delete(:sms) if Rails.configuration.features[:sms_document_action_disabled]
