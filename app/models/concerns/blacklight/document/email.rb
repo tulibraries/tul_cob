f11325f033b9fd886e31289a8c0d36638b9e588f
sensei100 <tuh42082@temple.edu>
2018-09-25 13:27:40 -0400

    BL-639 Add holdings information to email messages
    Adds the library name, location and call number for each holding to the 
    email message text.

--- app/models/concerns/blacklight/document/email.rb
+++ app/models/concerns/blacklight/document/email.rb
@@ -2,6 +2,7 @@
 
 # This module provides the body of an email export based on the document's semantic values
 module Blacklight::Document::Email
+  include ApplicationHelper
   # Return a text string that will be the body of the email
   # Overridden in order to add our own custom fields to email text.
   def to_email_text
@@ -15,7 +16,12 @@ module Blacklight::Document::Email
         body << I18n.t(label, value: value.join("; ").gsub("|", "; "))
       end
     end
-
+    body << add_holdings_information
     return body.join("\n") unless body.empty?
+  end
+
+  def add_holdings_information
+    holdings = materials.collect { |material| material["library"] + " - " + material["location"] + " - " + material["call_number"] }
+    I18n.t("blacklight.email.text.location", value: "\n" + holdings.join("\n"))
   end
 end
