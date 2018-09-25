b5db7016899c638240ed5664d204425dce94b195
David Kinzer <dtkinzer@gmail.com>
2018-09-25 13:22:45 -0400

    Fix bookmark tools not rendering all actions. (#710)
    Because we are passing @document instead of @document_list to
    the `render_show_actions` method in the bookmarks context, not all
    actions can be added via configuration.
    
    This change updates the bookmarks action template to pass @document_list to
    `render_show_actions` and removes adding RIS action via the template and
    instead adds that via configuration as would be expected.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,4 +5,8 @@ class BookmarksController < CatalogController
   include Blacklight::Bookmarks
   include MultiSourceBookmarks
   include BookmarksConfig
+
+  configure_blacklight do |config|
+    add_show_tools_partial(:ris, label: "RIS File", modal: false, path: :ris_path)
+  end
 end
