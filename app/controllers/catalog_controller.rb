5328b702e04673fed5e82f00d4c4e3362ec6b8d7
David Kinzer <dtkinzer@gmail.com>
2018-10-02 15:18:57 -0400

    Move field display alternative to config.
    Removes the unless field is name check in template and adds an explicit display config to the field.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -363,7 +363,7 @@ class CatalogController < ApplicationController
     config.add_show_field "alma_mms_display", label: "Catalog Record ID"
     config.add_show_field "language_display", label: "Language"
     config.add_show_field "url_more_links_display", label: "Other Links", helper_method: :check_for_full_http_link
-    config.add_show_field "electronic_resource_display", label: "Availability", helper_method: :check_for_full_http_link
+    config.add_show_field "electronic_resource_display", label: "Availability", helper_method: :check_for_full_http_link, if: false
 
     config.add_show_field "bound_with_ids", display: false
 
