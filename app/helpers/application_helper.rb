a9fe529fbc4b84de396f33f71dee3b096b9e2518
Jen <tuh42082@temple.edu>
2018-10-11 15:45:58 -0400

    BL-691 Add breadcrumbs to record pages (#748)
    Breadcrumb Labels should change depending on the user's current search 
    context and should go back to their current search state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -308,4 +308,19 @@ module ApplicationHelper
       link_to(name, send(path, params.except(:controller, :action)), class: link_class)
     end
   end
+
+  def breadcrumb_links
+    case controller_name
+    when "books"
+      link_back_to_catalog(label: "Books & More")
+    when "primo_central"
+      link_back_to_catalog(label: "Articles")
+    when "catalog"
+      link_back_to_catalog(label: "More")
+    when "journals"
+      link_back_to_catalog(label: "Journals")
+    else
+      link_back_to_catalog(label: "More")
+    end
+  end
 end
