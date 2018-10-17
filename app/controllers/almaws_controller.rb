ebf4f47ee8019a68c122367cdf858c516169a9b4
sensei100 <tuh42082@temple.edu>
2018-10-17 13:58:14 -0400

    BL-545 EZ-borrow integration
    Uses the alma request options api to retreive the link to EZ-borrow.  
    The link should only appear on items that are books currently 
    unavailable at Temple Libraries.

--- app/controllers/almaws_controller.rb
+++ app/controllers/almaws_controller.rb
@@ -30,6 +30,7 @@ class AlmawsController < ApplicationController
   def request_options
     @mms_id = params[:mms_id]
     @items = Alma::BibItem.find(@mms_id, limit: 100)
+    @books = @items.items.map { |item| item.item_data.dig("physical_material_type", "value") }.first
     @holding_id = CobAlma::Requests.item_holding_id(@items)
     @item_pid = CobAlma::Requests.item_pid(@items)
     @author = @items.map { |item| item["bib_data"]["author"].to_s }.first
