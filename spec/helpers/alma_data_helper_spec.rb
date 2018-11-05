95f0ba71fa18dfe57278a752ee9e8a3d3c449b36
Jen <tuh42082@temple.edu>
2018-11-05 16:04:08 -0500

    Revert "Revert "BL-359 Suppress items from availability display (#793)" (#796)"
    This reverts commit b502e4f3dec141cd8cad6d2a947595ff4c98c342.

--- spec/helpers/alma_data_helper_spec.rb
+++ spec/helpers/alma_data_helper_spec.rb
@@ -543,4 +543,83 @@ RSpec.describe AlmaDataHelper, type: :helper do
     end
   end
 
+  describe "#unsuppressed_holdings(items_list, document)" do
+    context "holding_id is found in solr" do
+      let(:items_list) do
+        { "MAIN" => [Alma::BibItem.new(
+          "holding_data" =>
+             { "holding_id" => "22419862390003811"
+          }
+      ), Alma::BibItem.new(
+        "holding_data" =>
+           { "holding_id" => "22426649410003811",
+        }
+      )]
+      }
+      end
+
+      let(:document) {
+          {
+            "holdings_display" => ["22419862390003811"]
+          }
+        }
+
+      it "returns only items found in both api data and document" do
+        unsuppressed_holdings(items_list, document)
+        expect(items_list["MAIN"].count).to eq(1)
+      end
+    end
+
+    context "holding_id is not found in solr" do
+      let(:items_list) do
+        { "MAIN" => [Alma::BibItem.new(
+          "holding_data" =>
+             { "holding_id" => "22419862390003811"
+          }
+      ), Alma::BibItem.new(
+        "holding_data" =>
+           { "holding_id" => "123",
+        }
+      )]
+      }
+      end
+
+      let(:document) {
+          {
+            "holdings_display" => ["456"]
+          }
+        }
+
+      it "does not include the item with that holding_id" do
+        unsuppressed_holdings(items_list, document)
+        expect(items_list["MAIN"].count).to eq(0)
+      end
+    end
+
+    context "all holding ids are found in document and api data" do
+      let(:items_list) do
+        { "MAIN" => [Alma::BibItem.new(
+          "holding_data" =>
+             { "holding_id" => "456"
+          }
+      ), Alma::BibItem.new(
+        "holding_data" =>
+           { "holding_id" => "123",
+        }
+      )]
+      }
+      end
+
+      let(:document) {
+          {
+            "holdings_display" => ["456", "123"]
+          }
+        }
+
+      it "does not include the item with that holding_id" do
+        unsuppressed_holdings(items_list, document)
+        expect(items_list["MAIN"].count).to eq(2)
+      end
+    end
+  end
 end
