9f53f919356d22d2777985623b0d16fc8307878f
sensei100 <tuh42082@temple.edu>
2018-11-19 11:07:28 -0500

    BL-622 Use translations for requests
    Refactors the modals and forms for requests to use translations

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -4,6 +4,6 @@ module RequestHelper
   include Blacklight::CatalogHelperBehavior
 
   def request_modal(mms_id, document_counter, pickup_locations, request_level)
-    link_to("Request", "#", id: "request-btn-#{document_counter}", class: "btn btn-primary request-button search-results-request-btn", data: { "ajax-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
+    link_to(t("requests.request_button"), "#", id: "request-btn-#{document_counter}", class: "btn btn-primary request-button search-results-request-btn", data: { "ajax-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
   end
 end
