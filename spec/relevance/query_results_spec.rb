610a79e7939cf4617d103b5462a53ac0d60f368f
David Kinzer <dtkinzer@gmail.com>
2018-11-19 09:22:56 -0500

    Refactors search relevancy tests. (#815)
    As a developer I want to quickly decipher the reason that a test fails
    in order to get started on fixing the underlying issue.
    
    This commit adds two new rspec matchers that encapsulate some analysis
    utils for the search results.
    
    This commit also updates the tests to use the new utils as well as
    refactor the tests themselves to be more declarative and avoid the extra
    step of looking through a fixture file for expected items.

--- spec/relevance/query_results_spec.rb
+++ spec/relevance/query_results_spec.rb
@@ -3,70 +3,282 @@
 require "rails_helper"
 
 RSpec.describe CatalogController, type: :controller, relevance: true do
+  render_views
+
+  let(:search_term) { search_terms.values.join(" ") }
+
+  let(:search_query) {
+    -> (page) { get(:index, params: { q: search_term, page: page, format: "json" }) }
+  }
+
+  let(:docs) {
+    [1..2]
+      .map { |page| search_query[page.to_s] }
+      .map { |r| JSON.parse(r.body)["response"]["docs"] }
+      .flatten
+  }
+
+  let(:doc_ids) { docs.map { |doc| doc["id"] } }
 
   describe "Query results as JSON" do
-    render_views
+    context "search for title 'peer interaction and second language learning'" do
+      let(:search_terms)  { { title: "peer interaction and second language learning" } }
+      let(:primary_results) { [
+        "991001254809703811",
+        "991022272009703811" ] }
 
-    context "to check that the search results" do
-      fixtures = YAML.load_file("#{fixture_path}/search_features.yml")
-      test_queries = fixtures.fetch("results_queries")
-      docs = []
-      # Take the test queries from the YAML file one at a time...
-      test_queries.each do |test_item|
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
 
-        search_string = ""
-        # Concatenate the specified search terms into one search string
-        test_item["query_type"].each do |query_field|
-          search_term = test_item[query_field]
-          if search_term.kind_of?(Array) == true
-            search_term = search_term[0].to_s
-          end
-          search_string += search_term + " "
-        end
+    context "search for title 'hillbilly elegy'" do
+      let(:search_terms) { { title: "hillbilly elegy" } }
+      let(:primary_results) { [ "991024587289703811" ] }
 
-        it "for search #{search_string} contains the correct items in the correct order" do
-          # Perform the search and concatenate the first two pages of results (we aren't interested in results past 2 pages)
-          for page in [1..2] do
-            get(:index, params: { q: search_string, page: page.to_s }, format: "json")
-            docs += JSON.parse(response.body)["response"]["docs"]
-          end
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
 
-          # For every item returned in the search...
-          docs.each do |search_result|
-            # The magic here is that we have to invert our way of looking at expected results vs actual results
-            # We're going to see if each returned item exists in our set of expect results...
-            # (And that we haven't run out of expected items to check)
-            if test_item.include?("doc_id") && test_item["doc_id"].size > 0
-              # ...And if the item is found in our list...
-              if test_item["doc_id"].include?(search_result["id"])
-                # ...Remove it from the list of expected items
-                # The goal being to empty the list of expected items.
-                # Once it's empty, we know all items have been found and the test is fulfilled.
-                test_item["doc_id"].delete(search_result["id"])
-              end
-            # Once we've emptied the list of primary results, now we can look for secondary results in the same manner
-            elsif test_item["doc_id"].size == 0 && test_item.include?("secondary_id") && !(test_item["secondary_id"].nil?) && test_item["secondary_id"].length > 0
-              if test_item["secondary_id"].include?(search_result["id"])
-                test_item["secondary_id"].delete(search_result["id"])
-              end
-            end
-          end
-          # If the test is about to fail, print some helpful debugging information
-          if (test_item["doc_id"].length > 0) || (test_item.include?("secondary_id") && !(test_item["secondary_id"].nil?) && test_item["secondary_id"].length > 0)
-            puts "\n search string:"
-            puts search_string
-            puts "\n test item (ids not found):"
-            puts test_item
-            puts "\n"
-            puts "\n search results:"
-            puts docs
-            puts "\n"
-          end
-          # The test conditions are, we've found all our primary items and removed them from the test list,
-          # And that we've found all the secondary items if there are any for this test
-          expect(test_item).to satisfy { test_item["doc_id"].length == 0 }
-          expect(test_item).to satisfy { test_item.exclude?("secondary_id") || (test_item["secondary_id"].nil?) || test_item["secondary_id"].length == 0 }
-        end
+    context "search 'Temple university 125 years of service to philadelphia'" do
+      let(:search_terms) {
+        { title: "Temple university",
+          subtitle: "125 years of service to philadelphia" } }
+
+      let(:primary_results) {
+        [ "991036742233003811",
+          "991017814679703811",
+          "991001513649703811",
+          "991002431869703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'our children's future child care policy in canada'" do
+      let(:search_terms) {
+        { title: "our children's future",
+          subtitle: "child care policy in canada" } }
+
+      let(:primary_results) {
+        [ "991036816752403811",
+          "991024784309703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'new england journal of medicine'" do
+      let(:search_terms) { { title: "new england journal of medicine" } }
+      let(:primary_results) {
+        [ "991036753565903811",
+          "991026206569703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'foot and ankle international'" do
+      let(:search_terms) { { title: "foot and ankle international" } }
+      let(:primary_results) {
+        [ "991036791313203811",
+          "991011580459703811"] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'chronicle of higher education'" do
+      let(:search_terms) { { title: "chronicle of higher education" } }
+      let(:primary_results) {
+        [ "991036721510203811",
+          "991012152319703811",
+          "991017377949703811",
+          "991006975379703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'housing association of delaware valley records'" do
+      let(:search_terms) { { title: "housing association of delaware valley records" } }
+      let(:primary_results) { [ "991003108369703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'lawrence gitman'" do
+      let(:search_terms) { { author: "lawrence gitman" } }
+      let(:primary_results) { [ "991008448689703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'miles livy'" do
+      let(:search_terms) { { keywords: "miles livy" } }
+      let(:primary_results) { [ "991034319589703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'moter learning magill'" do
+      let(:search_terms) { { keywords: "motor learning magill" } }
+      let(:primary_results) {
+        [ "991009723459703811",
+          "991022257719703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'Mahlon Howard Hellerich dissertation'" do
+      let(:search_terms) { { keywords: "Mahlon Howard Hellerich dissertation" } }
+      let(:primary_results) { [ "991034105309703811" ] }
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search '00095982'" do
+      let(:search_terms) { { ISSN: "00095982" } }
+      let(:primary_results) {
+        [ "991036721510203811",
+          "991012152319703811",
+          "991017377949703811",
+          "991006975379703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search '9780140449198'" do
+      let(:search_terms) { { ISBN: "9780140449198" } }
+      let(:primary_results) { [ "991006700799703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search '0140449191'" do
+      let(:search_terms) { { ISBN: "0140449191" } }
+      let(:primary_results) { [ "991006700799703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search '991036743832103811'" do
+      let(:search_terms) { { doc_id: "991036743832103811" } }
+      let(:primary_results) { [ "991036743832103811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search '991035120939703811'" do
+      let(:search_terms) { {  callnum: '"PN1997.2 .S462x 2003"' } }
+      let(:primary_results) { [ "991035120939703811" ] }
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'bicycle thieves'" do
+      let(:search_terms) { { title: "bicycle thieves" } }
+      let(:primary_results) {
+        [ "991024736809703811",
+          "991035018259703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'journal of materials chemistry'" do
+      let(:search_terms) { { title: "journal of materials chemistry" } }
+      let(:primary_results) { [ "991036749632703811" ] }
+      let(:secondary_reults) {
+        [ "991036749633503811",
+          "991036749630403811",
+          "991036749630103811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+          .come_before(secondary_reults)
+      end
+    end
+
+    context "search 'new york times'" do
+      let(:search_terms) { { title: "new york times" } }
+      let(:primary_results) {
+        [ "991036797063703811",
+          "991026245969703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'gray's anatomy the anatomical basis of clinical practice'" do
+      let(:search_terms) {
+        { title: "gray's anatomy",
+          subtitle: "the anatomical basis of clinical practice" } }
+      let(:primary_results) {
+        [ "991007333849703811",
+          "991003194609703811",
+          "991028264669703811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+      end
+    end
+
+    context "search 'Handbook on injectable drugs'" do
+      let(:search_terms) { { title: "Handbook on injectable drugs" } }
+      let(:primary_results) { [ "991000868839703811" ] }
+      let(:secondary_reults) {
+        [ "991033401389703811",
+          "991011446329703811",
+          "991036802592903811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+          .come_before(secondary_reults)
+      end
+    end
+
+    context "search 'the epic of gilgamesh'" do
+      let(:search_terms) { { title: "the epic of gilgamesh" } }
+      let(:primary_results) {
+        [ "991006700799703811",
+          "991017636819703811",
+          "991006996069703811",
+          "991013242359703811",
+          "991029586949703811" ] }
+      let(:secondary_reults) {
+        [ "991036749005103811",
+          "991036742769003811" ] }
+
+      it "returns expected values" do
+        expect(doc_ids).to have_items(primary_results)
+          .come_before(secondary_reults)
       end
     end
   end
