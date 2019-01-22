e085d9e4875eeadf8b1d9c7fc869cf6dac9aee61
Jen <tuh42082@temple.edu>
2019-01-22 08:39:00 -0500

    BL-781 Reserve item status (#914)
    - It can be confusing to users to see reserve items listed as available since they can only be checked out for a few hours and must remain in the library.  
    - Updates the item status to display library use only

--- app/helpers/alma_data_helper.rb
+++ app/helpers/alma_data_helper.rb
@@ -7,7 +7,7 @@ module AlmaDataHelper
 
   def availability_status(item)
     if item.in_place? && item.item_data["requested"] == false
-      if item.non_circulating?
+      if item.non_circulating? || item.location == "reserve"
         content_tag(:span, "", class: "check") + "Library Use Only"
       else
         content_tag(:span, "", class: "check") + "Available"
