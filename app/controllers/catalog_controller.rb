47fc0e846f1e9d2b44d227101372024cacb309ef
Jen <tuh42082@temple.edu>
2019-01-22 16:28:10 -0500

    Show tools dropdown menu displays tools (#922)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -570,7 +570,8 @@ class CatalogController < ApplicationController
 
     # Show tools
     config.add_show_tools_partial(:bookmark, partial: "bookmark_control", if: :render_bookmarks_control?)
-
+    config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
+    config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
 
     # Nav tools
     config.add_nav_action(:bookmark, partial: "blacklight/nav/bookmark", if: :render_bookmarks_control?)
