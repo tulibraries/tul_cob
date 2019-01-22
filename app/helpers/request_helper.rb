a75be172df14381217837a581d5c5a30134f691c
Jen <tuh42082@temple.edu>
2019-01-22 13:36:36 -0500

    Request modals displaying on both index and show pages (#920)

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -3,7 +3,7 @@
 module RequestHelper
   include Blacklight::CatalogHelperBehavior
 
-  def request_modal(mms_id, document_counter, pickup_locations, request_level)
-    link_to(t("requests.request_button"), "#", id: "request-btn-#{document_counter}", class: "btn btn-primary request-button search-results-request-btn", data: { "blacklight-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
+  def request_modal(mms_id, pickup_locations, request_level)
+    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn btn-primary request-button search-results-request-btn", data: { "blacklight-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
   end
 end
