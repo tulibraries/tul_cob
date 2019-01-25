1385bef3dadec0a24ef1a885600fdb90ff6a3998
sensei100 <tuh42082@temple.edu>
2019-01-25 12:04:29 -0500

    BL-789 xml views for books and journals
    - xml views and refworks are not currently available for books and journals. 
    - Adds the refworks_marc_txt to each model 
    - Adds helper methods for the refworks links

--- app/models/solr_book_document.rb
+++ app/models/solr_book_document.rb
@@ -3,4 +3,11 @@
 class SolrBookDocument < SolrDocument
   use_extension(Blacklight::Document::Email)
   use_extension(Blacklight::Document::Sms)
+
+  # Add Blacklight-Marc extension:
+  SolrBookDocument.use_extension(Blacklight::Solr::Document::Marc) do |doc|
+    doc.has_field? "marc_display_raw"
+  end
+  SolrBookDocument.extension_parameters[:marc_source_field] = "marc_display_raw"
+  SolrBookDocument.extension_parameters[:marc_format_type] = :marcxml
 end
