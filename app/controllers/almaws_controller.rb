8ecdb1abf888f4a0c19b83923b1459788dc56141
David Kinzer <dtkinzer@gmail.com>
2019-01-25 16:41:49 -0500

    Use do_with_json_logger.

--- app/controllers/almaws_controller.rb
+++ app/controllers/almaws_controller.rb
@@ -50,7 +50,7 @@ class AlmawsController < CatalogController
       log = { type: "item_request_options", mms_id: @mms_id, holding_id: @holding_id, item_pid: @item_pid, user: current_user.id }
       @item_level_holdings = CobAlma::Requests.item_holding_ids(@items)
       @request_options = @item_level_holdings.map { |holding_id, item_pid|
-        json_request_logger(log) { Alma::ItemRequestOptions.get(@mms_id, holding_id, item_pid, user_id: @user_id) }
+        do_with_json_logger(log) { Alma::ItemRequestOptions.get(@mms_id, holding_id, item_pid, user_id: @user_id) }
       }
         .sort_by { |r| r.raw_response.parsed_response.count }
         .last
