ce260279b050523ba12bee69032ddb04d125f7d7
David Kinzer <dtkinzer@gmail.com>
2019-02-04 13:43:38 -0500

    [BL-348] Implement Content DM API integration. (#948)
    * [BL-348] Implement Content DM API integration.
    
    REF BL-348
    
     ## Requirements
    
     * In the "Resource Type" column of the bento box, add Resource type
     value for "Digital Collections"
    
     * This value should include the number of results that there are for
     that query in ContentDM, accessed via ContentDM API
    
     * The facet link itself should open the search results for ContentDM,
     with the basic query from the Library Search.
    
    * Update spec/models/search/solr/response_spec.rb
    
    Co-Authored-By: dkinzer <dtkinzer@gmail.com>

---
+++ app/search_engines/bento_search/cdm_engine.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module BentoSearch
+  class CDMEngine
+    include BentoSearch::SearchEngine
+
+    def search_implementation(args)
+      query = args.fetch(:query, "")
+
+      # Avoid making a costly call for no reason.
+      if query.empty?
+        response = { "docs" => [] }
+      else
+        response = CDM::find(query)
+      end
+
+      results = BentoSearch::Results.new
+      results.total_items = response.dig("results", "pager", "total") || 0
+      results << BentoSearch::ResultItem.new(custom_data: response)
+
+      results
+    end
+  end
+end
