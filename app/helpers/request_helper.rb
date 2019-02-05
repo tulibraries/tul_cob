30f2a4813a86bcf4c52bc39e1b195bd2ff141501
Jen <tuh42082@temple.edu>
2019-02-05 15:43:11 -0500

    Updates styles for individual record pages (#953)

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -4,6 +4,6 @@ module RequestHelper
   include Blacklight::CatalogHelperBehavior
 
   def request_modal(mms_id, pickup_locations, request_level)
-    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn btn-primary request-button search-results-request-btn", data: { "blacklight-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
+    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn btn-sm btn-primary request-button search-results-request-btn", data: { "blacklight-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
   end
 end
