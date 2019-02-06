096d2d3adc2fcd01140afd5ffee0a98831d44503
David Kinzer <dtkinzer@gmail.com>
2019-02-06 13:42:21 -0500

    [BL-438] Sort facet items on merges. (#952)
    * [BL-438] Resort facet items on merges.
    
    If we do not sort a merged facet item then the item will just appear at
    the end of the list.  By providing a default sorting order with the
    possibility to overriding we can merge new facets without braking the
    expected behavior.
    
    * Fix fail when facet not defined.
    
    * Fix merge fails when config not provided.
    
    * Do not override the global CatalogController configuration.

--- spec/models/search/solr/response_spec.rb
+++ spec/models/search/solr/response_spec.rb
@@ -5,10 +5,11 @@ require "rails_helper"
 RSpec.describe Search::Solr::Response, type: :model do
 
   describe ".merge_facet" do
-    let(:response) { Search::Solr::Response.new(facet_counts, {}, {}) }
+    let(:config) { Blacklight::Configuration.new }
+    let(:response) { Search::Solr::Response.new(facet_counts, {}, { blacklight_config: config }) }
     let(:facet) { { name: "foo", value: "bar", hits: 1 } }
 
-    before  do
+    before(:each)  do
       response.merge_facet(facet)
     end
 
@@ -21,18 +22,39 @@ RSpec.describe Search::Solr::Response, type: :model do
     context "facet exists but field does not exist" do
       let(:facet) { { name: "cat", value: "bar", hits: 1 } }
 
-      it "appends the new field name and value" do
-        expect(response.facet_fields["cat"]).to eq(["memory", 3, "card", 2, "bar", 1])
+      it "merges the new field name and sorts by value asc" do
+        expect(response.facet_fields["cat"]).to eq(["bar", 1, "card", 2, "memory", 3])
       end
     end
 
     context "facet exists and field exists" do
       let(:facet) { { name: "cat", value: "memory", hits: 4 } }
 
-      it "appends the new field name and value and aggregations uses new value" do
-        expect(response.aggregations["cat"].items.count).to eq(2)
-        expect(response.aggregations["cat"].items.first.value).to eq("memory")
-        expect(response.aggregations["cat"].items.first.hits).to eq(4)
+      it "merges the new field overriding the old value" do
+        expect(response.facet_fields["cat"]).to eq(["card", 2, "memory", 4])
+      end
+    end
+
+    context "when a sorting procedure is provided" do
+      let(:facet) { { name: "cat", value: "bar", hits: 1 } }
+      let(:config) {
+        bc = Blacklight::Configuration.new
+        bc.add_facet_field "cat", sort_proc: -> (f) { (_, h) = f; -h }
+        bc
+      }
+
+      it "uses the defining sorting proc to do the sorting" do
+        expect(response.facet_fields["cat"]).to eq(["memory", 3, "card", 2, "bar", 1])
+      end
+    end
+
+    context "mutiple merges on same field" do
+      it "uses the last merged values"  do
+        response.merge_facet(name: "cat", value: "bar", hits: 2)
+        response.merge_facet(name: "cat", value: "bar", hits: 3)
+        response.merge_facet(name: "cat", value: "bar", hits: 4)
+
+        expect(response.facet_fields["cat"]).to eq(["bar", 4, "card", 2, "memory", 3])
       end
     end
 
