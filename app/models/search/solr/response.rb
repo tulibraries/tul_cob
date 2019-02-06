df1e1d8de584df12c64bf5f28352f601f5fbf532
David Kinzer <dtkinzer@gmail.com>
2019-02-06 13:42:21 -0500

    [BL-438] Sort facet items on merges. (#952)
    * [BL-438] Resort facet items on merges.
    
    If we do not sort a merged facet item then the item will just appear at
    the end of the list.  By providing a default sorting order with the
    possibility to overriding we can merge new facets without braking the
    expected behavior.
    
    * Fix fail when facet not defined.
    
    * Fix merge fails when config not provided.
    
    * Do not override the global CatalogController configuration.

--- app/models/search/solr/response.rb
+++ app/models/search/solr/response.rb
@@ -4,7 +4,17 @@ module Search::Solr
   class Response < Blacklight::Solr::Response
     def merge_facet(name:, value:, hits: nil)
       if self.dig("facet_counts", "facet_fields", name)
-        self["facet_counts"]["facet_fields"][name].append(value, hits)
+        # We need to sort on merge or facet item always appends to list.
+        config = blacklight_config&.facet_fields || {}
+
+        sort_proc = config.dig(name, "sort_proc") ||
+          -> (f) { (v, _) = f; v.titleize }
+
+        merged = Hash[*facet_fields[name]]
+          .merge(value => hits).sort_by(&sort_proc)
+          .to_a.flatten
+
+        self["facet_counts"]["facet_fields"][name] = merged
       else
         self["facet_counts"]["facet_fields"][name] = [ value, hits ]
       end
