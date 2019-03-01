7bcc8a2daa1e371d6bee30fe80cef8754585f1d2
David Kinzer <dtkinzer@gmail.com>
2019-03-01 14:56:50 -0500

    [BL-814] Fix bookmark not working from book or journal. (#1030)
    REF BL-814
    REF BL-822
    
    Having books and journal documents be represented by different models
    SolrBookDocument and SolrJournalDocument that derive from the
    SolrDocument is causing both the bookmark and email functionality for
    these documents to fail when actions occur from the bookmark or journal
    index pages.
    
    This is partly do to a misconfiguration.  Although we need these models
    in order to have correct paths .. they should be equal to SolrDocument
    not derive from.
    
    Updating them to equal SolrDocument fixes both the issues with
    bookmarking and the issue with sending emails.
    
     ## Depends on
    
    * Also adds temporary rake task to fix old data in the bookmarks table.
    - [ ] This rake task needs to be run in order for fix to take full effect

--- app/models/solr_book_document.rb
+++ app/models/solr_book_document.rb
@@ -1,13 +1,3 @@
 # frozen_string_literal: true
 
-class SolrBookDocument < SolrDocument
-  use_extension(Blacklight::Document::Email)
-  use_extension(Blacklight::Document::Sms)
-
-  # Add Blacklight-Marc extension:
-  SolrBookDocument.use_extension(Blacklight::Solr::Document::Marc) do |doc|
-    doc.has_field? "marc_display_raw"
-  end
-  SolrBookDocument.extension_parameters[:marc_source_field] = "marc_display_raw"
-  SolrBookDocument.extension_parameters[:marc_format_type] = :marcxml
-end
+SolrBookDocument = SolrDocument
