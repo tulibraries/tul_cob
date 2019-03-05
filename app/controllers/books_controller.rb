36fe4355e38b80384043592a1317ad3b63280e76
David Kinzer <dtkinzer@gmail.com>
2019-03-05 08:53:05 -0500

    [BL-814] Override the default catalog URL. (#1033)
    Blacklight has a way to override the default routing behavior for
    indexed document via configurations. @see
    Blacklight::SearchState.url_for_document. This commit updates the
    configurations for books_controller and journals_controller to point
    index urls to the right place.  # Please enter the commit message for
    your changes. Lines starting # with '#' will be ignored, and an empty
    message aborts the commit.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < CatalogController
   add_breadcrumb "Record", :solr_book_document_path, only: [ :show ]
 
   configure_blacklight do |config|
+    config.show.route = { controller: "books" }
     config.search_builder_class = BooksSearchBuilder
     config.document_model = ::SolrBookDocument
     # Do not allow any further filtering on type.
