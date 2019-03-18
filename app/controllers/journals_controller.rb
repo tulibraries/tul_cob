e6c3fab12c862aea66ff3a2fde93a31091dc08a2
David Kinzer <dtkinzer@gmail.com>
2019-03-18 16:05:20 -0400

    Remove twilio from app. (#1061)
    * Remove twilio from app.
    
    * Remove twilio configuration.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "twilio-ruby"
-
 class JournalsController < CatalogController
   add_breadcrumb "Journals", :back_to_journals_path, options: { id: "breadcrumbs_journal" }, only: [ :show ]
   add_breadcrumb "Record", :solr_journal_document_path, only: [ :show ]
