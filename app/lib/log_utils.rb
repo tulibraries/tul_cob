94e0c02bc40ea26f4a38e899ecbca73624fa9a40
David Kinzer <dtkinzer@gmail.com>
2019-03-19 15:45:29 -0400

    [BL-820] Add referer user-agent to api request logs. (#1066)
    REF BL-820
    See also tulibraries/tul_cob_playbook#48

--- app/lib/log_utils.rb
+++ app/lib/log_utils.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 module LogUtils
+  # Rails limits access to the request context to controllers.
+  thread_mattr_accessor :request
+
   def self.json_request_logger(logger, params = {})
     start = params.delete(:start)
     now = Time.now
     params[:timestamp] = now
-
+    # It's still possible for request to be nil in batch context.
+    # (i.e. when this module is used outside of Rails)
+    params[:referer] = request.referer if request
+    params[:user_agent] = request.user_agent if request
     params[:duration] = (now - start) if start
+
     logger.info JSON.dump(params)
   end
 end
