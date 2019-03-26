b10afeb0cd8038971a17204138b44778a3ad393e
sensei100 <tuh42082@temple.edu>
2019-03-26 14:21:55 -0400

    Adding stimulus back in

--- app/javascript/packs/controllers/availability_controller.js
+++ app/javascript/packs/controllers/availability_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
     this.buttonTarget.classList.toggle("collapsed");
 
     if (!this.buttonTarget.classList.contains("clicked")) {
-      // $(this.spinnerTarget).show();
+      $(this.spinnerTarget).show();
       fetch(this.data.get("url"), {
         credentials: "same-origin",
         headers: {
@@ -21,8 +21,8 @@ export default class extends Controller {
       })
         .then(response => response.text())
         .then(html => {
-          // $(this.spinnerTarget).remove();
-          // this.panelTarget.innerHTML = html
+          $(this.spinnerTarget).remove();
+          this.panelTarget.innerHTML = html
           this.buttonTarget.classList.add("clicked")
           $(this.panelTarget).parent().removeClass("hidden");
           $(this.requestTarget).removeClass("hidden");
