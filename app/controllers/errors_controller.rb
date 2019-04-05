b38af6cfa96c07f1e6964eb2c2a38041a6e94295
David Kinzer <dtkinzer@gmail.com>
2019-04-05 12:54:38 -0400

    [BL-853] Do not render layout for almaws errors.
    REF BL-853
    
    almaws controller items are all for ajax so errors should not render the
    full layout.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,10 +2,15 @@
 
 class ErrorsController < ApplicationController
   def not_found
-    render(status: 404)
+    render(layout(status: 404))
   end
 
   def internal_server_error
-    render(status: 500)
+    render(layout(status: 500))
   end
+
+  private
+    def layout(options)
+      request.env["REQUEST_PATH"].match?(/^\/almaws/) ? options.merge(layout: false) : options
+    end
 end
