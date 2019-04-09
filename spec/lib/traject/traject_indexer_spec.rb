0cd65e208b0e8dad449388fe91cfdabc74a4d1c0
Jen <tuh42082@temple.edu>
2019-04-08 21:32:45 -0400

    Bl 872 asrs mappings (#1108)
    * BL-872 ASRS configurations
    
    - Change library and location mappings for asrs items
    - Update suppression methods
    
    * BL-872 ASRS requests
    
    - Refactor campus logic to include ASRS library
    
    * Fix suppress when multiple items present but one is UNASSIGNED.

--- spec/lib/traject/traject_indexer_spec.rb
+++ spec/lib/traject/traject_indexer_spec.rb
@@ -885,6 +885,12 @@ RSpec.describe Traject::Macros::Custom do
         expect(subject.map_record(records[5])).to eq("suppress_items_b" => [true])
       end
     end
+
+    context "when multiple item fields but single unassinged" do
+      it "doesn't suppress this file" do
+        expect(subject.map_record(records[6])).to eq({})
+      end
+    end
   end
 
   describe "full reindex #suppress_items" do
