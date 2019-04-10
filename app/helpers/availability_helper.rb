5c6c9f7b9c0d570f4a18c5832a7f8edc5b7fd02b
David Kinzer <dtkinzer@gmail.com>
2019-04-10 12:55:43 -0400

    [BL-872] Hide the ASRS_TEST location.

--- app/helpers/availability_helper.rb
+++ app/helpers/availability_helper.rb
@@ -83,7 +83,7 @@ module AvailabilityHelper
 
   def unwanted_library_locations(item)
     location = item.fetch("current_location", "")
-    !!location.match(/techserv|UNASSIGNED|intref/) || library(item) == "EMPTY"
+    !!location.match(/techserv|UNASSIGNED|intref|ASRS_TEST/) || library(item) == "EMPTY"
   end
 
   def library(item)
