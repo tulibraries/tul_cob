9b398eaf26fd2ab3df7542ced7e4a0dfda2adea1
Jen <tuh42082@temple.edu>
2019-04-22 13:42:56 -0400

    Bl 861 recreate search faceting (#1140)
    * BL-861 Bento style updates
    
    - Add new icons to bento compartments
    
    * [BL-861] Remove facet bento box but keep @response.
    
    * - order compartments
    - more styles for compartments
    
    * Fix alignment for FAQ link
    
    * add resource types back into bento view
    
    * BL-861 Refactor and style resource types
    
    * Fix cdm link broken.
    
    * BL-861 Update resource type icons
    
    * Fix rubocop offenses and remove failing spec for method no longer used
    
    * Add test for process_results method.
    
    * Also fixes broken render_facet_value method for cdm link not opening
    to new tab.
    
    * Remove books section.
    
    * Refactor More to be Books and Media
    
    * Adds total results number to the catalog link for books_and_media
    
    * Add test case when only cdm results present.
    
    * Remove unused code.
    
    * Remove code related to the books bento search that we are no longer using

--- spec/search_engines/books_and_media_search_spec.rb
+++ spec/search_engines/books_and_media_search_spec.rb
@@ -4,13 +4,13 @@ require "rails_helper"
 
 RSpec.describe BentoSearch, type: :search_engine do
 
-  let(:search_engine)  { BentoSearch.get_engine("more") }
+  let(:search_engine)  { BentoSearch.get_engine("books_and_media") }
 
   let(:search_results) { VCR.use_cassette("bento_search_more") { search_engine.search("food") } }
 
   let(:expected_fields) { RSpec.configuration.bento_expected_fields }
 
-  describe "Bento  More Search Engine" do
+  describe "Bento Books and Media Search Engine" do
     let (:item) { search_results[0] }
 
     it "sets custom_data to a Blackligh::Solr::Response" do
@@ -30,17 +30,17 @@ RSpec.describe BentoSearch, type: :search_engine do
     end
   end
 
-  describe "#proc_minus_books_journals" do
+  describe "#proc_minus_journals" do
     let(:controller) { CatalogController.new }
     let(:builder) { SearchBuilder.new(controller) }
 
     it "does not affect builder.proccessor_chain automatically" do
-      expect(builder.processor_chain).not_to include(:no_books_or_journals)
+      expect(builder.processor_chain).not_to include(:no_journals)
     end
 
-    it "Appends :no_books_or_journals processor to processor_chain" do
-      _builder = search_engine.proc_minus_books_journals[builder]
-      expect(_builder.processor_chain).to include(:no_books_or_journals)
+    it "Appends :no_journals processor to processor_chain" do
+      _builder = search_engine.proc_minus_journals[builder]
+      expect(_builder.processor_chain).to include(:no_journals)
     end
   end
 
