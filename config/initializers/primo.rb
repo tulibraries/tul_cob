5c3ecc5b35f2e38bb562cd9c6264bb638b926459
David Kinzer <dtkinzer@gmail.com>
2019-05-08 11:08:52 -0400

    [Bl-877] Update alma and timeout.
    * Updates alma to use new version that limits  offset to < 500.
    * Updates timeout to 10 seconds for primo and alma api (because we
    noticed that locally we were timing out when it was set to default of
    5 seconds).

--- config/initializers/primo.rb
+++ config/initializers/primo.rb
@@ -6,4 +6,5 @@ Primo.configure do |config|
   config.vid     = "TULI"
   config.scope   = "pci_scope"
   config.enable_loggable = true
+  config.timeout = Rails.configuration.bento[:primo][:timeout] || 10
 end
