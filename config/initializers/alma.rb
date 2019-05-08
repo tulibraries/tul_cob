1cd9e111c468a0c599570b9867028b058b453393
David Kinzer <dtkinzer@gmail.com>
2019-05-08 11:35:22 -0400

    [Bl-877] Update alma and timeout. (#1183)
    * Updates alma to use new version that limits  offset to < 500.
    * Updates timeout to 10 seconds for primo and alma api (because we
    noticed that locally we were timing out when it was set to default of
    5 seconds).

--- config/initializers/alma.rb
+++ config/initializers/alma.rb
@@ -4,6 +4,7 @@ Alma.configure do |config|
   # You have to set te apikey
   config.apikey = Rails.configuration.alma[:apikey]
   config.enable_loggable = true
+  config.timeout = Rails.configuration.alma[:timeout] || 10
 end
 ENV["ALMA_API_KEY"] ||= Rails.configuration.alma[:apikey]
 ENV["ALMA_DELIVERY_DOMAIN"] ||= Rails.configuration.alma[:delivery_domain]
