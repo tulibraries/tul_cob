05653230de0e7317cbcfce23e2f7d39938aa46b8
Christina Harlow <cmharlow@gmail.com>
2019-06-10 17:06:46 -0400

    updating travis to deploy to stage & qa

---
+++ .travis/deploy-stage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd ..
+git clone --single-branch --branch master git@github.com:tulibraries/tul_cob_playbook.git # clone deployment playbook
+cd tul_cob_playbook
+sudo pip install pipenv # install playbook requirements
+pipenv install # install playbook requirements
+pipenv run ansible-galaxy install -r requirements.yml # install playbook role requirements
+cp .circleci/.vault ~/.vault # setup vault password retrieval from travis envvar
+chmod +x ~/.vault  # setup vault password retrieval from travis envvar
+
+# deploy to qa using ansible-playbook
+pipenv run ansible-playbook -i inventory/stage/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private-key=~/.ssh/.conan_the_deployer
