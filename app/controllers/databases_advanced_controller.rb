226a9d3e026843eeca25a1c7b66b0dfefb23ab6f
David Kinzer <dtkinzer@gmail.com>
2019-07-01 13:39:44 -0400

    [Bl-809] Update database advanced search facets. (#1280)
    * Do not show the subject facet.
    * Label the format facet to match regular search.

--- app/controllers/databases_advanced_controller.rb
+++ app/controllers/databases_advanced_controller.rb
@@ -3,6 +3,11 @@
 class DatabasesAdvancedController < AdvancedController
   copy_blacklight_config_from(DatabasesController)
 
+  configure_blacklight do |config|
+    config.facet_fields = ActiveSupport::OrderedHash.new
+    config.add_facet_field "az_format", field: "format", label: "Database Type"
+  end
+
   add_breadcrumb "Databases", :back_to_databases_path, options: { id: "breadcrumbs_databases" }
   add_breadcrumb I18n.t(:databases_advanced_search), :databases_advanced_search_path,
     only: [ :index ]
