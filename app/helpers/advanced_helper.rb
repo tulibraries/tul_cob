b125cc8eb1c4c720cabb1b40ffeac481a29b3d71
sensei100 <tuh42082@temple.edu>
2019-07-18 09:05:52 -0400

    - Adds header from website - Refactor advanced_link method to display on root_path

--- app/helpers/advanced_helper.rb
+++ app/helpers/advanced_helper.rb
@@ -84,6 +84,9 @@ module AdvancedHelper
     elsif current_page? everything_path
       id = :catalog_advanced_search
       url = advanced_search_path(query)
+    elsif current_page? root_path
+      id = :catalog_advanced_search
+      url = advanced_search_path(query)
     end
 
     link_to(t(id), url, class: "advanced_search", id: id) if id
