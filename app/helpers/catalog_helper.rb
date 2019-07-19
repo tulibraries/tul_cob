4a65df755bedc5e837d1d8ab554806e3ebb85ffa
sensei100 <tuh42082@temple.edu>
2019-07-19 11:54:30 -0400

    BL-932 availability styles
    - Moves request button
    - Changes colors for online availability
    - Makes availability icons larger
    - Remove availability header text

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -317,7 +317,7 @@ module CatalogHelper
     item_html = [item_html, electronic_notes]
       .select(&:present?).join(" ").html_safe
 
-    content_tag(:div, item_html , class: " electronic_links online-list-items online-card")
+    content_tag(:div, item_html , class: " electronic_links online-list-items online-card border-top-0")
   end
 
   def render_electronic_notes(field)
