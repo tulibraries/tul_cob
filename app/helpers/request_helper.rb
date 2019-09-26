aca48b2ab820de5ba7a401ca00b0b02154349e60
sensei100 <tuh42082@temple.edu>
2019-09-26 11:11:31 -0400

    Fixes mistake in conditional

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -4,7 +4,7 @@ module RequestHelper
   include Blacklight::CatalogHelperBehavior
 
   def request_modal(mms_id, pickup_locations, request_level)
-    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn btn-sm btn-primary request-button float-right #{params[:action] == 'search-results-request-btn' ? 'search-results-request-btn' : 'record-page-request-btn'}", data: { "blacklight-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
+    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn btn-sm btn-primary request-button float-right #{params[:action] == 'index' ? 'search-results-request-btn' : 'record-page-request-btn'}", data: { "blacklight-modal": "trigger", "action": "availability#modal", "target": "availability.href" })
   end
 
   def ez_borrow_link_with_updated_query(url)
