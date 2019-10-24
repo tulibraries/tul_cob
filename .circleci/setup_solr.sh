a3a03a9a549bb1d6a7ffe964dc253ac0eca2f622
David Kinzer <dtkinzer@gmail.com>
2019-10-24 13:35:21 -0400

    Remove set -e and set docker run switch back to -d

--- .circleci/setup_solr.sh
+++ .circleci/setup_solr.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-set -e
-
 docker pull solr:$SOLR_VERSION
 docker run -p 8983:8983 \
   -v $PWD/solr/conf:/opt/solr/server/solr/configsets/default/conf \
-  --rm -it solr:$SOLR_VERSION bash \
+  -d solr:$SOLR_VERSION bash \
   -c "precreate-core az-database; precreate-core blacklight-core-dev; precreate-core web-content; exec solr -f"
 
 # Health Check the Solr Server
