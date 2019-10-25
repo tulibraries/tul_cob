e2aa705b2c607d75f18eb7f693c1c9b493b1b0e7
David Kinzer <dtkinzer@gmail.com>
2019-10-25 09:02:27 -0400

    Update node version the circleci way.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y libmysqlclient-dev nodejs
+# Add mysql deps so bundle install doesn't fail.
+sudo apt-get install -y libmysqlclient-dev
+
+# Update node version so yarn build doesn't fail.
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install v12.13.0 && nvm use v12.13.0 && nvm alias default v12.13.0
+node -v
 
 cp config/secrets.yml.example config/secrets.yml
 cp config/alma.yml.example config/alma.yml
