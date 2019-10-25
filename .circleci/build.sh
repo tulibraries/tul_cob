68562603ddd65cfcdf7cf8db9ca7b1388f0b0981
David Kinzer <dtkinzer@gmail.com>
2019-10-24 22:36:09 -0400

    Install mysql.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+sudo apt-get install libmysqlclient-dev -y
 cp config/secrets.yml.example config/secrets.yml
 cp config/alma.yml.example config/alma.yml
 cp config/bento.yml.example config/bento.yml
-RAILS_ENV=test bundle install --without production
+
+RAILS_ENV=test bundle install
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake db:seed
 EDITOR='vim -c wqa' bundle exec rails credentials:edit 2> /dev/null
