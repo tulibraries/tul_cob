c5e2855e937169b5226789148ea83c9fa09b989a
David Kinzer <dtkinzer@gmail.com>
2019-10-25 16:16:06 -0400

    Use all environment inventory changes on deploy. (#1548)
    I wont pretend like I know exactly what ansible magic is going on here
    but, this seems to make a difference when I deploy from my local.
    
    i.e. using `-i inventory/qa` instead of `-i inventory/qa/hosts`

--- .circleci/deploy-qa.sh
+++ .circleci/deploy-qa.sh
@@ -11,5 +11,5 @@ cp .circleci/.vault ~/.vault # setup vault password retrieval from travis envvar
 chmod +x ~/.vault  # setup vault password retrieval from travis envvar
 
 # deploy to qa using ansible-playbook
-pipenv run ansible-playbook -i inventory/qa/hosts playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private-key=~/.ssh/.conan_the_deployer --skip-tags "solr"
+pipenv run ansible-playbook -i inventory/qa playbook.yml --vault-password-file=~/.vault -e 'ansible_ssh_port=9229' --private-key=~/.ssh/.conan_the_deployer --skip-tags "solr"
 echo "BE AWARE THAT SOLR CONFIG CHANGES WERE NOT DEPLOYED AS PART OF THIS BUILD"
