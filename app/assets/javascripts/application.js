3a6dc5a079fe007af04fb24c9622739837bd84a6
sensei100 <tuh42082@temple.edu>
2019-12-13 15:40:58 -0500

    BL-1088 Blacklight modal bug
    - Changing our jQuery to 3 in the application.js file caused the modal functionality to stop working. Revert back to requiring only jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
 //= require 'blacklight_advanced_search'
 //= require rails-ujs
 //= require popper
