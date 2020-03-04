386ffd1fbf16ec4e3dc048f56b4909759097e7e4
sensei100 <tuh42082@temple.edu>
2020-03-04 16:13:34 -0500

    BL-1108 Begins with advanced search
    - The begins with operator does not currently work for all fields.
    - This hides the begins with operator when a user selects a field from the search options dropdown that does not support that functionality

--- app/javascript/packs/controllers/advanced_controller.js
+++ app/javascript/packs/controllers/advanced_controller.js
@@ -6,25 +6,25 @@ import { Controller } from "stimulus"
     }
 
 export default class extends Controller {
-  static targets = [ "operator" ]
+  static targets = [ "operator", "options" ]
 
-  connect() {
+  initialize() {
     $("option[value='begins_with']").hide();
   }
 
   select() {
-    let search_options_1 = $("#f_1 option:selected").text();
-    let search_options_2 = $("#f_2 option:selected").text();
-    let search_options_3 = $("#f_3 option:selected").text();
-
+    let count = $(event.currentTarget).data("count");
+    let select_id = $(event.currentTarget).attr("id");
+    let search_options = $(event.currentTarget).children("option:selected").text();
+    let begins_with = document.getElementById(`operator[q_${count}]`);
     let begins_with_options = ["Title", "Author/creator/contributor", "Subject", "Publisher", "Call Number", "Series Title"]
 
-    if(begins_with_options.includes(search_options_1)) {
-      console.log("HI")
-      $("option[value='begins_with']").show();
-      $("option[value='begins_with']").text("begins with");
-    } else {
-      $("option[value='begins_with']").hide();
+    if(select_id.includes(count)) {
+      if(begins_with_options.includes(search_options)) {
+        $(begins_with).children("option[value='begins_with']").show();
+      } else {
+        $(begins_with).children("option[value='begins_with']").hide();
+      }
     }
   }
 }
