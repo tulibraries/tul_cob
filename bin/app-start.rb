02ea07740c1b08a20fb137b842dbf09a0133bf7f
David Kinzer <dtkinzer@gmail.com>
2020-03-19 14:28:42 -0400

    Try running rails webpacker:compile

--- bin/app-start.rb
+++ bin/app-start.rb
@@ -20,6 +20,7 @@ File.delete server_pid if File.exist? server_pid
 `bundle install`
 
 # Start rails app but do not block the rest of the script.
+`rails webpacker:compile`
 `rails db:migrate`
 `yarn`
 exec("rails s -p 3000 -b '0.0.0.0'") if fork == nil
