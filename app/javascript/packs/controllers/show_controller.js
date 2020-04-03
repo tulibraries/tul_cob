2a6d5dc11ab968c7bc479d572420a3077def729f
Chad Nelson <chad.nelson@temple.edu>
2020-04-03 09:31:12 -0400

    CLean up some JS and CSS issues

--- app/javascript/packs/controllers/show_controller.js
+++ app/javascript/packs/controllers/show_controller.js
@@ -22,11 +22,12 @@ export default class extends Controller {
       .then(response => response.text())
       .then(html => {
         this.panelTarget.innerHTML = html
-        $("#requests-container").removeClass("hidden");
-        $('[data-long-list]').longList();
-        var mms_id = $("#record-view-iframe").data("availability-id");
-        var requests_url = $("#request-url-data-" + mms_id).data("requests-url");
-        $("#request-btn-" + mms_id).attr("href", requests_url);
+        // REMOVED WHILE LIBRARIES CLOSED
+        //$("#requests-container").removeClass("hidden");
+        //$('[data-long-list]').longList();
+        //var mms_id = $("#record-view-iframe").data("availability-id");
+        //var requests_url = $("#request-url-data-" + mms_id).data("requests-url");
+        //$("#request-btn-" + mms_id).attr("href", requests_url);
       })
   }
 
