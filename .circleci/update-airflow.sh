d61f5ee36f8e1972e4ab239af3a60847f838b5e3
David Kinzer <dtkinzer@gmail.com>
2020-06-04 12:19:45 -0400

    BL-1201 Update airflow catalog production solr collction.
    * On deployments to production make required airflow changes.

---
+++ .circleci/update-airflow.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Load App Envs
+source ./.env
+
+# Get Airflow Deployment Scripts.
+cd ..
+git clone --single-branch --branch master git@github.com:tulibraries/ansible-playbook-airflow.git
+cd ansible-playbook-airflow
+
+# Install requirements
+sudo pip install pipenv
+pipenv install
+pipenv run ansible-galaxy install -r requirements.yml # install playbook role requirements
+echo $ANSIBLE_VAULT_PASSWORD > ~/.vault
+
+# Run
+pipenv run ansible-playbook -i inventory/prod tul_cob.yml --private-key=~/.ssh/conan_the_deployer -e collection_name=$CATALOG_COLLECTION
