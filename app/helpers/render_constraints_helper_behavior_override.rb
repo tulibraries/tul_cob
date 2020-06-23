9fc46293614690d9098725d3e8566c57f0dc5188
David Kinzer <dtkinzer@gmail.com>
2020-06-23 16:11:44 -0400

    BL-1171: Update blacklight_range_limit.
    * Updates blacklight_range_limit to latest version.
    * Patches JS issue.
    * Hides remove button (to keep current behavior)
    * Patches render_constraints_filters method (projectblacklight/blacklight_range_limit#152)

--- app/helpers/render_constraints_helper_behavior_override.rb
+++ app/helpers/render_constraints_helper_behavior_override.rb
@@ -19,4 +19,26 @@ module RenderConstraintsHelperBehaviorOverride
                                 classes: ["filter", "filter-" + facet.parameterize])
     end, "\n")
   end
+  ##
+  # Override with v7.5.0 version do to bug.
+  #
+  # @see https://github.com/projectblacklight/blacklight_range_limit/issues/152
+  # #
+  def render_constraints_filters(my_params = params)
+    content = super(my_params)
+    # add a constraint for ranges?
+    if my_params[:range].present? && my_params[:range].respond_to?(:each_pair)
+      my_params[:range].each_pair do |solr_field, hash|
+
+        next unless hash["missing"] || (!hash["begin"].blank?) || (!hash["end"].blank?)
+        content << render_constraint_element(
+          facet_field_label(solr_field),
+          range_display(solr_field, my_params),
+          escape_value: false,
+          remove: remove_range_param(solr_field, my_params)
+        )
+      end
+    end
+    return content
+  end
 end
