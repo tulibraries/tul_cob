dcffb0c449a373ac9cd9e2930b471d9e940a91ae
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-07-02 09:56:26 -0400

    test that resource type facets have class they need to render icons

--- spec/features/home_facets_spec.rb
+++ spec/features/home_facets_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.feature "Home Facets" do
 
   describe "Facets rendered" do
-    it "does not render facets on hompe pages only on search pages" do
+    it "does not render facets on home pages only on search pages" do
       visit "/catalog"
       home_facets = page.all(".facet-values li").length
 
@@ -14,6 +14,12 @@ RSpec.feature "Home Facets" do
 
       search_facets = page.all(".facet-values li").length
       expect(home_facets).to be < search_facets
+    end
+
+    it "assigns a class to resource type facets" do
+      visit "/catalog?search_field=all_fields&q=test"
+      expect(page).to have_current_path("/catalog?search_field=all_fields&q=test")
+      expect(page).to have_css(".facet_select.facet_book")
     end
   end
 end
