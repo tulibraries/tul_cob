f5f1effb52366742b9235e6f5ca55c5f32e54d6a
David Kinzer <dtkinzer@gmail.com>
2020-07-13 10:46:54 -0400

    BL-1248: Make sure query is URL encoded.
    Fixes URI::InvalidURIError in errors # internal_server_error in CDM
    search context.

--- spec/search_engines/cdm_seach_spec.rb
+++ spec/search_engines/cdm_seach_spec.rb
@@ -4,15 +4,26 @@ require "rails_helper"
 
 RSpec.describe "cdm search engine", type: :search_engine do
 
-  let(:search_results) { BentoSearch.get_engine("cdm").search("foo") }
+  let(:query) { "foo" }
+  let(:search_results) { BentoSearch.get_engine("cdm").search(query) }
   let(:content_dm_results) { { "results" => { "pager" => { "total" => "415" } } } }
 
-  it "sets the total found items" do
+  before do
     stub_request(:get, /contentdm/)
       .to_return(status: 200,
     headers: { "Content-Type" => "application/json" },
     body: JSON.dump(content_dm_results))
+  end
 
+  it "sets the total found items" do
     expect(search_results.total_items).to eq("415")
+  end
+
+  context "non ASCII query" do
+    let(:query) { "Read Myron H. Dembo. 2004. Motivation and Learning Strategies for College Success: A Self-Management Approach, Chapter 1, “Academic Self-Management,” 3-17." }
+
+    it "handles non asci queries" do
+      expect(search_results.total_items).to eq("415")
+    end
   end
 end
