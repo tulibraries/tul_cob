ea0b5468a957d589e92507483f991bdffa681321
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-07-20 13:37:54 -0400

    ensure that multi-valued fields on show page are rendered as links

--- app/presenters/show_presenter.rb
+++ app/presenters/show_presenter.rb
@@ -3,12 +3,14 @@
 class ShowPresenter < Blacklight::ShowPresenter
   def each_primary_field
     fields_to_render do |field_name, field_config, field_presenter|
+      field_presenter.except_operations << Blacklight::Rendering::Join
       yield field_name, field_config, field_presenter if field_config[:type] == :primary
     end
   end
 
   def each_secondary_field
     fields_to_render.each do |field_name, field_config, field_presenter|
+      field_presenter.except_operations << Blacklight::Rendering::Join
       yield field_name, field_config, field_presenter unless field_config[:type] == :primary
     end
   end
