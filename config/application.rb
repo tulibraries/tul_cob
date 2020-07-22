27809fbebbc34b956a980976020d0de9db921f6c
David Kinzer <dtkinzer@gmail.com>
2020-07-22 12:06:10 -0400

    Remove secrets from Honeybaddger error messages.

--- config/application.rb
+++ config/application.rb
@@ -49,3 +49,18 @@ module Tulcob
     #config.log_level = :debug
   end
 end
+
+Honeybadger.configure do |config|
+  secrets = {
+    solrcloud_user: ENV["SOLRCLOUD_USER"],
+    solrcloud_password: ENV["SOLRCLOUD_PASSWORD"],
+    primo_apikey: Rails.configuration.bento.dig("primo", "apikey"),
+    alma_apikey: Rails.configuration.alma["apikey"],
+  }
+
+  config.before_notify do |notice|
+    secrets.each do |secret_name, secret_value|
+      notice.error_message.gsub!(secret_value, "[:#{secret_name}]") unless secret_value.blank?
+    end
+  end
+end
