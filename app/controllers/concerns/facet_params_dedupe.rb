3e5d8f8fb248a1b160fedc07b388faa72aca6564
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-08-14 11:36:42 -0400

    rubocop errors

--- app/controllers/concerns/facet_params_dedupe.rb
+++ app/controllers/concerns/facet_params_dedupe.rb
@@ -5,8 +5,8 @@ module FacetParamsDedupe
     # Converts facet params like ?f[foo]=bar&f[foo]=bar to ?f[foo]=bar
     before_action do
       if params["f"]
-        facet_params = params.extract!('f')
-        params.merge!(f: facet_params["f"].transform_values {|v| v.is_a?(Array) ? v.uniq : v })
+        facet_params = params.extract!("f")
+        params.merge!(f: facet_params["f"].transform_values { |v| v.is_a?(Array) ? v.uniq : v })
       end
     end
   end
