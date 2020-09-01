380e959b8fb074617a52aaa86bd0230609030e68
tulibraries-devops <dtkinzer@gmail.com>
2020-09-01 13:53:39 -0400

    BL-1297: Add export article bookmarks button.
    Add temporary feature add allow users to export their bookmarks.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,4 +5,17 @@ class BookmarksController < CatalogController
   include Blacklight::Bookmarks
   include MultiSourceBookmarks
   include BookmarksConfig
+
+
+  def export_articles
+    bookmarks = current_or_guest_user.bookmarks
+      .where(document_type: "PrimoCentralDocument")
+
+    urls = bookmarks
+      .map { |b| "https://librarysearch.temple.edu/articles/#{b.document_id}" }
+
+    bookmarks.update_all("title = 'exported'")
+
+    send_data urls.join("\n"), filename: "article_bookmark_urls.txt"
+  end
 end
--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -101,4 +101,31 @@ RSpec.describe BookmarksController do
       expect(response.code).to eq "500"
     end
   end
+
+  describe "export_articles" do
+    it "exports urls" do
+      @controller.send(:current_or_guest_user).bookmarks.create! document_id: "foo", document_type: "PrimoCentralDocument"
+      @controller.send(:current_or_guest_user).bookmarks.create! document_id: "bar", document_type: "PrimoCentralDocument"
+
+      get :export_articles
+      # We successfully export test file
+      expect(response).to be_successful
+      expect(response.content_type).to eq("text/plain")
+
+      description = response["Content-Disposition"]
+      expect(description).to eq("attachment; filename=\"article_bookmark_urls.txt\"")
+
+      # We export list of URLs
+      urls = <<~EOT
+        https://librarysearch.temple.edu/articles/foo
+        https://librarysearch.temple.edu/articles/bar
+      EOT
+      expect(response.body).to eq(urls.strip)
+
+      # We keep track of exports
+      title_update = @controller.send(:current_or_guest_user).bookmarks.first.title
+      expect(title_update).to eq("exported")
+    end
+  end
+
 end
