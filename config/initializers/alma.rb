b81d5b2b659fb1832f1ddc77808940977e3c89b5
dkinzer <dtkinzer@gmail.com>
2020-10-13 09:57:02 -0400

    Allow alma apikey env override.

--- config/initializers/alma.rb
+++ config/initializers/alma.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-Alma.configure do |config|
-  # You have to set te apikey
-  config.apikey = Rails.configuration.alma[:apikey]
-  config.enable_loggable = true
-  config.timeout = Rails.configuration.alma[:timeout] || 30
-end
 ENV["ALMA_API_KEY"] ||= Rails.configuration.alma[:apikey]
 ENV["ALMA_DELIVERY_DOMAIN"] ||= Rails.configuration.alma[:delivery_domain]
 ENV["ALMA_INSTITUTION_CODE"] ||= Rails.configuration.alma[:institution_code]
 ENV["ALMA_AUTH_SECRET"] ||= Rails.configuration.alma[:auth_secret]
+
+Alma.configure do |config|
+  # You have to set te apikey
+  config.apikey = ENV["ALMA_API_KEY"]
+  config.enable_loggable = true
+  config.timeout = Rails.configuration.alma[:timeout] || 30
+end
