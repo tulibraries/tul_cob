07468e41cc5f5f1890ea0c633ac753d7d0ff3207
dkinzer <dtkinzer@gmail.com>
2020-10-27 07:43:39 -0400

    BL-1353: Allow format=json for lib_guides enpoint

--- app/controllers/lib_guides_controller.rb
+++ app/controllers/lib_guides_controller.rb
@@ -3,6 +3,12 @@
 class LibGuidesController < ApplicationController
   def index
     @guides = LibGuidesApi.fetch(params[:q]).as_json
-    render layout: false
+
+    respond_to do |format|
+      format.html { render layout: false }
+      format.json do
+        render plain: @guides, status: 200, content_type: "application/json"
+      end
+    end
   end
 end
