7cf2fbcba73e27a259a9f7b016fe3e2f742cd7f4
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-12-07 15:20:20 -0500

    BL-1373 conditionally render lc classification metadata

---
+++ app/presenters/index_presenter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class IndexPresenter < Blacklight::IndexPresenter
+  def fields_to_render
+    return super unless block_given?
+    super do |field_name, field_config, field_presenter|
+      yield field_name, field_config, field_presenter unless field_name == "lc_call_number_display"
+    end
+  end
+
+  def lc_call_number_field_to_render
+    return unless fields["lc_call_number_display"] && block_given?
+    field_presenter = field_presenter(fields["lc_call_number_display"])
+    yield "lc_call_number_display", fields["lc_call_number_display"], field_presenter if field_presenter.render_field?
+  end
+end
