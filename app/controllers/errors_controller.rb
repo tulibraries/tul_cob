f2a3e9c1513a8cecc3d6369c44157ce5f063fe3f
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-12-09 09:56:14 -0500

    return empty body for asset 404s

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,12 @@
 
 class ErrorsController < ApplicationController
   def not_found
-    render(layout(status: 404, formats: :html))
+    respond_to do |format|
+      format.woff2 { render plain: "", status: 400, content_type: "text/plain" }
+      format.css { render plain: "", status: 400, content_type: "text/plain" }
+      format.js { render plain: "", status: 400, content_type: "text/plain" }
+      format.all { render(layout(status: 404, formats: :html)) }
+    end
   end
 
   def internal_server_error
