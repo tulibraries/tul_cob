55312f9dc972f98babd391301cad6122656f9333
sensei100 <tuh42082@temple.edu>
2020-12-10 11:53:12 -0500

    BL-1375 Call number feature flag
    - Removes the call number feature flag

--- spec/features/indices_spec.rb
+++ spec/features/indices_spec.rb
@@ -36,7 +36,7 @@ RSpec.feature "Indices" do
     }
     context "searching shows all facets" do
       scenario "User searches catalog" do
-        visit "/catalog?with_libguides=true&with_call_number_facet=true"
+        visit "/catalog?with_libguides=true"
         fill_in "q", with: "*"
         click_button "search"
         facet_headings = page.all(".facet-field-heading")
@@ -118,7 +118,7 @@ RSpec.feature "Indices" do
 
   feature "Sorting" do
     scenario "User requests results sorted by LC call number" do
-      visit "/catalog?q=&q=LCSORT+TEST&rows=20&sort=lc_call_number_sort+asc%2C+pub_date_sort+desc&with_call_number_facet=true"
+      visit "/catalog?q=&q=LCSORT+TEST&rows=20&sort=lc_call_number_sort+asc%2C+pub_date_sort+desc"
       expect(page.all(".documents-list .document").size).to eq 13
       (1..13).each do |i|
         expect(page.find(".document-position-#{i - 1} h3 a").native.attr("href")).to eq "/catalog/LCSORT_#{i.ordinalize.upcase}"
@@ -126,7 +126,7 @@ RSpec.feature "Indices" do
     end
 
     scenario "User requests results reverse sorted by LC call number" do
-      visit "/catalog?q=&q=LCSORT+TEST&rows=20&sort=lc_call_number_sort+desc%2C+pub_date_sort+desc&with_call_number_facet=true"
+      visit "/catalog?q=&q=LCSORT+TEST&rows=20&sort=lc_call_number_sort+desc%2C+pub_date_sort+desc"
       expect(page.all(".documents-list .document").size).to eq 13
       (1..13).each do |i|
         expect(page.find(".document-position-#{13 - i} h3 a").native.attr("href")).to eq "/catalog/LCSORT_#{i.ordinalize.upcase}"
