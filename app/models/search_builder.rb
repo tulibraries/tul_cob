17ea9cd4308704a1c6987c73f0ba87bd5d200c7c
David Kinzer <dtkinzer@gmail.com>
2021-03-29 09:48:31 -0400

    Revert "BL-1421: Always supress journals in book media context."

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -14,8 +14,8 @@ class SearchBuilder < Blacklight::SearchBuilder
         add_advanced_search_to_solr
         add_lc_range_search_to_solr
         spellcheck
-        limit_facets
         filter_suppressed
+        limit_facets
         sorting_preferences ]
 
   if ENV["SOLR_SEARCH_TWEAK_ENABLE"] == "on"
@@ -108,6 +108,10 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   def substitute_special_chars(value, _)
     value.gsub(/([:?]|\(\))/, " ") rescue value
+  end
+
+  def no_journals(solr_parameters)
+    solr_parameters["fq"] = ["!format:Journal/Periodical"]
   end
 
   ##
