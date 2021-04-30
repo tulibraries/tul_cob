5ce0021a7aba4ff17b5afbc6f96a1af70e7c22fa
David Kinzer <dtkinzer@gmail.com>
2021-04-30 14:48:17 -0400

    wip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,19 @@ require "vcr"
 require "database_cleaner"
 require "capybara/rspec"
 require "pry"
+require "simplecov-lcov"
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start "rails" do
+  # Code from other repositories
+  add_filter "/lib/alma_rb/"
+  add_filter "/lib/alma-blacklight/"
+  add_filter "/app/models/marc_indexer.rb"
+  add_filter "/app/views/"
+  add_filter "/app/channels/"
+  add_filter "/spec/"
+end
 
 WebMock.disable_net_connect!(allow_localhost: true)
 
