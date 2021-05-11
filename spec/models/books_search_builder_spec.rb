74affa759a95bce9b06e9dad7bad2efadb4a9c0c
David Kinzer <dtkinzer@gmail.com>
2021-05-11 08:55:35 -0400

    BL-1453: Always suppress journals in book media context.
    * Fixes books media bento totals not matching books media search totals.

---
+++ spec/models/books_search_builder_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe BooksSearchBuilder , type: :model do
+  let(:context) { CatalogController.new }
+  let(:params) { ActionController::Parameters.new }
+  let(:search_builder) { BooksSearchBuilder.new(context) }
+
+  subject { search_builder }
+
+  describe "#no_journals" do
+    before(:example) do
+      allow(search_builder).to receive(:blacklight_params).and_return(params)
+      subject.no_journals(solr_parameters)
+    end
+
+    context "default" do
+      let(:solr_parameters) { Blacklight::Solr::Request.new }
+
+      it "adds suppression to fq" do
+        expect(solr_parameters["fq"]).to eq(["!format:Journal/Periodical"])
+      end
+    end
+
+    context "when fq already exists" do
+      let(:solr_parameters) { Blacklight::Solr::Request.new(fq: ["foo"]) }
+
+      it "adds suppression to fq" do
+        expect(solr_parameters["fq"]).to eq(["foo", "!format:Journal/Periodical"])
+      end
+    end
+  end
+end
