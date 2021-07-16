b6fe10578f6675141757e0815914df6e0937f48f
sensei100 <tuh42082@temple.edu>
2021-07-16 13:29:08 -0400

    Add feature flag param to feature specs

--- spec/features/query_list_spec.rb
+++ spec/features/query_list_spec.rb
@@ -11,14 +11,14 @@ RSpec.feature "Query List" do
     let (:item) { fixtures.fetch("simple_search") }
 
     scenario "displays query lists for catalog" do
-      visit "catalog/#{item['doc_id']}"
+      visit "catalog/#{item['doc_id']}?query_list=true"
       expect(page).to have_text("Call Number Asc")
     end
 
     let (:item) { fixtures.fetch("url_856_0") }
 
     scenario "displays query lists for journals" do
-      visit "catalog/#{item['doc_id']}"
+      visit "catalog/#{item['doc_id']}?query_list=true"
       expect(page).to have_text("Call Number Desc")
     end
   end
