55d46fa0df78235394afc967def709d94a2b8945
Emily T <ebtoner@users.noreply.github.com>
2021-11-01 17:14:52 -0400

    BL-1579 Update EZBorrow integration for new system

--- spec/helpers/request_helper_spec.rb
+++ spec/helpers/request_helper_spec.rb
@@ -4,10 +4,10 @@ require "rails_helper"
 
 RSpec.describe RequestHelper, type: :helper do
   describe "#ez_borrow_link_with_updated_query(url)" do
-    let(:url) { "https://e-zborrow.relais-host.com/user/login.html?group=patron&LS=TEMPLE&dest=discovery&PI=12345&RK=12345&rft.stitle=A+thin+bright+line+%2F&rft.pub=The+University+of+Wisconsin+Press%2C&rft.place=Madison%2C+Wisconsin+%3A&rft.isbn=0299309304&rft.btitle=A+thin+bright+line+%2F&rft.genre=book&rft.normalized_isbn=9780299309305&rft.oclcnum=946770187&rft.mms_id=991028550499703811&rft.object_type=BOOK&rft.publisher=The+University+of+Wisconsin+Press%2C&rft.au=Bledsoe%2C+Lucy+Jane%2C+author.&rft.pubdate=%5B2016%5D&rft.title=A+thin+bright+line+%2F" }
+    let(:url) { "https://ezborrow.reshare.indexdata.com/?group=patron&LS=TEMPLE&dest=discovery&PI=12345&RK=12345&rft.stitle=A+thin+bright+line+%2F&rft.pub=The+University+of+Wisconsin+Press%2C&rft.place=Madison%2C+Wisconsin+%3A&rft.isbn=0299309304&rft.btitle=A+thin+bright+line+%2F&rft.genre=book&rft.normalized_isbn=9780299309305&rft.oclcnum=946770187&rft.mms_id=991028550499703811&rft.object_type=BOOK&rft.publisher=The+University+of+Wisconsin+Press%2C&rft.au=Bledsoe%2C+Lucy+Jane%2C+author.&rft.pubdate=%5B2016%5D&rft.title=A+thin+bright+line+%2F" }
 
     it "has correct link to resource" do
-      expect(ez_borrow_link_with_updated_query(url)).to eq("https://e-zborrow.relais-host.com/user/login.html?group=patron&LS=TEMPLE&dest=discovery&PI=12345&RK=12345&rft.title=A+thin+bright+line+%2F")
+      expect(ez_borrow_link_with_updated_query(url)).to eq("https://ezborrow.reshare.indexdata.com/Search/Results?lookfor=A+thin+bright+line+%2F&type=Title")
     end
   end
 
