b98b965a6f9988eedac7be91a2839f7c494265b8
sensei100 <tuh42082@temple.edu>
2022-04-06 11:52:26 -0400

    BL-1639 Update request button styling when logged in

--- app/helpers/request_helper.rb
+++ app/helpers/request_helper.rb
@@ -4,7 +4,7 @@ module RequestHelper
   include Blacklight::CatalogHelperBehavior
 
   def request_modal(mms_id, pickup_locations, request_level)
-    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn request-btn", data: { "blacklight-modal": "trigger", "action": "availability#modal show#loading", "availability-target": "href", "show-target": "href" })
+    link_to(t("requests.request_button"), "#", id: "request-btn-#{mms_id}", class: "btn request-btn record-page-request-btn", data: { "blacklight-modal": "trigger", "action": "availability#modal show#loading", "availability-target": "href", "show-target": "href" })
   end
 
   def request_redirect_url(mms_id)
