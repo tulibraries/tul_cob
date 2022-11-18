9ebe442ed52b6bfd19604456960d491aa133ac6e
sensei100 <tuh42082@temple.edu>
2022-11-18 15:58:22 -0500

    BL-1713 Summary preview updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -225,19 +225,30 @@ window.addEventListener('load', function(event) {
 });
 
 $(window).on('turbolinks:load', function() {
-	$(".summary-previews").each(function () {
-		console.log($(".summary-previews").height());
-		if ($(".summary-previews").height() > 40) {
-				let readMore = $('<a class="read-more">read more</a>');
+	const previews = document.getElementsByClassName("summary-previews");
+	
+	$(previews).each(function () {	
+		let readLess = $('<a class="read-less">less</a>');
+		let readMore = $('<a class="read-more">read more</a>');
+	
+		if ($(this).text().length > 300) {
+			console.log($(this).text().length);
 				$(this).css("display", "-webkit-box").css("-webkit-box-orient", "vertical").css("-webkit-line-clamp", "2").css("margin-bottom", "0");
 				$(readMore).insertAfter($(this));
+				$(readLess).insertAfter($(this)).hide();
 		}
-	});
 
-	$(".read-more").on("click", function () {
-		$(this).hide();
-		$(this).prev("div").removeAttr("style");
-		$(this).prev("div").css("margin-bottom", "20px");
+		$(readMore).on("click", function () {
+			$(this).hide();
+			console.log($(this).prev("div"));
+			$(this).siblings("div.summary-previews").removeAttr("style");
+			$(readLess).show();
+		});
+	
+			$(readLess).on("click", function () {
+			$(this).hide();
+			$(readMore).removeAttr("style");
+			$(this).prev("div").css("-webkit-box-orient", "vertical").css("-webkit-line-clamp", "2").css("margin-bottom", "0");
+		});
 	});
 });
-
