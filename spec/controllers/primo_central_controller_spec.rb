2f32af121a511531a8dea720633ef9b52a12451f
Jen <tuh42082@temple.edu>
2023-03-10 10:14:14 -0500

    BL-1758 Update author displays (#3671)

--- spec/controllers/primo_central_controller_spec.rb
+++ spec/controllers/primo_central_controller_spec.rb
@@ -17,27 +17,6 @@ RSpec.describe PrimoCentralController, type: :controller do
     allow(search_service).to receive(:search_results).and_return([mock_response, document])
   end
 
-  describe "#browse_creator" do
-    context "no creator" do
-      let(:presenter) { { document: document, field: "creator" } }
-      it "returns an empty list if nos creators are available" do
-        expect(controller.browse_creator(presenter)).to eq([])
-      end
-    end
-
-    context "a creator" do
-      let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
-        creator: ["Hello, World"],
-      )}
-      let(:presenter) { { document: document, field: "creator" } }
-      it "returns a list of links to creator search for each creator" do
-        expect(controller.browse_creator(presenter)).to eq([
-          "<a href=\"/?search_field=creator&amp;q=Hello%2C%20World\">Hello, World</a>",
-        ])
-      end
-    end
-  end
-
   describe "show action" do
     render_views
 
