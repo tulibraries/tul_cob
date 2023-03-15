591cd88fbd423e2509cb8d57f05db21a4c2d9d97
sensei100 <tuh42082@temple.edu>
2023-03-15 09:35:39 -0400

    Mask sensitive data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -397,11 +397,12 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.ignore_hosts "127.0.0.1", "localhost", "solr", "lgapi-us.libapps.com",
+  config.ignore_hosts "127.0.0.1", "localhost", "solr",
     "np-fim.temple.edu", ENV["SOLRCLOUD_HOST"], "chromedriver.storage.googleapis.com"
   config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data("LIB_GUIDES_API_KEY") { ENV["LIB_GUIDES_API_KEY"]}
   config.default_cassette_options = {
     match_requests_on: [:method]
   }
