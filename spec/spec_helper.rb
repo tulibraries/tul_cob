0ce06616f311ada4a90121d2b6bc97365b54a986
sensei100 <tuh42082@temple.edu>
2023-03-15 09:43:05 -0400

    fix rubocop again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -402,7 +402,7 @@ VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data("LIB_GUIDES_API_KEY") { ENV["LIB_GUIDES_API_KEY"]}
+  config.filter_sensitive_data("LIB_GUIDES_API_KEY") { ENV["LIB_GUIDES_API_KEY"] }
   config.default_cassette_options = {
     match_requests_on: [:method]
   }
