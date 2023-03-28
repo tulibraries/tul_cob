4a07633a4cb748115e3161364bcc55e5ce9947ac
sensei100 <tuh42082@temple.edu>
2023-03-28 10:54:49 -0400

    BL-1758 Align responsibility_display to the left on record pages

--- app/presenters/show_presenter.rb
+++ app/presenters/show_presenter.rb
@@ -18,4 +18,11 @@ class ShowPresenter < Blacklight::ShowPresenter
   def render_field?(field_config)
     field_presenter(field_config).render_field? and has_value? field_config
   end
+
+  def each_responsibility_field
+    fields_to_render do |field_name, field_config, field_presenter|
+      field_presenter.except_operations << Blacklight::Rendering::Join
+      yield field_name, field_config, field_presenter if field_config[:type] == :responsibility
+    end
+  end
 end
