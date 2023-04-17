92499d4a312dc9e1aa3fb4773d53347b46eb6d2b
Emily Toner <ebtoner@users.noreply.github.com>
2023-04-17 15:02:43 -0400

    Update axe_spec requirements and make needed code changes (#3764)

--- spec/features/axe_spec.rb
+++ spec/features/axe_spec.rb
@@ -5,28 +5,19 @@ require "rails_helper"
 RSpec.describe "Accessibility testing", api: false, js: true do
   xit "validates the home page" do
     visit root_path
-    #expect(page).to be_accessible
-    expect(page).to be_axe_clean
+    expect(page).to be_axe_clean.according_to :wcag2a, :wcag2aa
   end
 
-  xit "validates the catalog page" do
+  xit "validates a catalog search results page" do
     visit "/catalog"
     fill_in "q", with: "history"
     click_button "search"
-
-    # aria-allowed-role doesn"t like nav[role="region"]
-    #expect(page).to be_accessible(skipping: ["aria-allowed-role"])
-    expect(page).to be_axe_clean
+    expect(page).to be_axe_clean.according_to :wcag2a, :wcag2aa
   end
 
-  xit "validates the single results page" do
+  xit "validates the full record page for catalog record" do
     visit solr_document_path("991012171319703811")
-    #expect(page).to be_accessible
-    expect(page).to be_axe_clean
+    expect(page).to be_axe_clean.according_to :wcag2a, :wcag2aa
   end
 
-  def be_accessible(skipping: [])
-    # typeahead does funny things with the search bar
-    be_axe_clean.excluding(".tt-hint").skipping(skipping + [("color-contrast" if Bootstrap::VERSION < "5")])
-  end
 end
