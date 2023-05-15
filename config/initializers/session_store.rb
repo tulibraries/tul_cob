42087d233441afea5fdeb2843e3f4e1d82ca88b5
David Kinzer (he/him) <dtkinzer@gmail.com>
2023-05-15 12:22:44 -0400

    DEVO-805: Use memacached as the session store. (#3795)
    Move the session state out of the app to avoid needing to coordinate sessions with the k8 ingress.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,10 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_tulcob_session"
+
+if ENV["K8"] == "yes"
+  require "action_dispatch/middleware/session/dalli_store"
+  Rails.application.config.session_store :dalli_store, "_tulcob_session"
+else
+  Rails.application.config.session_store :cookie_store, key: "_tulcob_session"
+end
