b324dfb62172e1e3c66b9f38ecc631dca2ad255c
Emily Toner <ebtoner@users.noreply.github.com>
2023-11-17 09:06:26 -0500

    BL-1818 Reorganize code in application.js (#4069)

---
+++ app/assets/javascripts/libwizard_tutorial.js
@@ -0,0 +1,8 @@
+var lwz_options_f000bc5937fea3ef1bafbb124876025a = {"title" :
+																										"Library Search Basics",
+																										"fg": "#000000",
+																										"bg": "#f7941d",
+																										"pos": "50%",
+																										"side": "left",
+																										"button": "Display Help",
+																										"launch": false,}
