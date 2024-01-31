a07d2fb5c6a7894da0b31fdb6bb6e3817b9bd20c
sensei100 <tuh42082@temple.edu>
2024-01-31 16:24:22 -0500

    Use max_per_page setting

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -13,6 +13,7 @@ class CatalogController < ApplicationController
   before_action :authenticate_purchase_order!, only: [ :purchase_order, :purchase_order_action ]
   before_action :set_thread_request
   before_action only: :index do
+    blacklight_config.max_per_page = 50
     if params[:page] && params[:page].to_i > 250
       flash[:error] = t("blacklight.errors.deep_paging")
       redirect_to root_path
