a5a0a545c62aa8903bad4e59e6f5992755669aa1
Chris Doyle <cdoyle@temple.edu>
2024-02-16 13:55:00 -0500

    move controller logic to service object

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -21,7 +21,7 @@ class SearchController < CatalogController
 
       @results = process_results(searcher.results)
       @lib_guides_query_term = helpers.derived_lib_guides_search_term(@response)
-      @cdm_records = cdm_records(params[:q])
+      @cdm_records = CdmConnector.call(query: params[:q])
     end
 
     respond_to do |format|
@@ -60,33 +60,5 @@ class SearchController < CatalogController
       else
         results.except("cdm")
       end
-    end
-
-    def cdm_records(query)
-      query.gsub("/", " ")
-      query = ERB::Util.url_encode(query)
-      fields = "title!date"
-      format = "json"
-      cdm_url = "https://digital.library.temple.edu/digital/bl/dmwebservices/index.php?q=dmQuery/all/CISOSEARCHALL^#{query}^all^and/#{fields}/sortby/3/#{format}"
-      results = []
-      response = []
-
-      begin
-        response = JSON.load(URI.open(cdm_url))
-        total_items = response.dig("pager", "total") || 0
-        response["records"].each do |i|
-          item = OpenStruct.new
-          item.title = i.fetch("title")
-          item.date = i.fetch("date")
-          item.collection = i.fetch("collection")
-          item.link = "https://digital.library.temple.edu/digital/collection#{i["collection"]}/id/#{i["pointer"]}"
-          item.thumbnail = "https://digital.library.temple.edu/utils/ajaxhelper/?CISOROOT=#{i["collection"]}&CISOPTR=#{i["pointer"]}&action=2&DMSCALE=10&DMHEIGHT=340"
-          results << item
-        end
-      rescue StandardError => e
-        total_items = 0
-        Honeybadger.notify("Ran into error while try to process CDM: #{e.message}")
-      end
-      results
     end
 end
---
+++ app/services/cdm_connector.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class CdmConnector < ApplicationService
+  def initialize(*args)
+    query = args.first[:query]
+    query.gsub("/", " ")
+    query = ERB::Util.url_encode(query)
+    fields = "title!date"
+    format = "json"
+
+    @results = []
+
+    @service_url = "https://digital.library.temple.edu/digital/bl/dmwebservices/index.php?q=dmQuery/all/CISOSEARCHALL^#{query}^all^and/#{fields}/sortby/3/#{format}"
+  end
+
+  def call
+    begin
+      response = JSON.load(URI.open(@service_url))
+      total_items = response.dig("pager", "total") || 0
+      response["records"].each do |i|
+        item = OpenStruct.new
+        item.title = i.fetch("title")
+        item.date = i.fetch("date")
+        item.collection = i.fetch("collection")
+        item.link = "https://digital.library.temple.edu/digital/collection#{i["collection"]}/id/#{i["pointer"]}"
+        item.thumbnail = "https://digital.library.temple.edu/utils/ajaxhelper/?CISOROOT=#{i["collection"]}&CISOPTR=#{i["pointer"]}&action=2&DMSCALE=10&DMHEIGHT=340"
+        @results << item
+      end
+    rescue StandardError => e
+      total_items = 0
+      Honeybadger.notify("Ran into error while try to process CDM: #{e.message}")
+    end
+    @results
+  end
+end
+
+   
