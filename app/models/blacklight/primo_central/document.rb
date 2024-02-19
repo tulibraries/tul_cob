7b55261d66f044cd2ebc6d11ab0e7cac2cceb508
Steven Ng <steven.ng@temple.edu>
2024-02-19 07:50:32 -0800

    Extend tiemout time to 4 seconds instead

--- app/models/blacklight/primo_central/document.rb
+++ app/models/blacklight/primo_central/document.rb
@@ -156,7 +156,7 @@ module Blacklight::PrimoCentral::Document
 
       Thread.new {
         begin
-          HTTParty.get(libkey_articles_url, timeout: 10)["data"]
+          HTTParty.get(libkey_articles_url, timeout: 4)["data"]
             &.slice("retractionNoticeUrl", "fullTextFile", "contentLocation")
         rescue => e
           Honeybadger.notify(e)
