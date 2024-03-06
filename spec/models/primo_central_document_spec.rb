79f4dc8e624afa9904805f860a5acdc426840c87
ebtoner <emily.toner@temple.edu>
2024-03-06 17:40:57 -0500

    docum

--- spec/models/primo_central_document_spec.rb
+++ spec/models/primo_central_document_spec.rb
@@ -4,8 +4,8 @@ require "rails_helper"
 
 RSpec.describe PrimoCentralDocument, type: :model do
   let (:config) { PrimoCentralController.new.blacklight_config }
-  let(:doc) { { "pnxId" => "foobar" } }
-  let (:subject) { PrimoCentralDocument.new(doc, blacklight_config: config) }
+  let(:docs) { { "pnxId" => "foobar" } }
+  let (:subject) { PrimoCentralDocument.new(docs, blacklight_config: config) }
 
   it "is configurable" do
     expect(config).to_not be_nil
@@ -20,14 +20,14 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "a pnxs object is loaded" do
-    let(:doc) { { "pnxId" => "TN_fizzfuzz" } }
+    let(:docs) { { "pnxId" => "TN_fizzfuzz" } }
     it "removes TN_ from beginning of id" do
       expect(subject.id).to eq("fizzfuzz")
     end
   end
 
   context "with pnx description " do
-    let(:doc) { { "pnx" => { "search" => { "description" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "search" => { "description" => [ "foo" ] } } } }
 
     it "maps the description" do
       expect(subject["description"]).to eq("foo")
@@ -35,7 +35,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx subject" do
-    let(:doc) { { "pnx" => { "search" => { "subject" => [ "foo", "bar"] } } } }
+    let(:docs) { { "pnx" => { "search" => { "subject" => [ "foo", "bar"] } } } }
 
     it "maps the subject" do
       expect(subject["subject"]).to eq([ "foo", "bar" ])
@@ -43,7 +43,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx type" do
-    let(:doc) { { "pnx" => { "display" => { "type" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "type" => [ "foo" ] } } } }
 
     it "maps the type and format" do
       expect(subject["type"]).to eq([ "foo" ])
@@ -52,7 +52,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx title" do
-    let(:doc) { { "pnx" => { "display" => { "title" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "title" => [ "foo" ] } } } }
 
     it "maps the title" do
       expect(subject["title"]).to eq("foo")
@@ -60,7 +60,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx contributor" do
-    let(:doc) { { "pnx" => { "display" => { "contributor" => [ "foo;bar" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "contributor" => [ "foo;bar" ] } } } }
 
     it "maps the contributor" do
       expect(subject["contributor"]).to eq(["foo", "bar"])
@@ -68,56 +68,56 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx dislpay publisher" do
-    let(:doc) { { "pnx" => { "display" => { "publisher" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "publisher" => [ "foo" ] } } } }
     it "maps the publisher" do
       expect(subject["publisher"]).to eq(["foo"])
     end
   end
 
   context "with pnx addata pub" do
-    let(:doc) { { "pnx" => { "addata" => { "pub" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "addata" => { "pub" => [ "foo" ] } } } }
     it "maps the publisher" do
       expect(subject["publisher"]).to eq(["foo"])
     end
   end
 
   context "with pnx display relation" do
-    let(:doc) { { "pnx" => { "display" => { "relation" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "relation" => [ "foo" ] } } } }
     it "maps the relation" do
       expect(subject["relation"]).to eq(["foo"])
     end
   end
 
   context "with pnx search isbn" do
-    let(:doc) { { "pnx" => { "search" => { "isbn" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "search" => { "isbn" => [ "foo" ] } } } }
     it "maps isbn" do
       expect(subject["isbn"]).to eq(["foo"])
     end
   end
 
   context "with pnx addata lccn" do
-    let(:doc) { { "pnx" => { "addata" => { "lccn" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "addata" => { "lccn" => [ "foo" ] } } } }
     it "maps lccn" do
       expect(subject["lccn"]).to eq(["foo"])
     end
   end
 
   context "with pnx search issn" do
-    let(:doc) { { "pnx" => { "search" => { "issn" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "search" => { "issn" => [ "foo" ] } } } }
     it "maps issn" do
       expect(subject["issn"]).to eq(["foo"])
     end
   end
 
   context "with pnx display ispartof" do
-    let(:doc) { { "pnx" => { "display" => { "ispartof" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "ispartof" => [ "foo" ] } } } }
     it "maps isPartOf" do
       expect(subject["isPartOf"]).to eq("foo")
     end
   end
 
   context "with pnx search creatorcontrib" do
-    let(:doc) { { "pnx" => { "search" => { "creatorcontrib" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "search" => { "creatorcontrib" => [ "foo" ] } } } }
 
     it "maps creator" do
       expect(subject["creator"]).to eq(["foo"])
@@ -125,7 +125,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx search creationdate" do
-    let(:doc) { { "pnx" => { "search" => { "creationdate" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "search" => { "creationdate" => [ "foo" ] } } } }
 
     it "maps date" do
       expect(subject["date"]).to eq(["foo"])
@@ -133,7 +133,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx addata doi" do
-    let(:doc) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
 
     it "maps doi" do
       expect(subject["doi"]).to eq(["foo"])
@@ -141,7 +141,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx search language " do
-    let(:doc) { { "pnx" => { "search" => { "language" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "search" => { "language" => [ "foo" ] } } } }
 
     it "maps language" do
       expect(subject["language"]).to eq(["foo"])
@@ -149,7 +149,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with pnx display language " do
-    let(:doc) { { "pnx" => { "display" => { "language" => [ "foo" ] } } } }
+    let(:docs) { { "pnx" => { "display" => { "language" => [ "foo" ] } } } }
 
     it "maps language" do
       expect(subject["language"]).to eq(["foo"])
@@ -157,7 +157,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "with lang3" do
-    let(:doc) { { "lang3" => "foo" } }
+    let(:docs) { { "lang3" => "foo" } }
 
     it "maps language" do
       expect(subject["language"]).to eq(["foo"])
@@ -166,7 +166,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
 
   describe "#has_direct_link?" do
     context "document is completely empty" do
-      let(:doc) { Hash.new }
+      let(:docs) { Hash.new }
 
       it "verifies there is no direct_link" do
         expect(subject.has_direct_link?).to be(false)
@@ -174,7 +174,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
     end
 
     context "document has a direct_link" do
-      let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+      let(:docs) { ActiveSupport::HashWithIndifferentAccess.new(
         delivery: { availability: ["fulltext_linktorsrc"] }
       )}
 
@@ -184,7 +184,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
     end
 
     context "document does not have a direct link" do
-      let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+      let(:docs) { ActiveSupport::HashWithIndifferentAccess.new(
         delivery: { availability: ["foobar"] }
       )}
 
@@ -207,33 +207,30 @@ RSpec.describe PrimoCentralDocument, type: :model do
   end
 
   context "document direct_link contains rft.isbn = ''" do
-    let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+    let(:docs) { ActiveSupport::HashWithIndifferentAccess.new(
       delivery: {
-        GetIt1: [{
-          "links" => [{ "link" => "http://foobar.com?rft.isbn=" }],
-        }] }) }
+        "almaOpenurl": "http://foobar.com?rft.isbn="
+      }) }
     it "sets @doc['isbn'] to nil" do
       expect(subject["isbn"]).to be_nil
     end
   end
 
   context "document direc_link contains a regular value" do
-    let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+    let(:docs) { ActiveSupport::HashWithIndifferentAccess.new(
       delivery: {
-        GetIt1: [{
-          "links" => [{ "link" => "http://foobar.com?rft.isbn=a" }],
-        }] }) }
+        "almaOpenurl": "http://foobar.com?rft.isbn=a"
+      }) }
     it "sets @doc['isbn'] to [a]" do
       expect(subject["isbn"]).to eq(["a"])
     end
   end
 
   context "document direct_link is proxied and isbn=foo" do
-    let(:doc) { ActiveSupport::HashWithIndifferentAccess.new(
+    let(:docs) { ActiveSupport::HashWithIndifferentAccess.new(
       delivery: {
-        GetIt1: [{
-          "links" => [{ "link" => "http://myproxy.edu?url=http://foobar.com?rft.isbn=foo" }],
-        }] }) }
+        "almaOpenurl": "http://myproxy.edu?url=http://foobar.com?rft.isbn=foo"
+      }) }
     it "sets @doc['isbn'] to foo" do
       expect(subject["isbn"]).to eq(["foo"])
     end
@@ -286,7 +283,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
 
   describe "libkey_articles_url" do
     context "doi not present" do
-      let(:doc) { {} }
+      let(:docs) { {} }
 
       it "returns a nil" do
         expect(subject.libkey_articles_url).to be_nil
@@ -295,7 +292,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
     end
 
     context "fullTextFile present" do
-      let(:doc) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
+      let(:docs) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
 
       it "returns the fullTextFile URL string" do
         stub_request(:get, /articles/)
@@ -312,7 +309,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
     end
 
     context "contentLocation present" do
-      let(:doc) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
+      let(:docs) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
 
       it "returns the contentLocation URL string" do
         stub_request(:get, /articles/)
@@ -326,7 +323,7 @@ RSpec.describe PrimoCentralDocument, type: :model do
     end
 
     context "retractionNoticeUrl present" do
-      let(:doc) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
+      let(:docs) { { "pnx" => { "addata" => { "doi" => [ "foo" ] } } } }
 
       it "returns the fullTextFile URL string" do
         stub_request(:get, /articles/)
