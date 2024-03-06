a585202f0323f4152dfb177c834f477e937712fd
David Kinzer <dtkinzer@gmail.com>
2024-03-06 10:11:29 -0500

    hotfix: primo api updates (v1.25.2.1)

--- config/initializers/primo.rb
+++ config/initializers/primo.rb
@@ -3,8 +3,8 @@
 Primo.configure do |config|
   config.apikey  = ENV["PRIMO_API_KEY"] || Rails.configuration.bento&.dig(:primo, :apikey)
   config.context = :PC
-  config.vid     = "TULI"
-  config.scope   = "pci_scope"
+  config.vid     = "01TULI_INST:TULI"
+  config.scope   = "CentralIndex"
   config.enable_loggable = true
   config.enable_log_requests = true
   config.timeout = Rails.configuration.bento&.dig(:primo, :timeout) || 10
