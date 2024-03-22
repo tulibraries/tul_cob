5f20ac2f36dd138977321c5dacba5a6d906cf6af
Emily Toner <ebtoner@users.noreply.github.com>
2024-03-22 09:55:26 -0400

    BL-1829 Reduce number of api calls / uri checks for CDM engine in bento (#4324)
    * BL-1829 Reduce number of api calls / uri checks for CDM engine in bento
    
    * Rearrange methods
    
    * Rubocop fix

--- app/search_engines/bento_search/cdm_engine.rb
+++ app/search_engines/bento_search/cdm_engine.rb
@@ -10,22 +10,32 @@ module BentoSearch
       "https://digital.library.temple.edu"
     end
 
-    def conform_to_bento_result(item)
-      cdm_collection = item.fetch("collection", "").gsub("/", "")
-      cdm_id = item.fetch("pointer", "")
-      BentoSearch::ResultItem.new(
-        title: item.fetch("title", ""),
-        publication_date: item.fetch("date", ""),
-        source_title: cdm_collection_name(cdm_collection),
-        unique_id: cdm_id,
-        link: "#{base_url}/digital/collection/#{cdm_collection}/id/#{cdm_id}",
-        other_links: [image_scale(cdm_collection, cdm_id)]
-      )
+    def cdm_api_response(args)
+      query = args.fetch(:query, "").gsub("/", " ")
+      query = ERB::Util.url_encode(query)
+      cdm_fields = "title!date"
+      cdm_format = "json"
+      cdm_collections_ids = I18n.t("bento.cdm_collections_list")
+      cdm_url = "#{base_url}/digital/bl/dmwebservices/index.php?q=dmQuery/#{cdm_collections_ids}/CISOSEARCHALL^#{query}^all^and/#{cdm_fields}/nosort/5/0/1/0/0/0/0/0/#{cdm_format}"
+      begin
+        JSON.load(URI.open(cdm_url))
+      rescue StandardError => e
+        Honeybadger.notify("Error trying to process CDM api response: #{e.message}")
+      end
+    end
+
+    def cdm_collections_api_response
+      collections_url = "#{base_url}/digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json"
+      begin
+        JSON.load(URI.open(collections_url))
+      rescue StandardError => e
+        Honeybadger.notify("Error trying to process CDM Collections api response: #{e.message}")
+      end
     end
 
     def image_scale(collection, id)
       full_image = "#{base_url}/digital/iiif/2/#{collection}:#{id}/full/,220/0/default.jpg"
-      thumb = "#{base_url}/utils/getthumbnail/collection/#{collection}/id/#{id}"
+      thumbnail_image = "#{base_url}/utils/getthumbnail/collection/#{collection}/id/#{id}"
       default_image = "#{base_url}/digital/api/singleitem/image/#{collection}/#{id}"
       the_image = nil
 
@@ -39,37 +49,40 @@ module BentoSearch
 
       if the_image.present?
         the_image
-      elsif image_available?(thumb)
-        thumb
+      elsif image_available?(thumbnail_image)
+        thumbnail_image
       else image_available?(default_image)
            default_image
       end
     end
 
     def search_implementation(args)
-      query = args.fetch(:query, "").gsub("/", " ")
-      query = ERB::Util.url_encode(query)
-      fields = args.fetch(:cdm_fields)
-      format = args.fetch(:cdm_format)
-      collections = I18n.t("bento.cdm_collections_list")
-      cdm_url = "#{base_url}/digital/bl/dmwebservices/index.php?q=dmQuery/#{collections}/CISOSEARCHALL^#{query}^all^and/#{fields}/nosort/5/0/1/0/0/0/0/0/#{format}"
       bento_results = BentoSearch::Results.new
       response = []
-
-      begin
-        response = JSON.load(URI.open(cdm_url))
-      rescue StandardError => e
-        Honeybadger.notify("Error trying to process CDM api response: #{e.message}")
-      end
+      response = cdm_api_response(args)
 
       if response["records"].present?
         bento_results.total_items = response.dig("pager", "total") || 0
+        collections = []
+        collections = cdm_collections_api_response
+
         response["records"].each do |i|
+          collection_id = i.fetch("collection", "").gsub("/", "")
+          cdm_id = i.fetch("pointer", "")
+
+          # only take records with images and with alphanumeric titles
           unless is_int?(i.fetch("title", ""))
             if bento_results.size < 3
-              if image_available?(image_scale(i.fetch("collection", "").gsub("/", ""), i.fetch("pointer", "")))  # only take records with images and with alphanumeric titles
-                item = BentoSearch::ResultItem.new
-                item = conform_to_bento_result(i)
+              cdm_image = image_scale(collection_id, cdm_id)
+              if cdm_image.present?
+                item = BentoSearch::ResultItem.new(
+                  title: i.fetch("title", ""),
+                  publication_date: i.fetch("date", ""),
+                  source_title: cdm_collection_name(collection_id, collections),
+                  unique_id: cdm_id,
+                  link: "#{base_url}/digital/collection/#{collection_id}/id/#{cdm_id}",
+                  other_links: [cdm_image]
+                )
                 bento_results << item
               end
             end
@@ -88,10 +101,8 @@ module BentoSearch
       res.size > 0
     end
 
-    def cdm_collection_name(cdm_alias)
-      cdm_url = "https://digital.library.temple.edu/digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json"
-      response = JSON.load(URI.open(cdm_url))
-      collection = response.select { |collection| collection["secondary_alias"] if collection["secondary_alias"] == cdm_alias }
+    def cdm_collection_name(collection_id, collections_response)
+      collection = collections_response.select { |collection| collection["secondary_alias"] if collection["secondary_alias"] == collection_id }
       collection.first["name"] unless collection.blank?
     end
   end
