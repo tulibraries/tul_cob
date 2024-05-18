deb5cf3cd64ac2e25f201241ea887ba39ec46e11
David Kinzer <dtkinzer@gmail.com>
2024-05-18 10:12:01 -0400

    Update how assets get built for rails 7.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.unknown_asset_fallback = true
 
