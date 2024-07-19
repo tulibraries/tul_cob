d4734ebdc9f0e928c03268018bc399f4a895a11b
ebtoner <emily.toner@temple.edu>
2024-07-19 11:30:44 -0400

    BL-1865 Update delivery estimate message and do some refactoring

--- app/models/request_confirmation.rb
+++ app/models/request_confirmation.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class RequestConfirmation
-  include Lookupable
-
-  attr_reader :request_id
-  attr_reader :sent_from
-  attr_reader :pickup_location
+  attr_reader :request_id, :sent_from, :pickup_location
 
   def initialize(response, pickup_location = nil)
     @request_id = response&.request_id
@@ -14,32 +10,44 @@ class RequestConfirmation
   end
 
   def message
-    I18n.t("requests.default_success_message") + delivery_estimate_message + I18n.t("requests.request_status_message")
+    [I18n.t("requests.default_success_message"), delivery_estimate_message, I18n.t("requests.request_status_message")].join("")
   end
 
-  def delivery_estimate
-    if pickup_location.blank? || [ "JAPAN", "ROME"].include?(pickup_location)
-      nil
-    elsif pickup_location == "MAIN" && sent_from == "ASRS"
-      "1 hour, delivered from the Charles Library BookBot when open"
-    elsif pickup_location == sent_from
+  def delivery_estimate_message
+    unless pickup_location.blank? || ["JAPAN", "ROME"].include?(pickup_location)
+      RequestDeliveryEstimate.new(sent_from, pickup_location).message
+    end
+  end
+end
+
+class RequestDeliveryEstimate
+  include Lookupable
+
+  attr_reader :sent_from, :pickup_location
+
+  def initialize(sent_from = nil, pickup_location = nil)
+    @sent_from = sent_from
+    @pickup_location = pickup_location
+  end
+
+  def duration
+    if pickup_location == "MAIN" && sent_from == "ASRS"
+      "1 hour"
+    elsif pickup_location == "MAIN" && sent_from == "MAIN"
       "1-2 business days"
-    else
+    elsif pickup_location.present?
       "1-3 business days"
     end
   end
 
-  def delivery_estimate?
-    delivery_estimate.present?
-  end
-
-  def delivery_estimate_message
+  def message
+    return nil if duration.nil?
     pickup_location_name = library_name_from_short_code(pickup_location)
 
-    if delivery_estimate?
-      I18n.t("requests.delivery_estimate_message", pickup: pickup_location_name, estimate: delivery_estimate)
+    if duration.include?("hour")
+      I18n.t("requests.estimate_hours", pickup: pickup_location_name, duration:)
     else
-      ""
+      I18n.t("requests.estimate_days", pickup: pickup_location_name, duration:)
     end
   end
 end
