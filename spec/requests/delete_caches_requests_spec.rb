9f95c0bc46e174961446fb6711e341aa59e480bb
David Kinzer <dtkinzer@gmail.com>
2024-09-13 22:15:06 -0400

    rubocop.

--- spec/requests/delete_caches_requests_spec.rb
+++ spec/requests/delete_caches_requests_spec.rb
@@ -3,35 +3,35 @@
 require "rails_helper"
 
 RSpec.describe "DELETE #clear_caches", type: :request do
-    # We disable forgery protection by default in  our test environment.
-    # We need to enable it to properly test this endpoint.
-    around do |example|
-      original_forgery_setting = ActionController::Base.allow_forgery_protection
-      original_caching_setting = ActionController::Base.perform_caching
-      ActionController::Base.allow_forgery_protection = true
-      ActionController::Base.perform_caching = true
-      example.run
-    ensure
-      ActionController::Base.allow_forgery_protection = original_forgery_setting
-      ActionController::Base.perform_caching = original_caching_setting
+  # We disable forgery protection by default in  our test environment.
+  # We need to enable it to properly test this endpoint.
+  around do |example|
+    original_forgery_setting = ActionController::Base.allow_forgery_protection
+    original_caching_setting = ActionController::Base.perform_caching
+    ActionController::Base.allow_forgery_protection = true
+    ActionController::Base.perform_caching = true
+    example.run
+  ensure
+    ActionController::Base.allow_forgery_protection = original_forgery_setting
+    ActionController::Base.perform_caching = original_caching_setting
+  end
+
+  context "anonymous user" do
+    it "clears the caches" do
+      # Go to a catalog page
+      get "/catalog/991019830779703811"
+
+      # Assert that a cache page has been created
+      cache_dir = Rails.configuration.action_controller.page_cache_directory
+      expected_cache_file = File.join(cache_dir, "catalog", "991019830779703811.html")
+      expect(File.exist?(expected_cache_file)).to be true
+
+      # Now delete the cache
+      delete("/clear_caches")
+      expect(response.body).to match "Cache has been cleared"
+
+      # Assert that the cached file gets deleted
+      expect(File.exist?(expected_cache_file)).to be false
     end
-
-    context "anonymous user" do
-      it "clears the caches" do
-        # Go to a catalog page
-        get "/catalog/991019830779703811"
-
-        # Assert that a cache page has been created
-        cache_dir = Rails.configuration.action_controller.page_cache_directory
-        expected_cache_file = File.join(cache_dir, "catalog", "991019830779703811.html")
-        expect(File.exist?(expected_cache_file)).to be true
-
-        # Now delete the cache
-        delete("/clear_caches")
-        expect(response.body).to match "Cache has been cleared"
-
-        # Assert that the cached file gets deleted
-        expect(File.exist?(expected_cache_file)).to be false
-      end
-    end
+  end
 end
