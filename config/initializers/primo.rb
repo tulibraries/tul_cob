d3374c9e60366203b67a9a551730a503d9d9dc0b
Steven Ng <steven.ng@temple.edu>
2024-12-17 15:26:55 -0700

    Add retry enable flag to primo initializer (#4826)

--- config/initializers/primo.rb
+++ config/initializers/primo.rb
@@ -8,5 +8,6 @@ Primo.configure do |config|
   config.enable_loggable = true
   config.enable_log_requests = true
   config.timeout = Rails.configuration.bento&.dig(:primo, :timeout) || 10
+  config.enable_retries = true
   config.retries = 3
 end
