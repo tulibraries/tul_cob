340d99b7bb673336975c3bee8e8d9da88f9f6b89
sensei100 <tuh42082@temple.edu>
2025-01-16 13:59:40 -0500

    Fixes a bug when params["operator"] is nil

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -35,17 +35,19 @@ class CatalogController < ApplicationController
   end
 
   def advanced_override_path
-    if params["q_1"]&.split&.count == 1 && params["operator"]["q_1"] == "is"
-      blacklight_config.solr_path = "single_quoted_search"
-    end
+    unless params["operator"].nil?
+      if params["q_1"]&.split&.count == 1 && params["operator"]["q_1"] == "is"
+        blacklight_config.solr_path = "single_quoted_search"
+      end
 
-    if
-      params["q_2"]&.split&.count == 1 && params["operator"]["q_2"] == "is"
-      blacklight_config.solr_path = "single_quoted_search"
-    end
+      if
+        params["q_2"]&.split&.count == 1 && params["operator"]["q_2"] == "is"
+        blacklight_config.solr_path = "single_quoted_search"
+      end
 
-    if params["q_3"]&.split&.count == 1 && params["operator"]["q_3"] == "is"
-      blacklight_config.solr_path = "single_quoted_search"
+      if params["q_3"]&.split&.count == 1 && params["operator"]["q_3"] == "is"
+        blacklight_config.solr_path = "single_quoted_search"
+      end
     end
   end
 
