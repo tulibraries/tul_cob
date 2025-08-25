6be52c087ac2260622a242927f6d6cc50f3dbf65
Chris Doyle <cdoyle@temple.edu>
2025-08-25 11:53:12 -0400

    move new contructor to component only

--- app/components/tul_thumbnail_component.rb
+++ app/components/tul_thumbnail_component.rb
@@ -6,13 +6,13 @@ class TulThumbnailComponent < Blacklight::Document::ThumbnailComponent
   def initialize(presenter: nil, document: nil, counter:, image_options: {}, gb_preview: nil)
     @presenter = presenter
     @document = presenter&.document || document
+    @decorated_doc = DocumentDecorator.new(@document)
     @image_options = { alt: "" }.merge(image_options)
     @gb_preview = gb_preview
   end
 
   def render_google_books_data_attribute
-    decorated_doc = DocumentDecorator.new(@document)
-    decorated_doc.isbn_data_attribute || decorated_doc.lccn_data_attribute || decorated_doc.oclc_data_attribute
+    @decorated_doc.isbn_data_attribute || @decorated_doc.lccn_data_attribute || @decorated_doc.oclc_data_attribute
   end
 
   def before_render
