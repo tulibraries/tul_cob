e704a9742a775644a867b6896e46e298df189b62
Chris Doyle <cdoyle@temple.edu>
2025-08-29 14:31:01 -0400

    Add defensive nil-handling in TulThumbnailComponent for decorator and render

--- app/components/tul_thumbnail_component.rb
+++ app/components/tul_thumbnail_component.rb
@@ -5,13 +5,14 @@ class TulThumbnailComponent < Blacklight::Document::ThumbnailComponent
 
   def initialize(presenter:, image_options: {}, gb_preview: nil)
     @presenter = presenter
-    @document = presenter.document
-    @decorated_doc = DocumentDecorator.new(@document)
+    @document = presenter&.document
+    @decorated_doc = @document ? DocumentDecorator.new(@document) : nil
     @image_options = { alt: "" }.merge(image_options)
     @gb_preview = gb_preview
   end
 
   def render_google_books_data_attribute
+    return nil unless @decorated_doc
     @decorated_doc.isbn_data_attribute || @decorated_doc.lccn_data_attribute || @decorated_doc.oclc_data_attribute
   end
 
@@ -24,6 +25,6 @@ class TulThumbnailComponent < Blacklight::Document::ThumbnailComponent
   end
 
   def render?
-    true
+    @document.present?
   end
 end
