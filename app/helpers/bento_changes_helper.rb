f6d0fe0c1632732b53595e5e6835f2b15ebfdaf0
sensei100 <tuh42082@temple.edu>
2025-11-07 11:01:40 -0500

    Fix method names

--- app/helpers/bento_changes_helper.rb
+++ app/helpers/bento_changes_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module BentoChangesHelper
-  def aspace_integration.enabled?
+  def aspace_integration_enabled?
     Flipflop.aspace_integration?
   end
 
-  def style_updates.enabled?
+  def style_updates_enabled?
     Flipflop.style_updates?
   end
 end
