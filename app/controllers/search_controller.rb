2ef0684a04c096690f8956e5b7782b2d333f0eb8
Chris Doyle <cdoyle@temple.edu>
2025-11-20 16:54:26 -0500

    rubocop again

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -32,16 +32,15 @@ class SearchController < CatalogController
         render template
       end
       format.json do
-
         @results["lib_guides_query_term"] = @lib_guides_query_term unless @results.nil?
 
         render plain: @results.to_json, status: 200, content_type: "application/json"
+      end
     end
   end
 
-end
-
   private
+
     def configure_bento_item_partials
       item_partial = Flipflop.style_updates? ? "bento_search/std_item_new" : "bento_search/std_item"
       bento_engines = %w[blacklight journals databases library_website books_and_media articles cdm lib_guides]
