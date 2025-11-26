88326645157749c9905cf6a99ae67dd30500d60d
Chris Doyle <cdoyle@temple.edu>
2025-11-26 10:08:38 -0500

    updates check for 'archival_collections' engine with tests

---
+++ spec/views/bento_search/bento_results_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "search/_bento_results.html.erb", type: :view do
+  let(:result_item) do
+    double(
+      "result",
+      failed?: false,
+      engine_id: "archival_collections",
+      total_items: { query_total: 1 }
+    )
+  end
+  let(:results_hash) { { "archival_collections" => result_item } }
+  let(:locals) do
+    {
+      results_class: "grid",
+      results: results_hash,
+      options: {}
+    }
+  end
+
+  before do
+    allow(view).to receive(:renderable_results).and_return(results_hash)
+    allow(view).to receive(:bento_search).and_return("archival-collections-card")
+    allow(view).to receive(:render_linked_results).and_return("")
+  end
+
+  it "skips archival collections when flipflop is disabled" do
+    allow(view).to receive(:aspace_integration_enabled?).and_return(false)
+
+    render partial: "search/bento_results", locals: locals
+
+    expect(rendered).not_to include("archival-collections-card")
+  end
+
+  it "renders archival collections when flipflop is enabled" do
+    allow(view).to receive(:aspace_integration_enabled?).and_return(true)
+
+    render partial: "search/bento_results", locals: locals
+
+    expect(rendered).to include("archival-collections-card")
+  end
+end
