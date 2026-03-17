a2311f7b29dd7235976086d98977b33ad805307a
Steven Ng <steven.ng@temple.edu>
2026-03-17 12:00:55 -0600

    Move LibGuides v1.2 update into Bento engine

--- app/search_engines/bento_search/lib_guides_engine.rb
+++ app/search_engines/bento_search/lib_guides_engine.rb
@@ -5,16 +5,34 @@ module BentoSearch
     def search_implementation(args)
       bento_results = BentoSearch::Results.new
       guides_response = []
-      guides_response = LibGuidesApi.fetch(args[:query]).as_json
+
+      token = access_token
+      return bento_results if token.blank?
+
+      path = "https://lgapi-us.libapps.com/1.2/guides"
+      query = args[:query].gsub(" ", "+")
+      query_params = {
+        site_id: ENV["LIB_GUIDES_SITE_ID"],
+        search_terms: query,
+        status: 1,
+        sort_by: "relevance",
+        expand: "owner",
+        guide_types: "1,2,3,4"
+      }.to_param
+
+      response = HTTParty.get(
+        "#{path}?#{query_params}",
+        headers: { "Authorization" => "Bearer #{token}" }
+      )
+
+      guides_response = response.success? ? JSON.parse(response.body) : []
 
       results = guides_response[0, 3]
 
       results.each do |i|
         item = BentoSearch::ResultItem.new
         item.title = i["name"]
-        if i["description"].present?
-          item.abstract = i["description"]
-        end
+        item.abstract = i["description"] if i["description"].present?
         item.link = i["url"]
         bento_results << item
       end
@@ -33,5 +51,24 @@ module BentoSearch
       link_text = Flipflop.style_updates? ? "See all results" : "View all results"
       helper.link_to link_text, url, class: "bento-full-results bento_lib_guides_header"
     end
+
+    private
+
+      def access_token
+        response = HTTParty.post(
+          "https://lgapi-us.libapps.com/1.2/oauth/token",
+          body: {
+            client_id: ENV["LIB_GUIDES_CLIENT_ID"],
+            client_secret: ENV["LIB_GUIDES_CLIENT_SECRET"],
+            grant_type: "client_credentials"
+          }
+        )
+        return nil unless response.success?
+
+        JSON.parse(response.body)["access_token"]
+      rescue => e
+        Honeybadger.notify("Fetching LibGuides OAuth token failed with #{e}")
+        nil
+      end
   end
 end
