f660d0506356a91d9e391b5e38a653832da09ec8
David Kinzer <dtkinzer@gmail.com>
2026-04-24 16:19:39 -0400

    rubocop.

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
