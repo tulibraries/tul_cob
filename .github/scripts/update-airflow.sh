21c44d13292a6d9d9e35b23b5fa39ac6beeac3db
Jen <tuh42082@temple.edu>
2026-05-12 11:27:26 -0400

    Update script to better handle deploy keys (#5746)

--- .github/scripts/update-airflow.sh
+++ .github/scripts/update-airflow.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-# Load App Envs
 source ./.env
 
-# Get Airflow Deployment Scripts.
 cd ..
+
 git clone --single-branch --branch main git@github.com:tulibraries/ansible-playbook-airflow.git
+
 cd ansible-playbook-airflow
 
-# Install requirements
 pipenv install
-pipenv run ansible-galaxy install -r requirements.yml # install playbook role requirements
-echo $ANSIBLE_VAULT_PASSWORD > ~/.vault
+pipenv run ansible-galaxy install -r requirements.yml
 
-# Run
+printf "%s" "$ANSIBLE_VAULT_PASSWORD" > ~/.vault
+chmod 600 ~/.vault
+
+test -s ~/.ssh/conan_the_deployer
+ssh-keygen -y -f ~/.ssh/conan_the_deployer > /dev/null
+chmod 600 ~/.ssh/conan_the_deployer
+
+unset SSH_AUTH_SOCK
+unset SSH_AGENT_PID
+
 pipenv run ansible-playbook -i inventory/prod tul_cob.yml --private-key=~/.ssh/conan_the_deployer -e collection_name=$CATALOG_COLLECTION
+
