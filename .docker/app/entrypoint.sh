bc7a0a75f97298846851fd5daa7c210773e5edf4
sensei100 <tuh42082@temple.edu>
2026-08-03 11:05:33 -0400

    DEVO-1129 App updates to run in restricted mode

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+mkdir -p /secure-tmp/bootsnap /secure-tmp/storage
+
 rails db:migrate 2>/dev/null || rails db:setup
 rm -f /app/.internal_test_app/tmp/pids/server.pid
 
--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,12 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require "bootsnap"
+
+Bootsnap.setup(
+  cache_dir: ENV.fetch("BOOTSNAP_CACHE_DIR", File.expand_path("../tmp/cache", __dir__)),
+  development_mode: ENV["RAILS_ENV"] == "development",
+  load_path_cache: true,
+  compile_cache_iseq: true,
+  compile_cache_yaml: true
+)
