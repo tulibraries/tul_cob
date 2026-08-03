ae3cee7efe5fb9dc064d65112c70e84cecf8c829
sensei100 <tuh42082@temple.edu>
2026-08-03 12:01:58 -0400

    DEVO-1129 More restricted mode changes

--- config/initializers/primo.rb
+++ config/initializers/primo.rb
@@ -6,8 +6,7 @@ Primo.configure do |config|
   config.vid     = "01TULI_INST:TULI"
   config.scope   = "CentralIndex"
   config.enable_loggable = true
-  config.enable_log_requests = ENV["PRIMO_ENABLE_LOG_REQUESTS"] == "true"
-  config.logger = Rails.logger
+  config.enable_log_requests = true
   config.timeout = 3
   config.enable_retries = true
   config.retries = 3
